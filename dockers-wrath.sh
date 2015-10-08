ef8cc036163e95e5faad17cd083565f02124189b
fusillicode <fusillicode@gmail.com>
2015-10-08 10:43:52 +0200

    just a script ;P

---
+++ dockers-wrath.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Stop containers
+docker stop $(docker ps -a -q)
+# Remove all containers with volumes
+docker rm -v $(docker ps -a -q)
+# Remove all images
+docker rmi $(docker images -q)
