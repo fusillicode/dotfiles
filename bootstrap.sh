e3b5d763bdd7a0cd88afd036745db6adc73fba15
Gian D <fusillicode@users.noreply.github.com>
2015-12-03 20:34:32 +0100

    create vim temp files directories

--- bootstrap.sh
+++ bootstrap.sh
@@ -115,3 +115,6 @@ mysql_secure_installation
 # Set up git
 git config --global push.default current
 git config --global core.filemode false
+
+# Set up Vim temp files directories
+mkdir -p ~/.vim/.backup ~/.vim/.swp ~/.vim/.undo
