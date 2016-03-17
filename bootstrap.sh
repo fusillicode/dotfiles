6553acf42bee862cca3823b389460de31d2e2ec2
fusillicode <2248534+fusillicode@users.noreply.github.com>
2016-03-17 18:37:08 +0100

    this is how it's done...

--- bootstrap.sh
+++ bootstrap.sh
@@ -107,10 +107,12 @@ echo "gem: --no-document" >> ~/.gemrc
 # gem install rubocop
 # gem install rubycritic
 # gem install wordmove
-# git clone https://github.com/wpscanteam/wpscan.git
-# cd wpscan
-# bundle install --without test
-# cd .. && rm -rf wpscan
+
+# Install WPScan
+# cd ~/bin &&
+#   git clone https://github.com/wpscanteam/wpscan.git &&
+#   cd wpscan
+#   bundle install --without test
 
 # Install Oh My Zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
