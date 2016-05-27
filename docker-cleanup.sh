1f86b6d234096faa0614d7b15f9d83a8b8c7f7aa
Gian D <fusillicode@users.noreply.github.com>
2016-05-27 14:28:17 +0200

    Delete docker-cleanup.sh

--- docker-cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Stop containers
-docker stop $(docker ps -a -q)
-# Remove all containers with volumes
-docker rm -v $(docker ps -a -q)
-# Remove all images
-docker rmi -f $(docker images -q)
