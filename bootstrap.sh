40d3c993c2dbb9080fee51cab09276c5cd4dd755
fusillicode <2248534+fusillicode@users.noreply.github.com>
2016-09-30 16:33:10 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -103,7 +103,7 @@ echo "gem: --no-document" >> ~/.gemrc
 
 # TODO Install gems
 # . ~/.rvm/scripts/rvm
-# gem install artoo brakeman bundler-audit bundler cane compass consistency_fail html2slim license_finder rails rails-audit rails_best_practices rake rubocop rubycritic wordmove
+# gem install brakeman bundler-audit bundler cane compass consistency_fail html2slim license_finder rails rails-audit rails_best_practices rake rubocop rubycritic wordmove
 
 # TODO Install python modules (ansible and its requirements)
 # pip install ansible boto boto3 psycopg2
