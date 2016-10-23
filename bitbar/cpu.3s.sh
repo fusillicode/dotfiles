caedf0209131780b487c390ddfe9a1efd532cab0
fusillicode <2248534+fusillicode@users.noreply.github.com>
2016-10-23 10:34:50 +0200

    customize bitbar cpu and temperature

---
+++ bitbar/cpu.3s.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+OLDIFS=$IFS
+
+get_cpu_stats() {
+    local IFS=$'\n'
+    topdata=($(top -F -R -l2 -o cpu -n 5 -s 2 -stats pid,command,cpu))
+    nlines=${#topdata[@]}
+
+    IFS=$OLDIFS
+    for ((i = nlines / 2; i < nlines; i++)); do
+        line=(${topdata[$i]})
+        word=${line[0]}
+        if [ "$word" = CPU ]; then
+            cpu_usage=$(echo ${line[*]} | grep -o 'CPU usage:\s[0-9]\+\.[0-9]\+' | tail -n1 | grep -o '[0-9]\+.[0-9]\+')
+        elif [ "$word" = PID ]; then
+            top_5=("${topdata[@]:$i}")
+        fi
+    done
+
+    IFS=$'\n'
+}
+
+get_cpu_stats
+
+echo "$cpu_usage | size=12"
+echo "---"
+top_5=("${top_5[@]/%/| font=Menlo}")
+IFS=$'\n'
+echo "${top_5[*]}"
