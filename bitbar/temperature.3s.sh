7fd3e5dad536eb021a39538eba56386c8a64bf79
fusillicode <fusillicode@gmail.com>
2016-10-23 10:41:47 +0200

    refactor

--- bitbar/temperature.3s.sh
+++ bitbar/temperature.3s.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cpu_temperature=$(/usr/local/bin/smc -k TC0P -r | grep -o '[0-9]\+\.')
+cpu_temperature=$(~/bin/smc -k TC0P -r | grep -o '[0-9]\+\.')
 echo "${cpu_temperature%?}°c | size=12"
 
 echo "---"
-fan_speed=$(/usr/local/bin/smc -f | grep 'Actual speed' | grep -o '[0-9]\+')
+fan_speed=$(~/bin/smc -f | grep 'Actual speed' | grep -o '[0-9]\+')
 echo "$fan_speed RPM"
