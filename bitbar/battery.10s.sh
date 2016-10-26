0089d2ae77e8414da4f14663aed1d89068855869
fusillicode <fusillicode@gmail.com>
2016-10-26 20:31:05 +0200

    reverse battery info

--- bitbar/battery.10s.sh
+++ bitbar/battery.10s.sh
@@ -2,13 +2,13 @@
 
 battery_info=$(system_profiler SPPowerDataType)
 
-cycle_count=$(echo "$battery_info" | grep "Cycle Count" | awk '{print $3}')
-echo "$cycle_count | size=12"
+charge_remaining=$(echo "$battery_info" | grep "Charge Remaining" | awk '{print $4}')
+echo "$charge_remaining mAh | size=12"
 
 echo "---"
 
-charge_remaining=$(echo "$battery_info" | grep "Charge Remaining" | awk '{print $4}')
-echo "$charge_remaining mAh"
+cycle_count=$(echo "$battery_info" | grep "Cycle Count" | awk '{print $3}')
+echo "$cycle_count cycles"
 
 condition=$(echo "$battery_info" | grep "Condition" | sed -e 's/^.*: //')
 echo "Battery condition: $condition"
