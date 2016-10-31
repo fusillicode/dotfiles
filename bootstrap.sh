148c63a329dc3bc7e8f0fe498d2f78629322ce6d
fusillicode <2248534+fusillicode@users.noreply.github.com>
2016-10-31 20:57:23 +0100

    remove uneeded atom languages

--- bootstrap.sh
+++ bootstrap.sh
@@ -121,7 +121,7 @@ echo "gem: --no-document" >> ~/.gemrc
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 # Install Atom packages
-apm install language-apache language-babel language-docker language-elixir language-elm language-generic-config language-haproxy language-haskell language-nginx language-rspec language-rust language-scala language-slim atom-alignment change-case custom-title git-plus highlight-column lines open-git-modified-files pinned-tabs rails-transporter ruby-test
+apm install language-babel language-docker language-elixir language-elm language-haskell language-rspec language-rust language-scala language-slim atom-alignment change-case custom-title git-plus highlight-column lines open-git-modified-files pinned-tabs rails-transporter ruby-test
 
 # Symlink Firefox to global Applications directory to fix Selenium driver
 ln -s ~/Applications/Firefox.app /Applications/
