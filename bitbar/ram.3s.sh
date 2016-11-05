105c519b76569339cf3651fb2ae9e8e14fb64c8b
fusillicode <fusillicode@gmail.com>
2016-11-05 15:53:30 +0100

    used instead of free mem usage

--- bitbar/ram.3s.sh
+++ bitbar/ram.3s.sh
@@ -2,4 +2,5 @@
 
 memory_pressure=$(memory_pressure)
 system_wide_memory_free_percentage=$(memory_pressure | grep 'System-wide memory free percentage' | grep -o '[0-9]\+')
-echo "${system_wide_memory_free_percentage}% | size=11"
+system_wide_used_memory_percentage=$((100 - $system_wide_memory_free_percentage))
+echo "${system_wide_used_memory_percentage}% | size=11"
