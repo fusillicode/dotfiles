f874c324ddefd68d98ae370e79db04402aa44f93
fusillicode <2248534+fusillicode@users.noreply.github.com>
2017-03-20 19:03:18 +0100

    Add nodejs with @kandros

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,6 +19,7 @@ brew tap caskroom/versions
 brew install awscli
 brew install beeftornado/rmtree/brew-rmtree
 brew install chromedriver
+brew install coreutils
 brew install cputhrottle
 brew install exercism
 brew install fwup
@@ -75,6 +76,8 @@ asdf plugin-add elm https://github.com/vic/asdf-elm.git
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
 asdf plugin-add haskell https://github.com/vic/asdf-haskell.git
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 asdf plugin-add php https://github.com/odarriba/asdf-php.git
 asdf plugin-add postgres https://github.com/smashedtoatoms/asdf-postgres.git
 asdf plugin-add redis https://github.com/smashedtoatoms/asdf-redis.git
