7ebbc2390b0ed24d629f2d58a0977dfccd7fc2c1
fusillicode <2248534+fusillicode@users.noreply.github.com>
2017-07-27 14:36:44 +0200

    + fasterer gem - geckodriver

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,7 +19,6 @@ brew tap caskroom/versions
 brew install awscli
 brew install chromedriver
 brew install exercism
-brew install geckodriver
 brew install git
 brew install heroku
 brew install imagemagick@6
@@ -101,7 +100,7 @@ bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 # asdf plugin-add swift https://github.com/fcrespo82/asdf-swift
 
 # Install Ruby gems
-gem install --no-rdoc --no-ri brakeman bundler-audit bundler cane compass consistency_fail html2slim license_finder rails rails-audit rails_best_practices rake reek ruby-lint rubocop rubycritic wordmove
+gem install --no-rdoc --no-ri brakeman bundler-audit bundler cane compass consistency_fail fasterer html2slim license_finder rails rails-audit rails_best_practices rake reek ruby-lint rubocop rubycritic wordmove
 
 # Install WpScan
 mkdir -p ~/bin/ && ~/bin && git clone https://github.com/wpscanteam/wpscan.git && cd wpscan && bundle install --without test
