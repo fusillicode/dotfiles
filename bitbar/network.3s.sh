88c01ef214a5e59a348c426f0d05668e630b41fd
fusillicode <fusillicode@gmail.com>
2017-09-01 12:03:50 +0200

    add network speed script 🌏

---
+++ bitbar/network.3s.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export PATH="/usr/local/bin:${PATH}"
+INTERFACES=$(ifconfig -lu)
+
+echo "$(ifstat -n -w -i en0 0.1 1 | tail -n 1 | awk '{printf "%0.2f Mb/s %0.2f Mb/s", $1/128, $2/128;}') | size=11"
+echo "---"
+
+for INTERFACE in ${INTERFACES}; do
+  if [[ ${INTERFACE} != "en0" ]]; then
+    echo "$(ifstat -n -w -i "${INTERFACE}" 0.1 1 | tail -n 1 | awk '{printf "%0.2f Mb/s %0.2f Mb/s", $1/128, $2/128;}') (${INTERFACE})"
+  fi
+done
