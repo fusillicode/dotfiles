384acc061d090f237324a61a389da44257d08ef9
fusillicode <2248534+fusillicode@users.noreply.github.com>
2017-09-03 09:39:09 +0200

    update BitBar script icons

--- bitbar/network.3s.sh
+++ bitbar/network.3s.sh
@@ -3,11 +3,11 @@
 export PATH="/usr/local/bin:${PATH}"
 INTERFACES=$(ifconfig -lu)
 
-echo "$(ifstat -n -w -i en0 0.1 1 | tail -n 1 | awk '{printf "%0.2f Mb/s %0.2f Mb/s", $1/128, $2/128;}') | size=11"
+echo "$(ifstat -n -w -i en0 0.1 1 | tail -n 1 | awk '{printf "%0.2f ▼ %0.2f ▲", $1/128, $2/128;}') | size=11"
 echo "---"
 
 for INTERFACE in ${INTERFACES}; do
   if [[ ${INTERFACE} != "en0" ]]; then
-    echo "$(ifstat -n -w -i "${INTERFACE}" 0.1 1 | tail -n 1 | awk '{printf "%0.2f Mb/s %0.2f Mb/s", $1/128, $2/128;}') (${INTERFACE})"
+    echo "$(ifstat -n -w -i "${INTERFACE}" 0.1 1 | tail -n 1 | awk '{printf "%0.2f ▼ %0.2f ▲", $1/128, $2/128;}') (${INTERFACE})"
   fi
 done
