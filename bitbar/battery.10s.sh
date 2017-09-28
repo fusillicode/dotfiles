95e520d4892d31bc492507a21396618154178579
fusillicode <2248534+fusillicode@users.noreply.github.com>
2017-09-28 16:59:14 +0200

    refactor bitbar plugins

--- bitbar/battery.10s.sh
+++ bitbar/battery.10s.sh
@@ -1,14 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 battery_info=$(system_profiler SPPowerDataType)
 
 charge_remaining=$(echo "$battery_info" | grep "Charge Remaining" | awk '{print $4}')
-echo "$charge_remaining ⚡︎ | size=11"
+echo "$charge_remaining ⚡︎ | size=10"
 
 echo "---"
-
 cycle_count=$(echo "$battery_info" | grep "Cycle Count" | awk '{print $3}')
-echo "$cycle_count cycles"
-
+echo "$cycle_count cycles | size=12"
 condition=$(echo "$battery_info" | grep "Condition" | sed -e 's/^.*: //')
-echo "Battery condition: $condition"
+echo "Battery condition: $condition | size=12"
--- bitbar/network.3s.sh
+++ bitbar/network.3s.sh
@@ -1,13 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 export PATH="/usr/local/bin:${PATH}"
-INTERFACES=$(ifconfig -lu)
 
-echo "$(ifstat -n -w -i en0 0.1 1 | tail -n 1 | awk '{printf "%0.2f ▼ %0.2f ▲", $1/128, $2/128;}') | size=11"
+echo "$(ifstat -n -w -i en0 0.1 1 | tail -n 1 | awk '{printf "%0.2f ▼ %0.2f ▲", $1/128, $2/128;}') | size=10"
+
 echo "---"
-
-for INTERFACE in ${INTERFACES}; do
-  if [[ ${INTERFACE} != "en0" ]]; then
-    echo "$(ifstat -n -w -i "${INTERFACE}" 0.1 1 | tail -n 1 | awk '{printf "%0.2f ▼ %0.2f ▲", $1/128, $2/128;}') (${INTERFACE})"
+for interface in $(ifconfig -lu); do
+  if [[ ${interface} != "en0" ]]; then
+    echo "$(ifstat -n -w -i "${interface}" 0.1 1 | tail -n 1 | awk '{printf "%0.2f ▼ %0.2f ▲", $1/128, $2/128;}') (${interface}) | size=12"
   fi
 done
