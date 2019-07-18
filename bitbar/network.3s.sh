27d30438cc8fde57bede1013c2ea45edde0af192
fusillicode <fusillicode@gmail.com>
2019-07-18 08:16:03 +0200

    Refactor network widget

--- bitbar/network.3s.sh
+++ bitbar/network.3s.sh
@@ -2,9 +2,11 @@
 
 export PATH="/usr/local/bin:${PATH}"
 
+interfaces=$(ifconfig -lu)
+
 echo "$(ifstat -n -w -i en0 0.1 1 | tail -n 1 | awk '{printf "%0.2f ▼ %0.2f ▲", $1/128, $2/128;}') | size=10"
 echo "---"
-for interface in $(ifconfig -lu); do
+for interface in ${interfaces}; do
   if [[ ${interface} != "en0" ]]; then
     echo "$(ifstat -n -w -i "${interface}" 0.1 1 | tail -n 1 | awk '{printf "%0.2f ▼ %0.2f ▲", $1/128, $2/128;}') (${interface}) | size=12 color=white"
   fi
