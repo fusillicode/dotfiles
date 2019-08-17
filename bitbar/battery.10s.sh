86da62f1802bff3c149a400de0485b7159e4c31b
fusillicode <fusillicode@gmail.com>
2019-08-17 15:50:22 +0200

    BitBar stuff

--- bitbar/battery.10s.sh
+++ bitbar/battery.10s.sh
@@ -3,7 +3,7 @@
 battery_info=$(system_profiler SPPowerDataType)
 
 charge_remaining=$(echo "$battery_info" | grep "Charge Remaining" | awk '{print $4}')
-echo "$charge_remaining ⚡︎ | size=10"
+echo "$charge_remaining ⚡︎ | size=12"
 echo "---"
 cycle_count=$(echo "$battery_info" | grep "Cycle Count" | awk '{print $3}')
 echo "$cycle_count cycles | size=12 color=white"
