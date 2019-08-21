44dd53841602ad23c12dd18c7abdd3819ae8a1cc
fusillicode <2248534+fusillicode@users.noreply.github.com>
2019-08-21 16:14:37 +0200

    Battery temp!

--- bitbar/inactive/temperature.7s.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cpu_temperature=$(/Applications/smcFanControl.app/Contents/Resources/smc -k TC0P -r | grep -o '[0-9]\+\.')
-echo "${cpu_temperature%?}℃ | size=10"
-echo "---"
-fan_speed=$(/Applications/smcFanControl.app/Contents/Resources/smc -k F0Ac -r | grep -o '\s\s[0-9]\+')
-echo "$fan_speed RPM | size=12 color=white"
