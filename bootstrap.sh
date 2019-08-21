2a27ccfcc62ace07c1b4104076594bdc9c8858a6
fusillicode <2248534+fusillicode@users.noreply.github.com>
2019-08-21 15:30:26 +0200

    Handle the Snake with asdf

--- bootstrap.sh
+++ bootstrap.sh
@@ -69,6 +69,7 @@ brew install autoconf wxmac
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin-add python https://github.com/asdf-vm/asdf-python.git
 
 # Install Ruby gems
 gem install --no-rdoc --no-ri \
