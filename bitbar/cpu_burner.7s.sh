bb18d03428014da73a4921664cae15cb7e80b0bb
fusillicode <fusillicode@gmail.com>
2019-08-27 09:12:01 +0200

    Add BitBar cpu burner

---
+++ bitbar/cpu_burner.7s.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cpu_burner_data=($(ps -Ar -o pid= -o %cpu= -o comm= | head -n 1))
+
+cpu_burner_name=${cpu_burner_data[2]##*/}
+
+echo "${cpu_burner_data[0]}  $cpu_burner_name  ${cpu_burner_data[1]}% | size=12"
