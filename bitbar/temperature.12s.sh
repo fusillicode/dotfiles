3cffbca50938e5053bb4085439a2b627ca392e72
fusillicode <2248534+fusillicode@users.noreply.github.com>
2020-12-27 15:19:06 +0100

    rg

--- bitbar/temperature.12s.sh
+++ bitbar/temperature.12s.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cpu_temperature=$(/Applications/smcFanControl.app/Contents/Resources/smc -k TC0P -r | grep -o '[0-9]\+\.')
-battery_temperature=$(/Applications/smcFanControl.app/Contents/Resources/smc -k TB0T -r | grep -o '[0-9]\+\.')
-echo "${cpu_temperature%?}℃  ${battery_temperature%?}℃ | size=12"
+cpu_temperature=$(/Applications/smcFanControl.app/Contents/Resources/smc -k TC0P -r | /usr/local/bin/rg ".*\s([0-9]{2})\..*" -r '$1')
+battery_temperature=$(/Applications/smcFanControl.app/Contents/Resources/smc -k TB0T -r | /usr/local/bin/rg ".*\s([0-9]{2})\..*" -r '$1')
+echo "${cpu_temperature%}℃  ${battery_temperature%}℃ | size=12"
