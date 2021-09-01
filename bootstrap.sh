26564acee2d3a932ae05a8bfa6f3ea5af1eee2e2
fusillicode <2248534+fusillicode@users.noreply.github.com>
2021-09-01 12:51:10 +0200

    Revert "Revert "welcome podman""
    This reverts commit a38db22d0f64a486fd9dd7321249f5ecf190fdef.

--- bootstrap.sh
+++ bootstrap.sh
@@ -43,7 +43,6 @@ brew install watch
 brew install zsh
 
 # Install Homebrew-Cask apps
-brew install homebrew/cask/docker
 brew install android-file-transfer
 brew install appcleaner
 brew install bitbar
@@ -56,6 +55,7 @@ brew install google-chrome
 brew install http-toolkit
 brew install league-of-legends
 # brew install libreoffice
+brew install podman
 brew install rectangle
 # brew install sfdx
 brew install skype
