854ae1d7d32ef79549ec1f8ba4500e706d26192f
fusillicode <2248534+fusillicode@users.noreply.github.com>
2022-04-29 22:11:55 +0200

    Update bitbar plugins for M1

--- bitbar/battery.33s.sh
+++ bitbar/battery.33s.sh
@@ -2,10 +2,12 @@
 
 battery_info=$(system_profiler SPPowerDataType)
 
-charge_remaining=$(echo "$battery_info" | /opt/homebrew/bin/rg "Full Charge Capacity" | awk '{print $5}')
-echo "$charge_remaining ⚡︎ | size=12"
+charge_remaining=$(echo "$battery_info" | /opt/homebrew/bin/rg "State of Charge" | awk '{print $5}')
+echo "⚡︎ $charge_remaining% | size=12"
 echo "---"
 cycle_count=$(echo "$battery_info" | /opt/homebrew/bin/rg "Cycle Count" | awk '{print $3}')
 echo "$cycle_count cycles | size=12 color=white"
+maximum_capacity=$(echo "$battery_info" | /opt/homebrew/bin/rg "Maximum Capacity" | awk '{print $3}')
+echo "Max capacity: $maximum_capacity | size=12 color=white"
 condition=$(echo "$battery_info" | /opt/homebrew/bin/rg "Condition" | sed -e 's/^.*: //')
 echo "Battery condition: $condition | size=12 color=white"
