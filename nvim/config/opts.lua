325b276682a1e149d965680103e1e524e888f2c5
Gianluca Randazzo <fusillicode@gmail.com>
2022-12-26 09:26:17 +0100

    break up init.lua

---
+++ nvim/config/opts.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = ' '
+vim.opt.autoindent = true
+vim.opt.background = 'dark'
+vim.opt.backspace = 'indent,eol,start'
+vim.opt.clipboard:append('unnamedplus')
+vim.opt.colorcolumn = '120'
+vim.opt.expandtab = true
+vim.opt.guicursor = ''
+vim.opt.ignorecase = true
+vim.opt.iskeyword:append('-')
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.shiftwidth = 2
+vim.opt.smartcase = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 2 
+vim.opt.termguicolors = true
+vim.opt.wrap = false
