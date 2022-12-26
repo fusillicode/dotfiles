9e33a8092a26af43ad87fbb68b4437c54cf12814
Gianluca Randazzo <fusillicode@gmail.com>
2022-12-26 09:26:17 +0100

    break up init.lua

---
+++ nvim/config/packer.lua
@@ -0,0 +1,25 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'jim-at-jibba/ariake-vim-colors'
+  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use 'lewis6991/gitsigns.nvim'
+  
+  if packer_bootstrap then
+    require('packer').sync()
+    return
+  end
+end)
+
