b462f5179f2b057cc4e4a899c162dc418df34a26
Gianluca Randazzo <fusillicode@gmail.com>
2022-12-27 08:20:46 +0100

    lots of stuff

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -11,7 +11,6 @@ require('telescope').setup{
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
