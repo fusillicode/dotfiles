e5f236c59ad9b4cf158c21c551be50eb4012b0ec
Gianluca Randazzo <fusillicode@gmail.com>
2022-12-31 13:53:08 +0100

    meh

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -26,7 +26,7 @@ local on_attach = function(_, bufnr)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+  -- vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
