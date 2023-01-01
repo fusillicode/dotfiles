9c28a5068456d5126b62cdb8bf82d833db6616a5
fusillicode <2248534+fusillicode@users.noreply.github.com>
2023-01-01 10:12:02 +0100

    nvim-tree ftw

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,3 @@
 local opts = { noremap = true, silent = true }
 vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
-vim.keymap.set("n", "<leader>e", ":Lexplore <cr>", opts)
---
+++ nvim/lua/config/nvim-tree.lua
@@ -0,0 +1,24 @@
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+    icons = {
+      show = {
+        file = false,
+        folder = false,
+        folder_arrow = false
+      }
+    }
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
