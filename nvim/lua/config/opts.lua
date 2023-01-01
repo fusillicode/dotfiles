db577c84f19bc642f9e30b7e23d878a1848740b9
Gianluca Randazzo <fusillicode@gmail.com>
2023-01-01 10:12:02 +0100

    nvim-tree ftw

--- nvim/lua/config/opts.lua
+++ nvim/lua/config/opts.lua
@@ -1,9 +1,6 @@
 vim.g.mapleader = ' '
-vim.g.netrw_banner = 0
-vim.g.netrw_liststyle = 3
-vim.g.netrw_browse_split = 0
-vim.g.netrw_altv = 1
-vim.g.netrw_winsize = 10
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 vim.opt.autoindent = true
 vim.opt.background = 'dark'
 vim.opt.backspace = 'indent,eol,start'
