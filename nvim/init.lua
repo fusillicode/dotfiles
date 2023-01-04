1447307b783bec698a62476427bd7a498ba14565
Gianluca Randazzo <fusillicode@gmail.com>
2023-01-04 16:10:00 +0100

    more cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -79,6 +79,7 @@ if is_packer_boostrapped then
 end
 
 vim.cmd [[colorscheme gruvbox]]
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.o.autoindent = true
@@ -190,6 +191,8 @@ require('telescope').setup {
 
 require('nvim-treesitter.configs').setup {
   ensure_installed = { 'lua', 'python', 'rust', 'help', 'vim' },
+  sync_install = true,
+  auto_install = false,
 
   highlight = { enable = true },
   incremental_selection = {
@@ -255,25 +258,15 @@ local on_attach = function(_, bufnr)
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
 
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-
+  nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
+  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end)
+  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
