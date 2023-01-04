63545559674854e1255f31d6103b2022f940bb82
Gianluca Randazzo <fusillicode@gmail.com>
2023-01-04 14:54:07 +0100

    Lot of stuff..

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -1,47 +1,6 @@
-
 require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'ellisonleao/gruvbox.nvim'
-  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-  use 'lewis6991/gitsigns.nvim'
   use 'Pocco81/auto-save.nvim'
-  use { 'nvim-treesitter/nvim-treesitter', run = { ':TSUpdate' } }
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-nvim-lua'
-  use 'L3MON4D3/LuaSnip'
   use 'rafamadriz/friendly-snippets'
   use 'simrat39/rust-tools.nvim'
-  use {
-    'saecki/crates.nvim',
-    requires = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('crates').setup()
-    end,
-  }
   use 'windwp/nvim-spectre'
-  use {
-    "ahmedkhalf/project.nvim",
-    config = function()
-      require("project_nvim").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
-  }
-  use 'nvim-tree/nvim-tree.lua'
-  use 'bogado/file-line'
-  use 'chrisgrieser/nvim-genghis'
-
-  if packer_bootstrap then
-    require('packer').sync()
-    return
-  end
 end)
