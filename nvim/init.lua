2b87fe0356152827c61d25373138d62941226e89
Gianluca Randazzo <fusillicode@gmail.com>
2023-01-09 18:07:49 +0100

    bring back diagnostic auto hover

--- nvim/init.lua
+++ nvim/init.lua
@@ -73,7 +73,7 @@ require 'tokyonight'.setup { style = 'night' }
 
 vim.cmd('autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()')
 vim.cmd('colorscheme tokyonight')
-vim.cmd('autocmd! CursorHold, CursorHoldI * lua vim.diagnostic.open_float(nil, { focus = false })')
+vim.cmd('autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, { focus = false })')
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = ' '
