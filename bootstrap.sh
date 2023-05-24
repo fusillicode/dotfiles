17dfc0222065bc59511869a07e9ab32a50d266ae
Gianluca Randazzo <fusillicode@gmail.com>
2023-05-24 13:43:58 +0200

    Switch to rtx?

--- bootstrap.sh
+++ bootstrap.sh
@@ -83,27 +83,21 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install --force cargo-make
 cargo install sqlx-cli
 
-# Configure asdf
-echo -e "\n. $(brew --prefix asdf)/asdf.sh" >> ~/.zshrc
-echo -e "\n. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash" >> ~/.zshrc
+# Configure rtx
+echo 'eval "$(~/bin/rtx activate zsh)"' >> ~/.zshrc
 
-# Install asdf plugins
+# Install rtx plugins
 # brew install bdw-gc libevent llvm && brew link llvm --force # Don't know, seems not needed anymore...¯\_(ツ)_/¯
-# asdf plugin-add crystal https://github.com/marciogm/asdf-crystal.git
+# rtx install crystal
 brew install autoconf wxwidgets
 export KERL_CONFIGURE_OPTIONS="--without-javac --with-ssl=$(brew --prefix openssl@1.1)"
-asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
-asdf install erlang latest
-asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-asdf install elixir latest
-# asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf plugin-add python
-asdf install python latest
-# brew install gpg && asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git && bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
-# https://github.com/asdf-vm/asdf-nodejs#nvmrc-and-node-version-files
-# "legacy_version_file = yes" >> ~/.asdfrc
-# asdf plugin-add java https://github.com/halcyon/asdf-java.git
-# asdf plugin-add terraform https://github.com/Banno/asdf-hashicorp.git
+rtx install erlang latest
+rtx install elixir latest
+# rtx install ruby
+rtx install python latest
+rtx install poetry latest
+# rtx install java latest
+# rtx install terraform latest
 
 # Install Ruby gems
 gem install --no-rdoc --no-ri \
@@ -128,9 +122,6 @@ mix archive.install hex phx_new 1.5.8
 
 # Install Python modules
 # pip install ansible black boto boto3 ipython psycopg2
-
-# Poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
 
 # Go stuff...for DevOps stuff...
 # go get -u github.com/grafana/tanka/cmd/tk
