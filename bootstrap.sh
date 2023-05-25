13f6fd4b027a48a8987bab2b02c3ca8a1c287e42
Gianluca Randazzo <fusillicode@gmail.com>
2023-05-25 08:44:00 +0200

    BIG CLEANUP!!! & switch to cargo install WHENEVER POSSIBLE! <3

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,23 +10,14 @@ brew update
 brew doctor --verbose
 
 # Install Homebrew apps
-# brew install act
-brew install awscli
 brew install ansible
-brew install atuin
-# brew tap codefresh-io/cli && brew install codefresh
+brew install awscli
 brew install cpulimit
-brew install git
 brew install gh
+brew install git
 brew install hadolint
 brew install hashicorp/tap/terraform-ls
-# brew tap heroku/brew && brew install heroku
-# brew install ifstat # This was for the network.sh BitBar script...that I don't use it anymore ¯\_(ツ)_/¯
-# brew install htop
-# brew install imagemagick@6
-brew install jdxcode/tap/rtx
 brew install jq
-# brew install jsonnet # For VSCode Jsonnet extension
 brew install kube-ps1
 brew install kubectx
 brew install kustomize
@@ -34,36 +25,27 @@ brew install lftp
 brew install libpq && brew link libpq --force
 brew install librdkafka # It also installs `lz4`, `lzlib` & `zstd`
 brew install mycli # For Python `mysqlclient`
-# brew install openvpn # It also installs `lzo` & `pkcs11-helper`
-brew install ripgrep
-brew install sd
 brew install stern
-brew install tmux
 brew install txn2/tap/kubefwd
 brew install vegeta
 brew install watch
 brew install yq
 brew install zsh
 
-# Install Homebrew-Cask apps
-brew install homebrew/cask/docker
+# Install Homebrew casks
 brew install android-file-transfer
 brew install appcleaner
 brew install bitbar
-# brew install camunda-modeler
 brew install chromedriver
 brew install discord
 brew install firefox
 brew install google-chrome
-# brew install helm
-# brew install http-toolkit
+brew install helix
+brew install homebrew/cask/docker
+brew install iterm2
 brew install league-of-legends
-# brew install libreoffice
 brew install rectangle
-# brew install sfdx
-# brew install skype
 brew install slack
-# brew install smcfancontrol
 brew install telegram
 brew install the-unarchiver
 brew install transmission --cask
@@ -71,69 +53,41 @@ brew install tunnelblick
 brew install vlc
 brew install whatsapp
 
-# Install docker-slim
-# curl -OL https://downloads.dockerslim.com/releases/1.26.1/dist_mac.zip && unzip dist_mac.zip && mv dist_mac/* ~/bin
-
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install rustup ❤️
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# Rust CLI apps ❤️
-cargo install --force cargo-make
-cargo install sqlx-cli
+# Rust bins ❤️
+cargo install atuin cargo-make ripgrep rtx-cli sd sqlx-cli taplo-cli
 
+# Configure atuin
+echo 'eval "$(atuin init zsh)"' >> ~/.zshrc
 # Configure rtx
-echo 'eval "$(~/bin/rtx activate zsh)"' >> ~/.zshrc
+echo 'eval "$(rtx activate zsh)"' >> ~/.zshrc
 
 # Install rtx plugins
-# brew install bdw-gc libevent llvm && brew link llvm --force # Don't know, seems not needed anymore...¯\_(ツ)_/¯
-# rtx install crystal
 brew install autoconf wxwidgets
 export KERL_CONFIGURE_OPTIONS="--without-javac --with-ssl=$(brew --prefix openssl@1.1)"
 rtx install erlang latest
 rtx install elixir latest
-# rtx install ruby
 rtx install python latest
 rtx install poetry latest
-# rtx install java latest
-# rtx install terraform latest
-
-# Install Ruby gems
-gem install --no-rdoc --no-ri \
-  brakeman bundler-audit bundler cane compass consistency_fail fasterer html2slim license_finder loc_counter rails \
-  rails-audit rails_best_practices rake reek ruby-lint rubocop rubycritic solargraph wordmove
-
-# Install WpScan
-mkdir -p ~/bin/ && ~/bin && git clone https://github.com/wpscanteam/wpscan.git && cd wpscan && bundle install --without test
+rtx install node latest
+rtx install elm latest
 
 # Install Elixir libs
 mix local.hex
 mix local.rebar
 mix archive.install hex phx_new 1.5.8
 
-# Install Erlang tools
-# curl -OL https://github.com/erlanglab/erlangpl/releases/download/0.9.0/erlangpl.tar.gz && \
-# tar -zxvf erlangpl.tar.gz && \
-# rm -rf erlangpl.tar.gz && \
-# mkdir -p ~/bin/ && \
-# mv erlangpl ~/bin
-# chmod +x ~/bin/*
+# Install Python stuff...
+pip install ruff python-lsp-server python-lsp-ruff
 
-# Install Python modules
-# pip install ansible black boto boto3 ipython psycopg2
-
-# Go stuff...for DevOps stuff...
-# go get -u github.com/grafana/tanka/cmd/tk
-# go get -u github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb
-
-# Haskell
-curl https://get-ghcup.haskell.org -sSf | sh
+# Install Node stuff...
+npm install -g @elm-tooling/elm-language-server yaml-language-server yarn
 
 # Upgrade and cleanup brew stuff...
 brew update && brew upgrade && brew cleanup -s && rm -rf ~/Library/Caches/Homebrew/*
 brew doctor
-
-# Twitch miner
-# pip install Twitch-Channel-Points-Miner-v2
