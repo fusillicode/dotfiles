7dcc3d491914952bbbc07ffc012aefa6278872b2
Gianluca Randazzo <fusillicode@gmail.com>
2023-10-07 16:15:25 +0200

    Start trying to tidy up errors

--- bin/weh/src/main.rs
+++ bin/weh/src/main.rs
@@ -3,12 +3,12 @@ mod utils;
 
 fn main() -> anyhow::Result<()> {
     let args = get_args();
-    let (cmd, args) = parse_cmd_and_args(&args);
+    let (cmd, args) = parse_cmd_and_args(&args)?;
 
     match cmd {
         "gh" => cmds::gh::run(args.into_iter()),
         "ho" => cmds::ho::run(args.into_iter()),
-        unexpected_cmd => anyhow::bail!("BOOM {} {:?}", unexpected_cmd, args),
+        unknown_cmd => anyhow::bail!("unknown cmd {} from args {:?}", unknown_cmd, args),
     }
 }
 
@@ -18,8 +18,8 @@ fn get_args() -> Vec<String> {
     x.collect::<Vec<String>>()
 }
 
-fn parse_cmd_and_args(args: &[String]) -> (&str, Vec<&str>) {
+fn parse_cmd_and_args(args: &[String]) -> anyhow::Result<(&str, Vec<&str>)> {
     args.split_first()
         .map(|(cmd, cmd_args)| (cmd.as_str(), cmd_args.iter().map(String::as_str).collect()))
-        .unwrap()
+        .ok_or_else(|| anyhow::anyhow!("cannot parse cmd and args from input args {:?}", args))
 }
