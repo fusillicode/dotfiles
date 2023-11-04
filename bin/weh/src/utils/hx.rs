10b9da6e06bb5a8e4c715efee49ac6e3dcd0f08b
Gianluca Randazzo <fusillicode@gmail.com>
2023-11-04 06:46:04 +0100

    I'm dumb

--- bin/weh/src/utils/hx.rs
+++ bin/weh/src/utils/hx.rs
@@ -1,5 +1,3 @@
-use std::io::BufRead;
-use std::io::Lines;
 use std::ops::Range;
 use std::path::PathBuf;
 use std::str::FromStr;
@@ -67,70 +65,80 @@ impl FromStr for HxCursorPosition {
     }
 }
 
-const ANSI_ESCAPE_SELECTION_BG_COLOR: &str = "[48:2::45:54:64m";
+const ANSI_ESCAPED_SELECTION_BG_COLOR: &str = "[48:2::45:54:64m";
+const ANSI_ESCAPED_BG_COLOR: &str = "[48:2::45:54:64m";
 
-fn get_line_number_from_ansi_escaped_line(ansi_escaped_line: &str) -> anyhow::Result<usize> {
-    strip_ansi_escapes::strip_str(ansi_escaped_line)
-        .split_whitespace()
-        .find_map(|x| x.parse::<usize>().ok())
-        .ok_or_else(|| anyhow!("no line number found in line '{ansi_escaped_line}'"))
+#[derive(Debug)]
+struct HxViewportSelectionLine<'a> {
+    idx: usize,
+    content: &'a str,
 }
 
-fn get_selection_range<B: BufRead>(
-    hx_pane_ansi_escaped_viewport: &[u8],
-    actual_selection: Lines<B>,
+impl<'a> HxViewportSelectionLine<'a> {
+    pub fn next_matching_background_in(
+        mut viewport_lines: std::iter::Enumerate<std::str::Lines<'a>>,
+        ansi_escaped_background_color: &str,
+    ) -> Option<(std::iter::Enumerate<std::str::Lines<'a>>, Self)> {
+        if let Some(viewport_selection_line) = viewport_lines
+            .find_map(|(idx, line)| {
+                line.contains(ansi_escaped_background_color)
+                    .then_some((idx, line))
+            })
+            .map(|(idx, content)| Self { idx, content })
+        {
+            return Some((viewport_lines, viewport_selection_line));
+        }
+        None
+    }
+
+    pub fn nbr_of_lines_from(&self, other: &Self) -> usize {
+        self.idx.abs_diff(other.idx)
+    }
+
+    pub fn get_line_number(&self) -> anyhow::Result<usize> {
+        strip_ansi_escapes::strip_str(self.content)
+            .split_whitespace()
+            .find_map(|x| x.parse::<usize>().ok())
+            .ok_or_else(|| anyhow!("no line number found in line '{}'", self.content))
+    }
+}
+
+fn get_selection_range(
+    hx_pane_ansi_escaped_viewport: &str,
+    hx_cursor_line_number: usize,
+    hx_actual_selection_size: usize,
 ) -> anyhow::Result<Range<usize>> {
-    let hx_pane_ansi_escaped_viewport = String::from_utf8_lossy(hx_pane_ansi_escaped_viewport);
+    if hx_actual_selection_size == 1 {
+        return Ok(hx_cursor_line_number..hx_cursor_line_number);
+    }
 
-    let hx_cursor = HxCursor::from_str(&strip_ansi_escapes::strip_str(
-        hx_pane_ansi_escaped_viewport
-            .lines()
-            .nth_back(2)
-            .ok_or_else(|| {
-                anyhow!(
-                    "no hx status line in pane hx pane ANSI escaped viewport {hx_pane_ansi_escaped_viewport}",
-                )
-            })?,
-    ))?;
-
-    let mut viewport_lines = hx_pane_ansi_escaped_viewport.lines().enumerate();
-    let (viewport_selection_start_line_idx, viewport_selection_start_line) = viewport_lines
-        .find_map(|(idx, line)| {
-            line.contains(ANSI_ESCAPE_SELECTION_BG_COLOR)
-                .then_some((idx, line))
-        })
-        .unwrap();
-    let (viewport_selection_end_line_idx, viewport_selection_end_line) = viewport_lines
-        .find_map(|(idx, line)| {
-            line.contains(ANSI_ESCAPE_SELECTION_BG_COLOR)
-                .then_some((idx, line))
-        })
-        .unwrap();
+    let viewport_lines = hx_pane_ansi_escaped_viewport.lines().enumerate();
+    let (viewport_lines, viewport_selection_line_start) =
+        HxViewportSelectionLine::next_matching_background_in(viewport_lines).unwrap();
+    let (_, viewport_selection_line_end) =
+        HxViewportSelectionLine::next_matching_background_in(viewport_lines).unwrap();
     let viewport_selection_size =
-        viewport_selection_end_line_idx - viewport_selection_start_line_idx;
+        viewport_selection_line_start.nbr_of_lines_from(&viewport_selection_line_end);
 
-    let actual_selection_size = actual_selection.count();
+    dbg!(&viewport_selection_line_start);
+    dbg!(&viewport_selection_line_end);
 
-    // If the actual selection is completely in the viewport
-    if actual_selection_size == viewport_selection_size {
-        let viewport_selection_start_line_number =
-            get_line_number_from_ansi_escaped_line(viewport_selection_start_line).unwrap();
-        let viewport_selection_end_line_number =
-            get_line_number_from_ansi_escaped_line(viewport_selection_end_line).unwrap();
-
-        return Ok(viewport_selection_start_line_number..viewport_selection_end_line_number);
+    // If the actual selection is fully contained in the viewport
+    if hx_actual_selection_size == viewport_selection_size {
+        return Ok(viewport_selection_line_start.get_line_number().unwrap()
+            ..viewport_selection_line_end.get_line_number().unwrap());
     }
 
     // If the actual selection expand before the viewport
-    let selection = if viewport_selection_start_line_idx == 0 {
-        hx_cursor.position.line - actual_selection_size..hx_cursor.position.line
+    let selection = if viewport_selection_line_start.idx == 0 {
+        hx_cursor_line_number - hx_actual_selection_size..hx_cursor_line_number
     // If the actual selection expand after the viewport
     } else {
-        hx_cursor.position.line..hx_cursor.position.line + actual_selection_size
+        hx_cursor_line_number..hx_cursor_line_number + hx_actual_selection_size
     };
 
-    // Sanity check if actual selection is smaller than the viewport selection. This should not happen
-    if actual_selection_size < viewport_selection_size {
+    // Sanity check if actual selection is smaller than the viewport selection. This should not happen.
+    if hx_actual_selection_size < viewport_selection_size {
         bail!("foo")
     }
 
@@ -172,51 +180,75 @@ mod tests {
     }
 
     #[test]
-    fn test_get_line_number_from_ansi_escaped_line_returns_the_expected_line_number() {
-        let result = get_line_number_from_ansi_escaped_line(
-            "[38:2::230:180:80m[48:2::15:20:25m●[39m [38:2::102:102:102m42[39m [38:2::170:217:76m▍[38:2::45:54:64m········[38:2::255:143:64mlet[38:2::45:54:64m·(B[0m[38:2::191:189:182m[48:2::15:20:25mselection_start_line(B[0m[38:2::102:102:102m[48:2::15:20:25m:[38:2::45:54:64m [38:2::102:102:102mi32[38:2::45:54:64m·[38:2::255:143:64m=[38:2::45:54:64m·[38:2::210:166:255m190[38:2::191:189:182m;[38:2::45:54:64m [39m                                                                           [48:2::19:23:33m[K",
-        );
+    fn test_get_line_number_returns_the_expected_line_number() {
+        let result = HxViewportSelectionLine {
+            idx: 0,
+            content: "[38:2::230:180:80m[48:2::15:20:25m●[39m [38:2::102:102:102m42[39m [38:2::170:217:76m▍[38:2::45:54:64m········[38:2::255:143:64mlet[38:2::45:54:64m·(B[0m[38:2::191:189:182m[48:2::15:20:25mselection_start_line(B[0m[38:2::102:102:102m[48:2::15:20:25m:[38:2::45:54:64m [38:2::102:102:102mi32[38:2::45:54:64m·[38:2::255:143:64m=[38:2::45:54:64m·[38:2::210:166:255m190[38:2::191:189:182m;[38:2::45:54:64m [39m                                                                           [48:2::19:23:33m[K"
+        }.get_line_number();
 
         assert_eq!(42, result.unwrap());
     }
 
     #[test]
-    fn test_get_line_number_from_ansi_escaped_line_returns_the_expected_error_if_doenst_find_a_line_number(
-    ) {
-        let result = get_line_number_from_ansi_escaped_line("");
+    fn test_get_line_number_returns_the_expected_error_if_doenst_find_a_line_number() {
+        let result = HxViewportSelectionLine {
+            idx: 0,
+            content: "",
+        }
+        .get_line_number();
 
         assert!(result.is_err());
     }
 
-    // #[test]
-    // fn test_get_selection_range_returns_the_expected_range_if_the_selection_is_completely_contained_in_the_viewport(
-    // ) {
-    //     let result = get_selection_range();
+    #[test]
+    fn test_get_selection_range_returns_the_expected_range_if_the_actual_selection_is_fully_contained_in_the_viewport(
+    ) {
+        let result = get_selection_range(
+            &std::fs::read_to_string("./fixtures/actual_selection_fully_contained_in_viewport.txt")
+                .unwrap(),
+            113,
+            7,
+        );
 
-    //     assert_eq!((1..2), result.unwrap());
-    // }
+        assert_eq!((108..113), result.unwrap());
+    }
 
-    // #[test]
-    // fn test_get_selection_range_returns_the_expected_range_if_the_selection_expand_before_the_viewport(
-    // ) {
-    //     let result = get_selection_range();
+    #[test]
+    fn test_get_selection_range_returns_the_expected_range_if_the_actual_selection_expands_before_the_viewport(
+    ) {
+        let result = get_selection_range(
+            &std::fs::read_to_string("./fixtures/actual_selection_expands_before_the_viewport.txt")
+                .unwrap(),
+            3,
+            3,
+        );
 
-    //     assert_eq!((1..2), result.unwrap());
-    // }
+        assert_eq!((1..2), result.unwrap());
+    }
 
-    // #[test]
-    // fn test_get_selection_range_returns_the_expected_range_if_the_selection_expand_after_the_viewport(
-    // ) {
-    //     let result = get_selection_range();
+    #[test]
+    fn test_get_selection_range_returns_the_expected_range_if_the_actual_selection_expands_after_the_viewport(
+    ) {
+        let result = get_selection_range(
+            &std::fs::read_to_string("./fixtures/actual_selection_expands_after_the_viewport.txt")
+                .unwrap(),
+            3,
+            3,
+        );
 
-    //     assert_eq!((1..2), result.unwrap());
-    // }
+        assert_eq!((1..2), result.unwrap());
+    }
 
-    // #[test]
-    // fn test_get_selection_range_returns_the_expected_error_if_the_selection_is_completely_contained_in_the_viewport_and_is_smaller_than_the_actual_selection(
-    // ) {
-    //     let result = get_selection_range();
+    #[test]
+    fn test_get_selection_range_returns_the_expected_error_if_the_actual_selection_is_fully_contained_in_the_viewport_but_the_viewport_is_smaller_than_the_actual_selection(
+    ) {
+        let result = get_selection_range(
+            &std::fs::read_to_string("./fixtures/actual_selection_fully_contained_in_viewport.txt")
+                .unwrap(),
+            3,
+            3,
+        );
 
-    //     assert_eq!((1..2), result.unwrap());
-    // }
+        assert_eq!((1..2), result.unwrap());
+    }
 }
