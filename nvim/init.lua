316aa50fd22d5581da21624f8a2fb2db9addffc8
Gianluca Randazzo <fusillicode@gmail.com>
2023-11-15 06:58:41 +0100

    This is the real LSP ALL symbols picker

--- nvim/init.lua
+++ nvim/init.lua
@@ -198,7 +198,7 @@ vim.keymap.set('n', '<leader>gs', require 'telescope.builtin'.git_status)
 vim.keymap.set('n', '<leader>d', function() require 'telescope.builtin'.diagnostics({ bufnr = 0 }) end)
 vim.keymap.set('n', '<leader>D', require 'telescope.builtin'.diagnostics)
 vim.keymap.set('n', '<leader>s', require 'telescope.builtin'.lsp_document_symbols)
-vim.keymap.set('n', '<leader>S', require 'telescope.builtin'.lsp_workspace_symbols)
+vim.keymap.set('n', '<leader>S', require 'telescope.builtin'.lsp_dynamic_workspace_symbols)
 vim.keymap.set('n', '<leader>t', ':TodoTelescope<CR>')
 vim.keymap.set('n', '<leader>z', vim.diagnostic.open_float)
 
