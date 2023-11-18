c50961992799a1fd0da383d9155c0cd3ff7252a4
Gianluca Randazzo <fusillicode@gmail.com>
2023-11-18 15:38:31 +0100

    Bye keymaps see you in core

--- nvim/lua/core.lua
+++ nvim/lua/core.lua
@@ -39,6 +39,56 @@ vopt.shortmess:append('sI')
 vwo.number = true
 vwo.signcolumn = 'yes'
 
+vim.keymap.set('', 'gn', ':bn<CR>')
+vim.keymap.set('', 'gp', ':bp<CR>')
+vim.keymap.set('', 'ga', '<C-^>')
+vim.keymap.set({ 'n', 'v' }, 'gh', '0')
+vim.keymap.set({ 'n', 'v' }, 'gl', '$')
+vim.keymap.set({ 'n', 'v' }, 'gs', '_')
+vim.keymap.set({ 'n', 'v' }, 'mm', '%', { remap = true })
+
+vim.keymap.set('v', '>', '>gv')
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('n', '>', '>>')
+vim.keymap.set('n', '<', '<<')
+vim.keymap.set({ 'n', 'v' }, 'U', '<C-r>')
+
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-o>', '<C-o>zz')
+vim.keymap.set('n', '<C-i>', '<C-i>zz')
+vim.keymap.set('n', '<C-j>', '<C-Down>', { remap = true })
+vim.keymap.set('n', '<C-k>', '<C-Up>', { remap = true })
+
+vim.keymap.set({ 'n', 'v' }, '<leader><leader>', ':w!<CR>')
+vim.keymap.set({ 'n', 'v' }, '<leader>x', ':bd<CR>')
+vim.keymap.set({ 'n', 'v' }, '<leader>X', ':bd!<CR>')
+vim.keymap.set({ 'n', 'v' }, '<leader>w', ':wa<CR>')
+vim.keymap.set({ 'n', 'v' }, '<leader>W', ':wa!<CR>')
+vim.keymap.set({ 'n', 'v' }, '<leader>q', ':q<CR>')
+vim.keymap.set({ 'n', 'v' }, '<leader>Q', ':q!<CR>')
+vim.keymap.set('n', '<esc>', ':noh<CR>')
+
+vim.keymap.set('n', 'dp', vim.diagnostic.goto_prev)
+vim.keymap.set('n', 'dn', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+
+vim.diagnostic.config {
+  float = {
+    border = 'single',
+    focusable = false,
+    header = '',
+    prefix = '',
+    source = 'always',
+    style = 'minimal',
+  },
+  severity_sort = true,
+  signs = true,
+  underline = false,
+  update_in_insert = true,
+  virtual_text = true
+}
+
 vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = 'YankHighlight',
