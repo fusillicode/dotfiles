ef10e20492fe85c3c0d90516f632b06d163215c6
Gianluca Randazzo <fusillicode@gmail.com>
2023-11-20 18:01:28 +0100

    OMG Lua you're so ugly...

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -16,35 +16,45 @@ return {
     local telescope = require('telescope')
     local telescope_builtin = require('telescope.builtin')
     local vkeyset = vim.keymap.set
-    local custom_preview = {
+    local custom_theme_opts = {
       show_line = false,
+      prompt_title = false,
       results_title = false,
       preview_title = false,
     }
+    local function custom_theme(picker, opts)
+      return function()
+        telescope_builtin[picker](vim.tbl_extend('force', custom_theme_opts, opts or {}))
+      end
+    end
 
-    vkeyset('n', '<leader>b', function() telescope_builtin.buffers(custom_preview) end)
-    vkeyset('n', '<leader>f', function() telescope_builtin.find_files(custom_preview) end)
-    vkeyset('n', '<leader>j', function() telescope_builtin.jumplist(custom_preview) end)
-    vkeyset('n', '<leader>c', function() telescope_builtin.git_commits(custom_preview) end)
-    vkeyset('n', '<leader>bc',
-      function() telescope_builtin.git_bcommits(vim.tbl_extend('force', custom_preview, { bufnr = 0 })) end)
-    vkeyset('n', '<leader>gb', function() telescope_builtin.git_branches(custom_preview) end)
-    vkeyset('n', '<leader>gs', function() telescope_builtin.git_status(custom_preview) end)
-    vkeyset('n', '<leader>d',
-      function() telescope_builtin.diagnostics(vim.tbl_extend('force', custom_preview, { bufnr = 0 })) end)
-    vkeyset('n', '<leader>D', function() telescope_builtin.diagnostics(custom_preview) end)
-    vkeyset('n', '<leader>s', function() telescope_builtin.lsp_document_symbols(custom_preview) end)
-    vkeyset('n', '<leader>S', function() telescope_builtin.lsp_dynamic_workspace_symbols(custom_preview) end)
-    vkeyset('n', 'gd', function() telescope_builtin.lsp_definitions(custom_preview) end)
-    vkeyset('n', 'gr', function() telescope_builtin.lsp_references(custom_preview) end)
-    vkeyset('n', 'gi', function() telescope_builtin.lsp_implementations(custom_preview) end)
-    vkeyset('n', '<leader>l', function() telescope.extensions.live_grep_args.live_grep_args(custom_preview) end)
+    vkeyset('n', 'gd', custom_theme('lsp_definitions', { prompt_prefix = 'LSP Defs > ' }))
+    vkeyset('n', 'gr', custom_theme('lsp_references', { prompt_prefix = 'LSP Refs > ' }))
+    vkeyset('n', 'gi', custom_theme('lsp_implementations', { prompt_prefix = 'LSP Impls > ' }))
+    vkeyset('n', '<leader>s', custom_theme('lsp_document_symbols', { prompt_prefix = 'LSP Symbols Buffer > ' }))
+    vkeyset('n', '<leader>S',
+      custom_theme('lsp_dynamic_workspace_symbols', { prompt_prefix = 'LSP Symbols Workspace > ' }))
+    vkeyset('n', '<leader>b', custom_theme('buffers', { prompt_prefix = 'Buffers > ' }))
+    vkeyset('n', '<leader>f', custom_theme('find_files', { prompt_prefix = 'Files > ' }))
+    vkeyset('n', '<leader>j', custom_theme('jumplist', { prompt_prefix = 'Jumps > ' }))
+    vkeyset('n', '<leader>c', custom_theme('git_commits', { prompt_prefix = 'Git Commits > ' }))
+    vkeyset('n', '<leader>bc', custom_theme('git_bcommits', { prompt_prefix = ' Git Commits Buffer >', bufnr = 0 }))
+    vkeyset('n', '<leader>gb', custom_theme('git_branches', { prompt_prefix = 'Git Branches > ' }))
+    vkeyset('n', '<leader>gs', custom_theme('git_status', { prompt_prefix = 'Git Status > ' }))
+    vkeyset('n', '<leader>d', custom_theme('diagnostics', { prompt_prefix = 'Diagnostics Buffer >', bufnr = 0 }))
+    vkeyset('n', '<leader>D', custom_theme('diagnostics', { prompt_prefix = 'Diagnostics Workspace > ' }))
+    vkeyset('n', '<leader>l',
+      function()
+        telescope.extensions.live_grep_args.live_grep_args(
+          vim.tbl_extend('force', custom_theme_opts, { prompt_prefix = 'rg > ' })
+        )
+      end)
     vkeyset('n', '<leader>F', ':Telescope file_browser path=%:p:h select_buffer=true<CR>')
     vkeyset('n', '<leader>t', ':TodoTelescope<CR>')
 
     local file_browser_actions = require('telescope._extensions.file_browser.actions')
     telescope.setup({
-      defaults = require('telescope.themes').get_dropdown({}),
+      defaults = vim.tbl_extend('force', require('telescope.themes').get_dropdown(), custom_theme_opts),
       extensions = {
         file_browser = {
           dir_icon = '',
