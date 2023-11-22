1cf62e6a599df23c8e24754d40565fa5ce20147b
Gianluca Randazzo <fusillicode@gmail.com>
2023-11-22 19:50:56 +0100

    Less is more

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -18,13 +18,10 @@ return {
         return '<Ignore>'
       end, { expr = true, })
 
-      vkeyset('n', '<leader>hs', gs.stage_hunk)
-      vkeyset('n', '<leader>hr', gs.reset_hunk)
       vkeyset('v', '<leader>hs', function() gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v'), }) end)
       vkeyset('v', '<leader>hr', function() gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v'), }) end)
       vkeyset('n', '<leader>hu', gs.undo_stage_hunk)
-      vkeyset('n', '<leader>tb', gs.toggle_current_line_blame)
-      vkeyset('n', '<leader>td', gs.toggle_deleted)
+      vkeyset('n', '<leader>tb', function() gs.blame_line({ full = true, }) end)
     end,
   },
 }
