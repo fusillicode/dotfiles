d642363f6276195000f6476f01c8ae1e036d6824
Gianluca Randazzo <fusillicode@gmail.com>
2023-11-23 11:14:56 +0100

    MAYYBE...

--- nvim/lua/plugins/gitlinker.lua
+++ nvim/lua/plugins/gitlinker.lua
@@ -1,6 +1,6 @@
 return {
   'ruifm/gitlinker.nvim',
-  keys = { '<leader>gy', mode = { 'n', 'v', }, },
+  keys = { '<leader>yy', mode = { 'n', 'v', }, },
   dependencies = { 'nvim-lua/plenary.nvim', },
   config = true,
 }
