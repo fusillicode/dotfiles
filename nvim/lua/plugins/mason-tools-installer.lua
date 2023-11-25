3e86a89125558eca8505fa120f2d1f26f8493519
Gianluca Randazzo <fusillicode@gmail.com>
2023-11-25 11:08:06 +0100

    OMG this is so bad...

--- nvim/lua/plugins/mason-tools-installer.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'WhoIsSethDaniel/mason-tool-installer.nvim',
-  cmd = { 'MasonToolsInstall', 'MasonToolsUpdate', 'MasonToolsClean', },
-  config = function()
-    require('mason-tool-installer').setup({
-      ensure_installed = vim.tbl_keys(require('../lsps')),
-    })
-  end,
-}
