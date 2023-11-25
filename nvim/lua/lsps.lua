124467fd82e3276969c2bf2498687d103bfd9d83
Gianluca Randazzo <fusillicode@gmail.com>
2023-11-25 11:08:15 +0100

    Revert "OMG this is so bad..."
    This reverts commit 3e86a89125558eca8505fa120f2d1f26f8493519.

---
+++ nvim/lua/lsps.lua
@@ -0,0 +1,51 @@
+return {
+  bashls = {},
+  docker_compose_language_service = {},
+  dockerls = {},
+  dotls = {},
+  graphql = {},
+  html = {},
+  helm_ls = {},
+  jsonls = {},
+  lua_ls = {
+    Lua = {
+      completion = {
+        callSnippet = 'Both',
+        callKeyword = 'Both',
+      },
+      format = {
+        defaultConfig = {
+          quote_style = 'single',
+          trailing_table_separator = 'always',
+          insert_final_newline = 'true',
+        },
+      },
+      hint = { enable = true, setType = true, },
+      diagnostics = { globals = { 'vim', }, },
+      telemetry = { enable = false, },
+      workspace = { checkThirdParty = false, },
+    },
+  },
+  marksman = {},
+  ruff_lsp = {},
+  rust_analyzer = {
+    ['rust-analyzer'] = {
+      cargo = {
+        build_script = { enable = true, },
+        extraArgs = { '--profile', 'rust-analyzer', },
+        extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = 'dev', },
+      },
+      check = { command = 'clippy', },
+      checkOnSave = { command = 'clippy', },
+      completion = { autoimport = { enable = true, }, },
+      imports = { enforce = true, granularity = { group = 'item', }, prefix = 'crate', },
+      lens = { debug = { enable = false, }, implementations = { enable = false, }, run = { enable = false, }, },
+      proc_macro = { enable = true, },
+      showUnlinkedFileNotification = false,
+    },
+  },
+  sqlls = {},
+  taplo = {},
+  tsserver = {},
+  yamlls = {},
+}
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -17,7 +17,7 @@ return {
       vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, { buffer = bufnr, })
     end
 
-    for lsp, settings in pairs(require('../mason-tools')['lsps']) do
+    for lsp, settings in pairs(require('../lsps')) do
       lspconfig[lsp].setup({
         capabilities = capabilities,
         on_attach = on_attach,
