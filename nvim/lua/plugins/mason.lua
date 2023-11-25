23b0c514b9e98eaa9f24299c58b85f4e677621e2
Gianluca Randazzo <fusillicode@gmail.com>
2023-11-25 14:28:21 +0100

    IS THAT IT!?

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -4,17 +4,26 @@ return {
   dependencies = { 'williamboman/mason-lspconfig.nvim', },
   config = function()
     require('mason').setup({})
-    local server_mapping = require('mason-lspconfig.mappings.server')
-    local registry = require('mason-registry')
 
-    for _, pkg_name in pairs(vim.tbl_keys(require('../lsps'))) do
-      local registry_name = server_mapping.lspconfig_to_package[pkg_name] or pkg_name
-      local ok, pkg = pcall(registry.get_package, registry_name)
+    local lspconfig_mappings_server = require('mason-lspconfig.mappings.server')
+    local mason_registry = require('mason-registry')
+    local mason_tools = require('../mason-tools')
+
+    local function install_mason_package(package_registry_name)
+      local ok, pkg = pcall(mason_registry.get_package, package_registry_name)
       if ok then
         if not pkg:is_installed() then
           pkg:install()
         end
       end
+    end
+
+    for lspconfig_name, _ in pairs(mason_tools['lsps']) do
+      install_mason_package(lspconfig_mappings_server.lspconfig_to_package[lspconfig_name])
+    end
+
+    for mason_tool_name, _ in pairs(mason_tools['others']) do
+      install_mason_package(mason_tool_name)
     end
   end,
 }
