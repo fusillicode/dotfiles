2d9c8e54b26850db4f8e0b5e20b684e4b6bfb21d
Gian Lu <fusillicode@users.noreply.github.com>
2023-11-25 14:30:19 +0100

    [20]: Oh boy here we go againnvim (#21)

---
+++ nvim/lua/core.lua
@@ -0,0 +1,106 @@
+local vg = vim.g
+vg.mapleader = ' '
+vg.maplocalleader = ' '
+
+for _, provider in ipairs { 'node', 'perl', 'python3', 'ruby', } do
+  vg['loaded_' .. provider .. '_provider'] = 0
+end
+
+local vo = vim.o
+vo.autoindent = true
+vo.backspace = 'indent,eol,start'
+vo.breakindent = true
+vo.colorcolumn = '120'
+vo.completeopt = 'menuone,noselect'
+vo.cursorline = true
+vo.expandtab = true
+vo.hlsearch = true
+vo.ignorecase = true
+vo.list = true
+vo.mouse = 'a'
+vo.showmode = false
+vo.number = true
+vo.shiftwidth = 2
+vo.sidescroll = 1
+vo.signcolumn = 'yes'
+vo.smartcase = true
+vo.splitbelow = true
+vo.splitright = true
+vo.tabstop = 2
+vo.termguicolors = true
+vo.undofile = true
+vo.updatetime = 250
+vo.wrap = false
+
+local vopt = vim.opt
+vopt.clipboard:append('unnamedplus')
+vopt.iskeyword:append('-')
+vopt.shortmess:append('sI')
+
+local vwo = vim.wo
+vwo.number = true
+vwo.signcolumn = 'yes'
+
+local vkeyset = vim.keymap.set
+vkeyset('', 'gn', ':bn<CR>')
+vkeyset('', 'gp', ':bp<CR>')
+vkeyset('', 'ga', '<C-^>')
+vkeyset({ 'n', 'v', }, 'gh', '0')
+vkeyset({ 'n', 'v', }, 'gl', '$')
+vkeyset({ 'n', 'v', }, 'gs', '_')
+vkeyset({ 'n', 'v', }, 'mm', '%', { remap = true, })
+
+vkeyset('v', 'p', '"_dP')
+vkeyset('v', '>', '>gv')
+vkeyset('v', '<', '<gv')
+vkeyset('n', '>', '>>')
+vkeyset('n', '<', '<<')
+vkeyset({ 'n', 'v', }, 'U', '<C-r>')
+
+vkeyset('n', '<C-u>', '<C-u>zz')
+vkeyset('n', '<C-d>', '<C-d>zz')
+vkeyset('n', '<C-o>', '<C-o>zz')
+vkeyset('n', '<C-i>', '<C-i>zz')
+vkeyset('n', '<C-j>', '<C-Down>', { remap = true, })
+vkeyset('n', '<C-k>', '<C-Up>', { remap = true, })
+
+vkeyset({ 'n', 'v', }, '<leader><leader>', ':w!<CR>')
+vkeyset({ 'n', 'v', }, '<leader>x', ':bd<CR>')
+vkeyset({ 'n', 'v', }, '<leader>X', ':bd!<CR>')
+vkeyset({ 'n', 'v', }, '<leader>w', ':wa<CR>')
+vkeyset({ 'n', 'v', }, '<leader>W', ':wa!<CR>')
+vkeyset({ 'n', 'v', }, '<leader>q', ':q<CR>')
+vkeyset({ 'n', 'v', }, '<leader>Q', ':q!<CR>')
+vkeyset('n', '<esc>', ':noh<CR>')
+
+local vdiag = vim.diagnostic
+vkeyset('n', 'dp', vdiag.goto_prev)
+vkeyset('n', 'dn', vdiag.goto_next)
+vkeyset('n', '<leader>e', vdiag.open_float)
+
+vdiag.config {
+  float = {
+    border = 'single',
+    focusable = false,
+    header = '',
+    prefix = '',
+    source = 'always',
+    style = 'minimal',
+  },
+  severity_sort = true,
+  signs = true,
+  underline = false,
+  update_in_insert = true,
+  virtual_text = true,
+}
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('YankHighlight', { clear = true, }),
+  pattern = '*',
+  callback = function() vim.highlight.on_yank() end,
+})
+
+vim.api.nvim_create_autocmd('FocusLost', {
+  group = vim.api.nvim_create_augroup('AutosaveBuffer', { clear = true, }),
+  command = ':silent! wa!',
+})
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,92 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  keys = { '<leader>', 'gd', 'gr', 'gi', },
+  branch = 'master',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-live-grep-args.nvim',
+    'nvim-telescope/telescope-file-browser.nvim',
+    'fdschmidt93/telescope-egrepify.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      cond = function() return vim.fn.executable 'make' == 1 end,
+    },
+  },
+  config = function()
+    local telescope = require('telescope')
+    local telescope_builtin = require('telescope.builtin')
+    local telescope_actions = require('telescope.actions')
+    local vkeyset = vim.keymap.set
+    local my_default = {
+      show_line = false,
+      prompt_title = false,
+      results_title = false,
+      preview_title = false,
+      mappings = {
+        i = {
+          ['<esc>'] = telescope_actions.close,
+        },
+      },
+    }
+    local function with_my_default(picker, opts)
+      return function()
+        telescope_builtin[picker](vim.tbl_extend('force', my_default, opts or {}))
+      end
+    end
+
+    vkeyset('n', 'gd', with_my_default('lsp_definitions', { prompt_prefix = 'LSP Def: ', }))
+    vkeyset('n', 'gr', with_my_default('lsp_references', { prompt_prefix = 'LSP Ref: ', }))
+    vkeyset('n', 'gi', with_my_default('lsp_implementations', { prompt_prefix = 'LSP Impl: ', }))
+    vkeyset('n', '<leader>s', with_my_default('lsp_document_symbols', { prompt_prefix = 'LSP Symbol: ', }))
+    vkeyset('n', '<leader>S',
+      with_my_default('lsp_dynamic_workspace_symbols', { prompt_prefix = 'LSP Symbol Workspace: ', }))
+    vkeyset('n', '<leader>b', with_my_default('buffers', { prompt_prefix = 'Buffer: ', }))
+    vkeyset('n', '<leader>f', with_my_default('find_files', { prompt_prefix = 'File: ', }))
+    vkeyset('n', '<leader>j', with_my_default('jumplist', { prompt_prefix = 'Jump: ', }))
+    vkeyset('n', '<leader>gc', with_my_default('git_commits', { prompt_prefix = 'Git Commit: ', }))
+    vkeyset('n', '<leader>gbb', with_my_default('git_bcommits', { prompt_prefix = ' Git Commit Buffer >', bufnr = 0, }))
+    vkeyset('n', '<leader>gb', with_my_default('git_branches', { prompt_prefix = 'Git Branch: ', }))
+    vkeyset('n', '<leader>gs', with_my_default('git_status', { prompt_prefix = 'Git Status: ', }))
+    vkeyset('n', '<leader>d', with_my_default('diagnostics', { prompt_prefix = 'Diagnostic: ', bufnr = 0, }))
+    vkeyset('n', '<leader>D', with_my_default('diagnostics', { prompt_prefix = 'Diagnostic Workspace: ', }))
+    vkeyset('n', '<leader>h', with_my_default('help_tags', { prompt_prefix = 'Help tag: ', }))
+    vkeyset('n', '<leader>/', function()
+      telescope.extensions.egrepify.egrepify(vim.tbl_extend('force', my_default, { prompt_prefix = 'rg: ', }))
+    end)
+    vkeyset('n', '<leader>F', function()
+      telescope.extensions.file_browser.file_browser(
+        vim.tbl_extend('force', my_default, { prompt_prefix = 'Dir/File: ', path = '%:p:h', select_buffer = true, })
+      )
+    end)
+    vkeyset('n', '<leader>T', ':TodoTelescope<CR>')
+
+    local file_browser_actions = require('telescope._extensions.file_browser.actions')
+    telescope.setup({
+      defaults = vim.tbl_extend('force', require('telescope.themes').get_dropdown(), my_default),
+      extensions = {
+        file_browser = {
+          dir_icon = '',
+          grouped = true,
+          hidden = { file_browser = true, folder_browser = true, },
+          hide_parent_dir = true,
+          hijack_netrw = true,
+          mappings = {
+            ['i'] = {
+              ['<C-h>'] = file_browser_actions.goto_parent_dir,
+            },
+          },
+        },
+      },
+      pickers = {
+        find_files = {
+          find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*', },
+        },
+      },
+    })
+
+    telescope.load_extension('fzf')
+    telescope.load_extension('file_browser')
+    telescope.load_extension('egrepify')
+  end,
+}
