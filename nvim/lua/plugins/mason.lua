b2eef8334d81d63db902aacd857ef6bedcdd0466
Gianluca Randazzo <fusillicode@gmail.com>
2023-11-25 12:46:19 +0100

    SOMETHING IS WORKING!?!?

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,6 +1,20 @@
 return {
   'williamboman/mason.nvim',
   cmd = 'Mason',
-  build = ':MasonUpdate',
-  config = true,
+  dependencies = { 'williamboman/mason-lspconfig.nvim', },
+  config = function()
+    require('mason').setup({})
+    local server_mapping = require('mason-lspconfig.mappings.server')
+    local registry = require('mason-registry')
+
+    for _, pkg_name in pairs(vim.tbl_keys(require('../lsps'))) do
+      local registry_name = server_mapping.lspconfig_to_package[pkg_name] or pkg_name
+      local ok, pkg = pcall(registry.get_package, registry_name)
+      if ok then
+        if not pkg:is_installed() then
+          pkg:install()
+        end
+      end
+    end
+  end,
 }
