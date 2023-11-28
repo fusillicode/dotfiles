dfeb5b20d5099486f1c1b53c0546ac4f035bd456
Gianluca Randazzo <fusillicode@gmail.com>
2023-11-28 09:28:06 +0100

    Re-add missing stage & reset hunk in normal mode

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -20,6 +20,8 @@ return {
         return '<Ignore>'
       end, { expr = true, })
 
+      vkeyset('n', '<leader>hs', gs.stage_hunk)
+      vkeyset('n', '<leader>hr', gs.reset_hunk)
       vkeyset('v', '<leader>hs', function() gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v'), }) end)
       vkeyset('v', '<leader>hr', function() gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v'), }) end)
       vkeyset('n', '<leader>hu', gs.undo_stage_hunk)
