98a0f8e534d21c436e1b5b1e1bbb97f458b9153d
fusillicode <2248534+fusillicode@users.noreply.github.com>
2023-11-29 18:05:05 +0100

    TYPOS IS WORKING!!! https://github.com/tekumara/typos-vscode/issues/17\#issuecomment-1832292795

--- nvim/lua/mason-tools.lua
+++ nvim/lua/mason-tools.lua
@@ -8,40 +8,44 @@ return {
     helm_ls = {},
     jsonls = {},
     lua_ls = {
-      Lua = {
-        completion = {
-          callSnippet = 'Both',
-          callKeyword = 'Both',
-        },
-        format = {
-          defaultConfig = {
-            quote_style = 'single',
-            trailing_table_separator = 'always',
-            insert_final_newline = 'true',
+      settings = {
+        Lua = {
+          completion = {
+            callSnippet = 'Both',
+            callKeyword = 'Both',
           },
+          format = {
+            defaultConfig = {
+              quote_style = 'single',
+              trailing_table_separator = 'always',
+              insert_final_newline = 'true',
+            },
+          },
+          hint = { enable = true, setType = true, },
+          diagnostics = { globals = { 'vim', }, },
+          telemetry = { enable = false, },
+          workspace = { checkThirdParty = false, },
         },
-        hint = { enable = true, setType = true, },
-        diagnostics = { globals = { 'vim', }, },
-        telemetry = { enable = false, },
-        workspace = { checkThirdParty = false, },
       },
     },
     marksman = {},
     ruff_lsp = {},
     rust_analyzer = {
-      ['rust-analyzer'] = {
-        cargo = {
-          build_script = { enable = true, },
-          extraArgs = { '--profile', 'rust-analyzer', },
-          extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = 'dev', },
+      settings = {
+        ['rust-analyzer'] = {
+          cargo = {
+            build_script = { enable = true, },
+            extraArgs = { '--profile', 'rust-analyzer', },
+            extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = 'dev', },
+          },
+          check = { command = 'clippy', },
+          checkOnSave = { command = 'clippy', },
+          completion = { autoimport = { enable = true, }, },
+          imports = { enforce = true, granularity = { group = 'item', }, prefix = 'crate', },
+          lens = { debug = { enable = false, }, implementations = { enable = false, }, run = { enable = false, }, },
+          proc_macro = { enable = true, },
+          showUnlinkedFileNotification = false,
         },
-        check = { command = 'clippy', },
-        checkOnSave = { command = 'clippy', },
-        completion = { autoimport = { enable = true, }, },
-        imports = { enforce = true, granularity = { group = 'item', }, prefix = 'crate', },
-        lens = { debug = { enable = false, }, implementations = { enable = false, }, run = { enable = false, }, },
-        proc_macro = { enable = true, },
-        showUnlinkedFileNotification = false,
       },
     },
     sqlls = {},
