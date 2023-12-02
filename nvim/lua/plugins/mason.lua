fbcb9a535e9ba123721cfbe7bb35ae1ad31e4659
Gianluca Randazzo <fusillicode@gmail.com>
2023-12-02 17:43:29 +0100

    Was it really that productive with just 4 hours of sleep?

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,3 +1,52 @@
+---@diagnostic disable-next-line: unused-local, unused-function
+local function dbg(foo)
+  print(vim.inspect(foo))
+  return foo
+end
+
+local function set_diff(s1, s2)
+  local diff = {}
+  for k, v in pairs(s1) do if s2[k] == nil then diff[k] = v end end
+  return diff
+end
+
+local function new_set(table)
+  local set = {}
+  for _, v in ipairs(table) do set[v] = true end
+  return set
+end
+
+local function get_mason_pkg(mason_registry, pkg_name)
+  local ok, pkg = pcall(mason_registry.get_package, pkg_name)
+
+  if not ok then
+    print('Error getting pkg ' .. pkg_name .. ' from mason-registry')
+    return
+  end
+
+  return pkg
+end
+
+local function install_mason_pkg(pkg)
+  if pkg:is_installed() then
+    print(pkg.name .. ' already installed')
+    return
+  end
+  print(pkg.name .. ' installing')
+  pkg:install()
+  print(pkg.name .. ' installed')
+end
+
+local function uninstall_mason_pkg(pkg)
+  if not pkg:is_installed() then
+    print(pkg.name .. ' already not installed')
+    return
+  end
+  print(pkg.name .. ' uninstalling')
+  pkg:uninstall()
+  print(pkg.name .. ' uninstalled')
+end
+
 return {
   'williamboman/mason.nvim',
   cmd = 'Mason',
@@ -9,27 +58,22 @@ return {
     local mason_registry = require('mason-registry')
     local mason_tools = require('../mason-tools')
 
-    local function install_mason_pkg(pkg_name)
-      local ok, pkg = pcall(mason_registry.get_package, pkg_name)
-
-      if not ok then
-        print('Error getting pkg ' .. pkg_name .. ' from mason-registry')
-        return
-      end
-
-      if not pkg:is_installed() then
-        print(pkg_name .. ' installing')
-        pkg:install()
-        print(pkg_name .. ' installed')
-      end
+    local pkgs_names = {}
+    for mason_tool_name, _ in pairs(vim.tbl_extend('error', mason_tools['lsps'], mason_tools['others'])) do
+      local pkg_name = lspconfig_mappings_server.lspconfig_to_package[mason_tool_name] or mason_tool_name
+      pkgs_names[pkg_name] = true
     end
 
-    for lspconfig_name, _ in pairs(mason_tools['lsps']) do
-      install_mason_pkg(lspconfig_mappings_server.lspconfig_to_package[lspconfig_name])
+    local installed_pkgs_names = new_set(mason_registry.get_installed_package_names())
+
+    for pkg_name_to_install in pairs(set_diff(pkgs_names, installed_pkgs_names)) do
+      local pkg = get_mason_pkg(mason_registry, pkg_name_to_install)
+      if pkg then install_mason_pkg(pkg) end
     end
 
-    for mason_tool_name, _ in pairs(mason_tools['others']) do
-      install_mason_pkg(mason_tool_name)
+    for pkg_name_to_uninstall in pairs(set_diff(installed_pkgs_names, pkgs_names)) do
+      local pkg = get_mason_pkg(mason_registry, pkg_name_to_uninstall)
+      if pkg then uninstall_mason_pkg(pkg) end
     end
   end,
 }
