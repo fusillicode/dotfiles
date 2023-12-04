8dba3bb9179cb0576252bbdc8104ecde42ade1a3
Gianluca Randazzo <fusillicode@gmail.com>
2023-12-04 10:13:04 +0000

    Goodbye telescope-file-browser, welcome Oil <3

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,25 @@
+return {
+  'stevearc/oil.nvim',
+  config = function()
+    local vkeyset = vim.keymap.set
+    vkeyset('n', '<leader>F', '<CMD>Oil --float<CR>')
+
+    require('oil').setup({
+      delete_to_trash = true,
+      view_options = {
+        show_hidden = true,
+      },
+      float = {
+        padding = 2,
+        max_width = 100,
+        max_height = 30,
+        override = function(conf)
+          return vim.tbl_extend('force', conf, { anchor = 'SW', })
+        end,
+      },
+      keymaps = {
+        ['<esc>'] = ':bd<CR>',
+      },
+    })
+  end,
+}
