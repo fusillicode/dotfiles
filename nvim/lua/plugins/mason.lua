8622b06c9720f7fe866656d6f94b0ae37aa5338d
fusillicode <2248534+fusillicode@users.noreply.github.com>
2023-12-07 06:18:04 +0000

    LESS IS MORE PART 2

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -2,7 +2,5 @@ return {
   'williamboman/mason.nvim',
   cmd = 'Mason',
   dependencies = { 'williamboman/mason-lspconfig.nvim', },
-  config = function()
-    require('mason').setup({})
-  end,
+  config = true,
 }
