f02d41292441946196be648a24396641df42c33d
fusillicode <2248534+fusillicode@users.noreply.github.com>
2023-12-09 21:03:14 +0000

    Revert "MINIMAL AF"
    This reverts commit 4da5f20c58e77dffca35f554ef3b61e147d6d1aa.

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,51 @@
+return {
+  'EdenEast/nightfox.nvim',
+  priority = 1000,
+  config = function()
+    local nordfox_light_bg = '#242830'
+
+    require('nightfox').setup({
+      options = {
+        color_blind = {
+          enable = true,
+        },
+      },
+      groups = {
+        all = {
+          ColorColumn              = { bg = nordfox_light_bg, },
+          CursorLine               = { bg = nordfox_light_bg, },
+          CursorLineNr             = { fg = 'white', style = 'bold', },
+          DiagnosticError          = { fg = 'red', },
+          DiagnosticHint           = { fg = 'aqua', },
+          DiagnosticInfo           = { fg = 'white', },
+          DiagnosticOk             = { fg = 'limegreen', },
+          DiagnosticWarn           = { fg = 'orange', },
+          DiagnosticFloatingError  = { fg = 'red', bg = nordfox_light_bg, },
+          DiagnosticFloatingHint   = { fg = 'aqua', bg = nordfox_light_bg, },
+          DiagnosticFloatingInfo   = { fg = 'white', bg = nordfox_light_bg, },
+          DiagnosticFloatingOk     = { fg = 'limegreen', bg = nordfox_light_bg, },
+          DiagnosticFloatingWarn   = { fg = 'orange', bg = nordfox_light_bg, },
+          DiagnosticUnderlineError = { sp = 'red', },
+          DiagnosticUnderlineHint  = { sp = 'aqua', },
+          DiagnosticUnderlineInfo  = { sp = 'white', },
+          DiagnosticUnderlineOk    = { sp = 'limegreen', },
+          DiagnosticUnderlineWarn  = { sp = 'orange', },
+          GitSignsAdd              = { fg = 'limegreen', },
+          GitSignsChange           = { fg = 'orange', },
+          GitSignsDelete           = { fg = 'red', },
+          LspInlayHint             = { fg = 'grey', bg = 'none', },
+          MatchParen               = { fg = 'black', bg = 'orange', },
+          TelescopeMatching        = { fg = 'orange', },
+          TelescopePromptPrefix    = { link = 'TelescopePromptBorder', },
+        },
+      },
+      palettes = {
+        all = {
+          bg1 = '#171b21',
+        },
+      },
+    })
+
+    vim.cmd.colorscheme('nordfox')
+  end,
+}
