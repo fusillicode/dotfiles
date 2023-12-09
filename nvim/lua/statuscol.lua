3deab00134cd9a91ee98158094b0aea12f0524ad
fusillicode <2248534+fusillicode@users.noreply.github.com>
2023-12-09 20:30:55 +0000

    Refactor & lot of space

---
+++ nvim/lua/statuscol.lua
@@ -0,0 +1,38 @@
+local M = {}
+_G.statuscol = M
+
+local trim = require('utils').trim
+
+local function format_extmark(extmark)
+  return (extmark and ('%#' .. extmark.sign_hl_group .. '#' .. trim(extmark.sign_text) .. '%*') or ' ')
+end
+
+function M.signs(bufnr, current_lnum)
+  local line_signs = vim.api.nvim_buf_get_extmarks(bufnr, -1, { current_lnum - 1, 0, }, { current_lnum - 1, -1, },
+    { type = 'sign', details = true, overlap = false, })
+
+  local git_sign, error_sign, warn_sign, hint_sign, info_sign, ok_sign
+  for _, sign in ipairs(line_signs) do
+    local sign_details = sign[4]
+
+    if sign_details.sign_hl_group:sub(1, 8) == 'GitSigns' then
+      git_sign = sign_details
+    elseif sign_details.sign_hl_group == 'DiagnosticSignError' then
+      error_sign = sign_details
+    elseif sign_details.sign_hl_group == 'DiagnosticSignWarn' then
+      warn_sign = sign_details
+    elseif sign_details.sign_hl_group == 'DiagnosticSignHint' then
+      hint_sign = sign_details
+    elseif sign_details.sign_hl_group == 'DiagnosticSignInfo' then
+      info_sign = sign_details
+    elseif sign_details.sign_hl_group == 'DiagnosticSignOk' then
+      ok_sign = sign_details
+    end
+  end
+
+  return format_extmark(error_sign or warn_sign or hint_sign or info_sign or ok_sign)
+      .. ' %=%{v:lnum} '
+      .. format_extmark(git_sign) .. ' '
+end
+
+return M
