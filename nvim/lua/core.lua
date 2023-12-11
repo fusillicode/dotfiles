1dfa4a25478f6b9db0179040a9593c85960c2fa8
Gianluca Randazzo <fusillicode@gmail.com>
2023-12-11 05:36:51 +0000

    This is ugly...

--- nvim/lua/core.lua
+++ nvim/lua/core.lua
@@ -26,37 +26,51 @@ vim.o.signcolumn = 'no'
 vim.o.statuscolumn = '%{%v:lua.require("statuscol").draw(bufnr(), v:lnum)%}'
 
 function statusline()
-  local errors, warns, hints, infos, oks = 0, 0, 0, 0, 0
+  local buffer = vim.fn.bufnr()
+  local buffer_errors, buffer_warns, buffer_hints, buffer_infos = 0, 0, 0, 0
+  local workspace_errors, workspace_warns, workspace_hints, workspace_infos = 0, 0, 0, 0
+
   for _, diagnostic in ipairs(vim.diagnostic.get()) do
+    if diagnostic.bufnr == buffer then
+      if diagnostic.severity == vim.diagnostic.severity.ERROR then
+        buffer_errors = buffer_errors + 1
+      elseif diagnostic.severity == vim.diagnostic.severity.WARN then
+        buffer_warns = buffer_warns + 1
+      elseif diagnostic.severity == vim.diagnostic.severity.HINT then
+        buffer_hints = buffer_hints + 1
+      elseif diagnostic.severity == vim.diagnostic.severity.INFO then
+        buffer_infos = buffer_infos + 1
+      end
+    end
     if diagnostic.severity == vim.diagnostic.severity.ERROR then
-      errors = errors + 1
+      workspace_errors = workspace_errors + 1
     elseif diagnostic.severity == vim.diagnostic.severity.WARN then
-      warns = warns + 1
+      workspace_warns = workspace_warns + 1
     elseif diagnostic.severity == vim.diagnostic.severity.HINT then
-      hints = hints + 1
+      workspace_hints = workspace_hints + 1
     elseif diagnostic.severity == vim.diagnostic.severity.INFO then
-      infos = infos + 1
+      workspace_infos = workspace_infos + 1
     end
   end
 
-  return (errors ~= 0 and '%#DiagnosticError#' .. 'E:' .. errors .. ' ' or '')
-      .. (warns ~= 0 and '%#DiagnosticWarn#' .. 'W:' .. warns .. ' ' or '')
-      .. (hints ~= 0 and '%#DiagnosticHint#' .. 'H:' .. hints .. ' ' or '')
-      .. (infos ~= 0 and '%#DiagnosticInfo#' .. 'I:' .. infos .. ' ' or '')
+  return (buffer_errors ~= 0 and '%#DiagnosticError#' .. 'E:' .. buffer_errors .. ' ' or '')
+      .. (buffer_warns ~= 0 and '%#DiagnosticWarn#' .. 'W:' .. buffer_warns .. ' ' or '')
+      .. (buffer_hints ~= 0 and '%#DiagnosticHint#' .. 'H:' .. buffer_hints .. ' ' or '')
+      .. (buffer_infos ~= 0 and '%#DiagnosticInfo#' .. 'I:' .. buffer_infos .. ' ' or '')
       .. '%#StatusLine#'
       .. '%f %m %r'
       .. '%='
-      .. (errors ~= 0 and '%#DiagnosticError#' .. 'E:' .. errors .. ' ' or '')
-      .. (warns ~= 0 and '%#DiagnosticWarn#' .. 'W:' .. warns .. ' ' or '')
-      .. (hints ~= 0 and '%#DiagnosticHint#' .. 'H:' .. hints .. ' ' or '')
-      .. (infos ~= 0 and '%#DiagnosticInfo#' .. 'I:' .. infos .. ' ' or '')
+      .. (workspace_errors ~= 0 and '%#DiagnosticError#' .. 'E:' .. workspace_errors .. ' ' or '')
+      .. (workspace_warns ~= 0 and '%#DiagnosticWarn#' .. 'W:' .. workspace_warns .. ' ' or '')
+      .. (workspace_hints ~= 0 and '%#DiagnosticHint#' .. 'H:' .. workspace_hints .. ' ' or '')
+      .. (workspace_infos ~= 0 and '%#DiagnosticInfo#' .. 'I:' .. workspace_infos .. ' ' or '')
 end
 
 vim.o.statusline = '%{%v:lua.statusline()%}'
 
-vim.api.nvim_create_autocmd('DiagnosticChanged', {
+vim.api.nvim_create_autocmd({ 'DiagnosticChanged', 'BufEnter', }, {
   group = vim.api.nvim_create_augroup('StatusLine', { clear = true, }),
-  callback = function(_) vim.wo.statusline = statusline() end,
+  callback = function(_) vim.o.statusline = statusline() end,
 })
 
 vim.o.sidescroll = 1
