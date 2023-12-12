43e550f40c586ba1e09402b1f61892d4b3875dd1
fusillicode <2248534+fusillicode@users.noreply.github.com>
2023-12-12 06:57:33 +0000

    Ugly & buggy but I love it

---
+++ nvim/lua/open-stuff.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+function M.open_under_cursor()
+  local cursor_col = vim.api.nvim_win_get_cursor(0)[2]
+  local line = vim.api.nvim_get_current_line()
+
+  local before = line:sub(1, cursor_col)
+  local after = line:sub(cursor_col + 1, #line)
+
+  local _, _, before_match = before:find('(%S+)$')
+  local _, _, after_match = after:find('^(%S+)')
+
+  local thing = vim.fn.trim((before_match or '') .. (after_match or ''))
+  if thing == '' then return end
+  vim.fn.jobstart({ 'open', thing }, {
+    detach = true,
+    on_exit = function(_, code, _)
+      if code ~= 0 then
+        print('Cannot open "' .. thing .. '" 🤖')
+      end
+    end,
+  })
+end
+
+return M
