80cbf721528b7848acdc51e39e0f13218ff0b7e7
Gianluca Randazzo <fusillicode@gmail.com>
2023-12-13 19:37:30 +0000

    require('fzf-lua')

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -3,74 +3,76 @@ return {
   keys = { '<leader>', 'gd', 'gr', 'gi', },
   dependencies = { { 'junegunn/fzf', build = './install --bin', }, },
   config = function()
+    local fzf_lua = require('fzf-lua')
     local keymap_set = require('utils').keymap_set
+
     keymap_set('n', '<leader>b', function()
-      require('fzf-lua').buffers()
+      fzf_lua.buffers()
     end)
     keymap_set('n', '<leader>f', function()
-      require('fzf-lua').files()
+      fzf_lua.files()
     end)
 
     keymap_set('n', '<leader>gs', function()
-      require('fzf-lua').git_status()
+      fzf_lua.git_status()
     end)
     keymap_set('n', '<leader>gc', function()
-      require('fzf-lua').git_commits()
+      fzf_lua.git_commits()
     end)
     keymap_set('n', '<leader>gbc', function()
-      require('fzf-lua').git_bcommits()
+      fzf_lua.git_bcommits()
     end)
     keymap_set('n', '<leader>gb', function()
-      require('fzf-lua').git_branches()
+      fzf_lua.git_branches()
     end)
 
     keymap_set('n', 'gr', function()
-      require('fzf-lua').lsp_references({ jump_to_single_result = true, })
+      fzf_lua.lsp_references({ jump_to_single_result = true, })
     end)
     keymap_set('n', 'gd', function()
-      require('fzf-lua').lsp_definitions({ jump_to_single_result = true, })
+      fzf_lua.lsp_definitions({ jump_to_single_result = true, })
     end)
     keymap_set('n', 'gi', function()
-      require('fzf-lua').lsp_implementations({ jump_to_single_result = true, })
+      fzf_lua.lsp_implementations({ jump_to_single_result = true, })
     end)
 
     keymap_set('n', '<leader>s', function()
-      require('fzf-lua').lsp_document_symbols()
+      fzf_lua.lsp_document_symbols()
     end)
     keymap_set('n', '<leader>S', function()
-      require('fzf-lua').lsp_live_workspace_symbols()
+      fzf_lua.lsp_live_workspace_symbols()
     end)
     keymap_set('n', '<leader>a', function()
-      require('fzf-lua').lsp_code_actions()
+      fzf_lua.lsp_code_actions()
     end)
 
     keymap_set('n', '<leader>d', function()
-      require('fzf-lua').diagnostics_document()
+      fzf_lua.diagnostics_document()
     end)
     keymap_set('n', '<leader>D', function()
-      require('fzf-lua').diagnostics_workspace()
+      fzf_lua.diagnostics_workspace()
     end)
 
     keymap_set('n', '/', function()
-      require('fzf-lua').lgrep_curbuf()
+      fzf_lua.lgrep_curbuf({ continue_last_search = true, })
     end)
     keymap_set('n', '<leader>/', function()
-      require('fzf-lua').live_grep_glob()
+      fzf_lua.live_grep_glob({ continue_last_search = true, })
     end)
 
     keymap_set('n', '<leader>t',
       function()
-        require('fzf-lua').grep_curbuf({ search = 'TODO|HACK|PERF|NOTE|FIX', no_esc = true, })
+        fzf_lua.grep_curbuf({ search = 'TODO:|HACK:|PERF:|NOTE:|FIX:|WARN:', no_esc = true, })
       end)
     keymap_set('n', '<leader>T', function()
-      require('fzf-lua').grep({ search = 'TODO|HACK|PERF|NOTE|FIX', no_esc = true, })
+      fzf_lua.grep({ search = 'TODO:|HACK:|PERF:|NOTE:|FIX:|WARN:', no_esc = true, })
     end)
 
     keymap_set('n', '<leader>l', function()
-      require('fzf-lua').resume()
+      fzf_lua.resume()
     end)
 
-    require('fzf-lua').setup({
+    fzf_lua.setup({
       'max-perf',
       fzf_opts = { ['--cycle'] = '', },
       winopts = {
