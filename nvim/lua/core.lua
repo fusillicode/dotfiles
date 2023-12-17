01d112a5c44ed1fce366455b5ca26361a64bcff3
fusillicode <2248534+fusillicode@users.noreply.github.com>
2023-12-17 12:32:24 +0100

    really zz all the things

--- nvim/lua/core.lua
+++ nvim/lua/core.lua
@@ -105,8 +105,8 @@ keymap_set('v', "<c-s>'", "c''<esc>P")
 keymap_set('v', '<c-s><', 'c<><esc>P')
 keymap_set('v', '<c-s>`', 'c``<esc>P')
 
-keymap_set('n', 'dp', vim.diagnostic.goto_prev)
-keymap_set('n', 'dn', vim.diagnostic.goto_next)
+keymap_set('n', 'dp', '<cmd>lua vim.diagnostic.goto_prev()<cr>zz')
+keymap_set('n', 'dn', '<cmd>lua vim.diagnostic.goto_next()<cr>zz')
 keymap_set('n', '<leader>e', vim.diagnostic.open_float)
 
 keymap_set('n', '<leader>gx', require('opener').open_under_cursor)
