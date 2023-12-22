270810391f9ea4282250796b6e9aab778f1757af
fusillicode <2248534+fusillicode@users.noreply.github.com>
2023-12-22 15:13:41 +0100

    Goodbye Mono?

--- .wezterm.lua
+++ .wezterm.lua
@@ -32,7 +32,6 @@ config.colors = {
     inactive_tab = { bg_color = background, fg_color = 'grey', },
   },
 }
-config.font = wezterm.font('Monaco')
 config.font_size = 16
 config.inactive_pane_hsb = { brightness = 0.5, }
 config.hide_tab_bar_if_only_one_tab = true
