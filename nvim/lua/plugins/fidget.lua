3ce2f5e3c8179df11266ff59ff05263e5d931b05
fusillicode <2248534+fusillicode@users.noreply.github.com>
2023-12-23 11:10:07 +0100

    I guess welcome back fidget <3

---
+++ nvim/lua/plugins/fidget.lua
@@ -0,0 +1,12 @@
+return {
+  'j-hui/fidget.nvim',
+  event = 'LspAttach',
+  opts = {
+    progress = {
+      display = {
+        render_limit = 1,
+        overrides = {},
+      },
+    },
+  },
+}
