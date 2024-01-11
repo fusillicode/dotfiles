857291c190e09fbb6eae4801ffb289387ba72f55
Gianluca Randazzo <fusillicode@gmail.com>
2024-01-11 08:36:04 +0100

    DROP MASON! 🙌

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,10 +1,112 @@
+local function lsps_configs()
+  return {
+    bashls = {},
+    docker_compose_language_service = {},
+    dockerls = {},
+    elixirls = {
+      cmd = { os.getenv('HOME') .. '/.local/bin' .. '/elixir-ls', },
+    },
+    elmls = {},
+    graphql = {},
+    html = {},
+    helm_ls = {},
+    jsonls = {
+      settings = {
+        json = {
+          validate = { enable = true, },
+          schemas = require('schemastore').json.schemas({
+            select = {
+              'GitHub Workflow Template Properties',
+            },
+          }),
+        },
+      },
+    },
+    lua_ls = {
+      settings = {
+        Lua = {
+          completion = {
+            callSnippet = 'Both',
+            callKeyword = 'Both',
+          },
+          format = {
+            defaultConfig = {
+              insert_final_newline = 'true',
+              quote_style = 'single',
+              trailing_table_separator = 'always',
+            },
+          },
+          hint = { enable = true, setType = true, },
+          diagnostics = { globals = { 'vim', }, },
+          telemetry = { enable = false, },
+          workspace = { checkThirdParty = false, },
+        },
+      },
+    },
+    marksman = {},
+    phpactor = {},
+    psalm = {
+      settings = {
+        psalm = {
+          configPaths = { 'psalm.xml', 'psalm.xml.dist', 'psalm-baseline.xml', },
+        },
+      },
+    },
+    ruff_lsp = {},
+    rust_analyzer = {
+      settings = {
+        ['rust-analyzer'] = {
+          cargo = {
+            build_script = { enable = true, },
+            extraArgs = { '--profile', 'rust-analyzer', },
+            extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = 'dev', },
+          },
+          check = { command = 'clippy', },
+          checkOnSave = { command = 'clippy', },
+          completion = { autoimport = { enable = true, }, },
+          imports = { enforce = true, granularity = { group = 'item', }, prefix = 'crate', },
+          lens = { debug = { enable = false, }, implementations = { enable = false, }, run = { enable = false, }, },
+          proc_macro = { enable = true, },
+          showUnlinkedFileNotification = false,
+        },
+      },
+    },
+    sqlls = {},
+    taplo = {},
+    typos_lsp = {
+      init_options = {
+        diagnosticSeverity = 'Warning',
+      },
+    },
+    tsserver = {},
+    yamlls = {
+      settings = {
+        yaml = {
+          schemaStore = {
+            enable = false,
+            url = '',
+          },
+          schemas = vim.tbl_extend('error',
+            require('schemastore').yaml.schemas({
+              select = {
+                'kustomization.yaml',
+                'GitHub Workflow',
+                'docker-compose.yml',
+              },
+            }),
+            { kubernetes = { 'k8s**.yaml', 'kube*/*.yaml', }, }
+          ),
+        },
+      },
+    },
+  }
+end
+
 return {
   'neovim/nvim-lspconfig',
   event = 'BufRead',
   dependencies = {
     'hrsh7th/cmp-nvim-lsp',
-    'williamboman/mason-lspconfig.nvim',
-    'williamboman/mason.nvim',
     'b0o/schemastore.nvim',
   },
   config = function()
@@ -18,7 +120,7 @@ return {
       keymap_set('n', '<leader>r', vim.lsp.buf.rename, { buffer = bufnr, })
     end
 
-    for lsp, config in pairs(require('mason-tools').tools()['lsps']) do
+    for lsp, config in pairs(lsps_configs()) do
       local lsp_setup = { capabilities = capabilities, on_attach = on_attach, }
       if config['cmd'] then lsp_setup.cmd = config['cmd'] end
       if config['settings'] then lsp_setup.settings = config['settings'] end
