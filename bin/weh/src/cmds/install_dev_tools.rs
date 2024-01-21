8f45455f278f8ebd9238b1524c43f396c693d032
Gianluca Randazzo <fusillicode@gmail.com>
2024-01-21 12:14:12 +0100

    Draft out get_bin_via_curl

--- bin/weh/src/cmds/install_dev_tools.rs
+++ bin/weh/src/cmds/install_dev_tools.rs
@@ -1,10 +1,8 @@
-use std::fs::File;
+use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
-use std::process::ExitStatus;
 use std::process::Stdio;
 
-use anyhow::anyhow;
 use anyhow::bail;
 
 pub fn run<'a>(mut args: impl Iterator<Item = &'a str>) -> anyhow::Result<()> {
@@ -85,4 +83,47 @@ fn get_latest_release(repo: &str) -> anyhow::Result<String> {
         repo,
         output
     )
+}
+
+enum OutputOption<'a> {
+    PipeTo(Command),
+    WriteTo(&'a Path),
+}
+
+// curl -SL https://github.com/rust-lang/rust-analyzer/releases/download/nightly/rust-analyzer-aarch64-apple-darwin.gz | \
+//   zcat > "$bin_dir"/rust-analyzer
+//
+// curl -SL https://github.com/tamasfe/taplo/releases/latest/download/taplo-full-darwin-aarch64.gz | \
+//   zcat > "$bin_dir"/taplo
+//
+// latest_release=$(get_latest_release "hashicorp/terraform-ls" | cut -c2-)
+// curl -SL https://releases.hashicorp.com/terraform-ls/"$latest_release"/terraform-ls_"$latest_release"_darwin_arm64.zip | \
+//   tar -xz -C "$bin_dir"
+//
+// repo="tekumara/typos-vscode"
+// latest_release=$(get_latest_release $repo)
+// curl -SL https://github.com/"$repo"/releases/download/"$latest_release"/typos-lsp-"$latest_release"-aarch64-apple-darwin.tar.gz | \
+//   tar -xz -C "$bin_dir"
+//
+// curl -SL https://github.com/hadolint/hadolint/releases/latest/download/hadolint-Darwin-x86_64 --output "$bin_dir"/hadolint
+// curl -SL https://github.com/mrjosh/helm-ls/releases/latest/download/helm_ls_darwin_amd64 --output "$bin_dir"/helm_ls
+// curl -SL https://github.com/artempyanykh/marksman/releases/latest/download/marksman-macos --output "$bin_dir"/marksmam
+fn get_bin_via_curl(url: &str, output_option: OutputOption) -> anyhow::Result<()> {
+    let mut curl_cmd = Command::new("curl");
+    curl_cmd.args(["-SL", url]);
+
+    match output_option {
+        OutputOption::PipeTo(mut cmd) => {
+            let curl_child = curl_cmd.stdout(Stdio::piped()).spawn()?;
+            cmd.stdin(Stdio::from(curl_child.stdout.unwrap()))
+                .status()?;
+            Ok(())
+        }
+        OutputOption::WriteTo(output_path) => {
+            curl_cmd.arg("--output");
+            curl_cmd.arg(output_path.to_str().unwrap());
+            curl_cmd.status()?;
+            Ok(())
+        }
+    }
 }
