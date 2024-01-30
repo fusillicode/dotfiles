fd52d5a139229f7902fe7cba63da77ee91c2f5be
Gianluca Randazzo <fusillicode@gmail.com>
2024-01-30 14:41:42 +0100

    no prettierd for now

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,9 +4,9 @@ return {
   cmd = 'ConformInfo',
   opts = {
     formatters_by_ft = {
-      javascript = { { 'prettierd', 'prettier', }, },
-      typescript = { { 'prettierd', 'prettier', }, },
-      typescriptreact = { { 'prettierd', 'prettier', }, },
+      javascript = { { 'prettier', }, },
+      typescript = { { 'prettier', }, },
+      typescriptreact = { { 'prettier', }, },
     },
     format_on_save = { timeout_ms = 500, lsp_fallback = true, },
   },
