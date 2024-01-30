7dffa20419503edad4401944ca69b24ec6ffd523
Gianluca Randazzo <fusillicode@gmail.com>
2024-01-30 13:06:29 +0100

    Hardcore updates

--- bin/update_brew.sh
+++ bin/update_brew.sh
@@ -2,9 +2,13 @@
 
 set -euo pipefail
 
-# ✨ brew stuff
 brew update
 brew upgrade
+
+# Hardcore stuff 🦾
+brew upgrade --cask homebrew/cask-versions/wezterm-nightly --no-quarantine --greedy-latest
+brew upgrade neovim --fetch-HEAD
+
 brew cleanup -s
 rm -rf ~/Library/Caches/Homebrew/*
 brew doctor
