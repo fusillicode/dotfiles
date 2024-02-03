2b28ba4090e0002d8f114e2ea1287e68e2720893
Gianluca Randazzo <fusillicode@gmail.com>
2024-02-03 11:11:26 +0100

    This is BIG! inlay_hints as they should be ❤️ 😌

--- nvim/lua/plugins/lsp-inlayhints.lua
+++ nvim/lua/plugins/lsp-inlayhints.lua
@@ -10,6 +10,10 @@ return {
         end
 
         local client = vim.lsp.get_client_by_id(args.data.client_id)
+        -- https://vinnymeller.com/posts/neovim_nightly_inlay_hints/#globally
+        if client.server_capabilities.inlayHintProvider then
+          vim.lsp.inlay_hint.enable(args.buf, true)
+        end
         require('lsp-inlayhints').on_attach(client, args.buf)
       end,
     })
