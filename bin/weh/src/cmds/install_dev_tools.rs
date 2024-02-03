46df6df2015f65a7888fe5652f492b66ccf3c441
Gianluca Randazzo <fusillicode@gmail.com>
2024-02-03 10:41:32 +0100

    Yeah stringly typed all the way

--- bin/weh/src/cmds/install_dev_tools.rs
+++ bin/weh/src/cmds/install_dev_tools.rs
@@ -9,10 +9,12 @@ use anyhow::bail;
 pub fn run<'a>(mut args: impl Iterator<Item = &'a str> + std::fmt::Debug) -> anyhow::Result<()> {
     let dev_tools_dir = args
         .next()
-        .ok_or_else(|| anyhow!("missing dev_tools_dir arg from {args:?}"))?;
+        .ok_or_else(|| anyhow!("missing dev_tools_dir arg from {args:?}"))?
+        .trim_end_matches("/");
     let bin_dir = args
         .next()
-        .ok_or_else(|| anyhow!("missing bin_dir arg from {args:?}"))?;
+        .ok_or_else(|| anyhow!("missing bin_dir arg from {args:?}"))?
+        .trim_end_matches("/");
 
     std::fs::create_dir_all(dev_tools_dir)?;
     std::fs::create_dir_all(bin_dir)?;
