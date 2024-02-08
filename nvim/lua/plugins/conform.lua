a1f015b19559501011ca5e6f198864f44fa09507
Gianluca Randazzo <fusillicode@gmail.com>
2024-02-08 17:56:25 +0100

    prettierd all the things

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,11 +4,15 @@ return {
   cmd = 'ConformInfo',
   opts = {
     formatters_by_ft = {
+      css = { { 'prettierd', }, },
+      graphql = { { 'prettierd', }, },
+      html = { { 'prettierd', }, },
       javascript = { { 'prettierd', }, },
       markdown = { { 'prettierd', }, },
       sql = { { 'sqlfluff', }, },
       typescript = { { 'prettierd', }, },
       typescriptreact = { { 'prettierd', }, },
+      yaml = { { 'prettierd', }, },
       ['*'] = { 'trim_whitespaces', 'trim_newlines', },
     },
     format_on_save = { timeout_ms = 500, lsp_fallback = true, },
