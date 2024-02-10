76b4862c1e420f3a0c71f0710a95ee8e948c722a
Gianluca Randazzo <fusillicode@gmail.com>
2024-02-10 15:55:26 +0100

    Is this really it?!

--- bin/tempura/src/cmds/install_dev_tools.rs
+++ bin/tempura/src/cmds/install_dev_tools.rs
@@ -20,222 +20,214 @@ pub fn run<'a>(mut args: impl Iterator<Item = &'a str> + Debug) -> anyhow::Resul
     std::fs::create_dir_all(dev_tools_dir)?;
     std::fs::create_dir_all(bin_dir)?;
 
-    // log_into_github()?;
-    //
-    // curl_install(
-    //     "https://github.com/rust-lang/rust-analyzer/releases/download/nightly/rust-analyzer-aarch64-apple-darwin.gz",
-    //     OutputOption::UnpackVia(Command::new("zcat"), &format!("{bin_dir}/rust-analyzer"))
+    log_into_github()?;
+
+    curl_install(
+        "https://github.com/rust-lang/rust-analyzer/releases/download/nightly/rust-analyzer-aarch64-apple-darwin.gz",
+        OutputOption::UnpackVia(Command::new("zcat"), &format!("{bin_dir}/rust-analyzer"))
+    )?;
+
+    // get_bin_via_curl(
+    //     "https://github.com/tamasfe/taplo/releases/latest/download/taplo-full-darwin-aarch64.gz",
+    //     OutputOption::UnpackVia(Command::new("zcat"), &format!("{bin_dir}/taplo")),
     // )?;
-    //
-    // // get_bin_via_curl(
-    // //     "https://github.com/tamasfe/taplo/releases/latest/download/taplo-full-darwin-aarch64.gz",
-    // //     OutputOption::UnpackVia(Command::new("zcat"), &format!("{bin_dir}/taplo")),
-    // // )?;
-    //
-    // let repo = "hashicorp/terraform-ls";
-    // let latest_release = &get_latest_release(repo)?[1..];
-    // curl_install(
-    //     &format!("https://releases.hashicorp.com/terraform-ls/{latest_release}/terraform-ls_{latest_release}_darwin_arm64.zip"),
-    //     OutputOption::PipeInto(Command::new("tar").args(["-xz", "-C", bin_dir])),
-    // )?;
-    //
-    // let repo = "tekumara/typos-vscode";
-    // let latest_release = get_latest_release(repo)?;
-    // curl_install(
-    //     &format!("https://github.com/{repo}/releases/download/{latest_release}/typos-lsp-{latest_release}-aarch64-apple-darwin.tar.gz"),
-    //     OutputOption::PipeInto(Command::new("tar").args(["-xz", "-C", bin_dir])),
-    // )?;
-    //
-    // let repo = "errata-ai/vale";
-    // let latest_release = get_latest_release(repo)?;
-    // curl_install(
-    //     &format!("https://github.com/{repo}/releases/download/{latest_release}/vale_{}_macOS_arm64.tar.gz", latest_release[1..].to_owned()),
-    //     OutputOption::PipeInto(Command::new("tar").args(["-xz", "-C", bin_dir])),
-    // )?;
-    //
-    // curl_install(
-    //     "https://github.com/hadolint/hadolint/releases/latest/download/hadolint-Darwin-x86_64",
-    //     OutputOption::WriteTo(&format!("{bin_dir}/hadolint")),
-    // )?;
-    //
-    // curl_install(
-    //     "https://github.com/mrjosh/helm-ls/releases/latest/download/helm_ls_darwin_amd64",
-    //     OutputOption::WriteTo(&format!("{bin_dir}/helm_ls")),
-    // )?;
-    //
-    // curl_install(
-    //     "https://github.com/artempyanykh/marksman/releases/latest/download/marksman-macos",
-    //     OutputOption::WriteTo(&format!("{bin_dir}/marksman")),
-    // )?;
-    //
-    // let tool = "shellcheck";
-    // let repo = format!("koalaman/{tool}");
-    // let latest_release = get_latest_release(&repo)?;
-    // curl_install(
-    //     &format!("https://github.com/{repo}/releases/download/{latest_release}/{tool}-{latest_release}.darwin.x86_64.tar.xz"),
-    //     OutputOption::PipeInto(Command::new("tar").args(["-xz", "-C", "/tmp"])),
-    // )?;
-    // let exit_status = Command::new("mv")
-    //     .args([&format!("/tmp/{tool}-{latest_release}/{tool}"), bin_dir])
-    //     .status()?;
-    // if !exit_status.success() {
-    //     bail!("error moving /tmp/{tool} to {bin_dir}")
-    // }
-    //
-    // let tool = "elixir-ls";
-    // let repo = format!("elixir-lsp/{tool}");
-    // let dev_tools_repo_dir = format!("{dev_tools_dir}/{tool}");
-    // let latest_release = get_latest_release(&repo)?;
-    // std::fs::create_dir_all(&dev_tools_repo_dir)?;
-    // curl_install(
-    //     &format!("https://github.com/{repo}/releases/download/{latest_release}/{tool}-{latest_release}.zip"),
-    //     OutputOption::PipeInto(Command::new("tar").args(["-xz", "-C", &dev_tools_repo_dir])),
-    // )?;
-    // chmod_x(&format!("{dev_tools_repo_dir}/*"))?;
-    // let exit_status = Command::new("ln")
-    //     .args([
-    //         "-sf",
-    //         &format!("{dev_tools_repo_dir}/language_server.sh"),
-    //         &format!("{bin_dir}/elixir-ls"),
-    //     ])
-    //     .status()?;
-    // if !exit_status.success() {
-    //     bail!("error symlinking {dev_tools_repo_dir}/language_server.sh to {bin_dir}/elixir-ls")
-    // }
-    //
-    // // No `bin` link as it requires some local stuff so, leave the garbage in `dev-tools` and configure the LSP to point to
-    // // the `bin` there.
-    // let tool = "lua-language-server";
-    // let repo = format!("LuaLS/{tool}");
-    // let dev_tools_repo_dir = format!("{dev_tools_dir}/{tool}");
-    // let latest_release = get_latest_release(&repo)?;
-    // std::fs::create_dir_all(&dev_tools_repo_dir)?;
-    // curl_install(
-    //     &format!("https://github.com/{repo}/releases/download/{latest_release}/{tool}-{latest_release}-darwin-arm64.tar.gz"),
-    //     OutputOption::PipeInto(Command::new("tar").args(["-xz", "-C", &dev_tools_repo_dir])),
-    // )?;
-    //
-    // composer_install(
-    //     dev_tools_dir,
-    //     "phpactor",
-    //     "phpactor/phpactor",
-    //     bin_dir,
-    //     "phpactor",
-    // )?;
-    //
-    // composer_install(
-    //     dev_tools_dir,
-    //     "php-cs-fixer",
-    //     "friendsofphp/php-cs-fixer",
-    //     bin_dir,
-    //     "php-cs-fixer",
-    // )?;
-    //
-    // composer_install(dev_tools_dir, "psalm", "vimeo/psalm", bin_dir, "*")?;
-    //
-    // npm_install(
-    //     dev_tools_dir,
-    //     "commitlint",
-    //     &["@commitlint/cli", "@commitlint/config-conventional"],
-    //     bin_dir,
-    //     "commitlint",
-    // )?;
-    //
-    // npm_install(
-    //     dev_tools_dir,
-    //     "elm-language-server",
-    //     &["@elm-tooling/elm-language-server"],
-    //     bin_dir,
-    //     "elm-language-server",
-    // )?;
-    //
-    // npm_install(
-    //     dev_tools_dir,
-    //     "compose-language-service",
-    //     &["@microsoft/compose-language-service"],
-    //     bin_dir,
-    //     "docker-compose-langserver",
-    // )?;
-    //
-    // npm_install(
-    //     dev_tools_dir,
-    //     "bash-language-server",
-    //     &["bash-language-server"],
-    //     bin_dir,
-    //     "bash-language-server",
-    // )?;
-    //
-    // npm_install(
-    //     dev_tools_dir,
-    //     "dockerfile-language-server-nodejs",
-    //     &["dockerfile-language-server-nodejs"],
-    //     bin_dir,
-    //     "docker-langserver",
-    // )?;
-    //
-    // npm_install(
-    //     dev_tools_dir,
-    //     "eslint_d",
-    //     &["eslint_d"],
-    //     bin_dir,
-    //     "eslint_d",
-    // )?;
-    //
-    // npm_install(
-    //     dev_tools_dir,
-    //     "graphql-language-service-cli",
-    //     &["graphql-language-service-cli"],
-    //     bin_dir,
-    //     "graphql-lsp",
-    // )?;
-    //
-    // npm_install(
-    //     dev_tools_dir,
-    //     "prettierd",
-    //     &["@fsouza/prettierd"],
-    //     bin_dir,
-    //     "prettierd",
-    // )?;
-    //
-    // npm_install(
-    //     dev_tools_dir,
-    //     "sql-language-server",
-    //     &["sql-language-server"],
-    //     bin_dir,
-    //     "sql-language-server",
-    // )?;
-    //
-    // npm_install(
-    //     dev_tools_dir,
-    //     "vscode-langservers-extracted",
-    //     &["vscode-langservers-extracted"],
-    //     bin_dir,
-    //     "*",
-    // )?;
-    //
-    // npm_install(
-    //     dev_tools_dir,
-    //     "yaml-language-server",
-    //     &["yaml-language-server"],
-    //     bin_dir,
-    //     "yaml-language-server",
-    // )?;
-    //
-    // npm_install(
-    //     dev_tools_dir,
-    //     "typescript-language-server",
-    //     &["typescript-language-server", "typescript"],
-    //     bin_dir,
-    //     "typescript-language-server",
-    // )?;
-    //
-    // npm_install(
-    //     dev_tools_dir,
-    //     "quicktype",
-    //     &["quicktype"],
-    //     bin_dir,
-    //     "quicktype",
-    // )?;
+
+    let repo = "hashicorp/terraform-ls";
+    let latest_release = &get_latest_release(repo)?[1..];
+    curl_install(
+        &format!("https://releases.hashicorp.com/terraform-ls/{latest_release}/terraform-ls_{latest_release}_darwin_arm64.zip"),
+        OutputOption::PipeInto(Command::new("tar").args(["-xz", "-C", bin_dir])),
+    )?;
+
+    let repo = "tekumara/typos-vscode";
+    let latest_release = get_latest_release(repo)?;
+    curl_install(
+        &format!("https://github.com/{repo}/releases/download/{latest_release}/typos-lsp-{latest_release}-aarch64-apple-darwin.tar.gz"),
+        OutputOption::PipeInto(Command::new("tar").args(["-xz", "-C", bin_dir])),
+    )?;
+
+    let repo = "errata-ai/vale";
+    let latest_release = get_latest_release(repo)?;
+    curl_install(
+        &format!("https://github.com/{repo}/releases/download/{latest_release}/vale_{}_macOS_arm64.tar.gz", latest_release[1..].to_owned()),
+        OutputOption::PipeInto(Command::new("tar").args(["-xz", "-C", bin_dir])),
+    )?;
+
+    curl_install(
+        "https://github.com/hadolint/hadolint/releases/latest/download/hadolint-Darwin-x86_64",
+        OutputOption::WriteTo(&format!("{bin_dir}/hadolint")),
+    )?;
+
+    curl_install(
+        "https://github.com/mrjosh/helm-ls/releases/latest/download/helm_ls_darwin_amd64",
+        OutputOption::WriteTo(&format!("{bin_dir}/helm_ls")),
+    )?;
+
+    curl_install(
+        "https://github.com/artempyanykh/marksman/releases/latest/download/marksman-macos",
+        OutputOption::WriteTo(&format!("{bin_dir}/marksman")),
+    )?;
+
+    let tool = "shellcheck";
+    let repo = format!("koalaman/{tool}");
+    let latest_release = get_latest_release(&repo)?;
+    curl_install(
+        &format!("https://github.com/{repo}/releases/download/{latest_release}/{tool}-{latest_release}.darwin.x86_64.tar.xz"),
+        OutputOption::PipeInto(Command::new("tar").args(["-xz", "-C", "/tmp"])),
+    )?;
+    let exit_status = Command::new("mv")
+        .args([&format!("/tmp/{tool}-{latest_release}/{tool}"), bin_dir])
+        .status()?;
+    if !exit_status.success() {
+        bail!("error moving /tmp/{tool} to {bin_dir}")
+    }
+
+    let tool = "elixir-ls";
+    let repo = format!("elixir-lsp/{tool}");
+    let dev_tools_repo_dir = format!("{dev_tools_dir}/{tool}");
+    let latest_release = get_latest_release(&repo)?;
+    std::fs::create_dir_all(&dev_tools_repo_dir)?;
+    curl_install(
+        &format!("https://github.com/{repo}/releases/download/{latest_release}/{tool}-{latest_release}.zip"),
+        OutputOption::PipeInto(Command::new("tar").args(["-xz", "-C", &dev_tools_repo_dir])),
+    )?;
+    chmod_x(&format!("{dev_tools_repo_dir}/*"))?;
+    let exit_status = Command::new("ln")
+        .args([
+            "-sf",
+            &format!("{dev_tools_repo_dir}/language_server.sh"),
+            &format!("{bin_dir}/elixir-ls"),
+        ])
+        .status()?;
+    if !exit_status.success() {
+        bail!("error symlinking {dev_tools_repo_dir}/language_server.sh to {bin_dir}/elixir-ls")
+    }
+
+    // No `bin` link as it requires some local stuff so, leave the garbage in `dev-tools` and configure the LSP to point to
+    // the `bin` there.
+    let tool = "lua-language-server";
+    let repo = format!("LuaLS/{tool}");
+    let dev_tools_repo_dir = format!("{dev_tools_dir}/{tool}");
+    let latest_release = get_latest_release(&repo)?;
+    std::fs::create_dir_all(&dev_tools_repo_dir)?;
+    curl_install(
+        &format!("https://github.com/{repo}/releases/download/{latest_release}/{tool}-{latest_release}-darwin-arm64.tar.gz"),
+        OutputOption::PipeInto(Command::new("tar").args(["-xz", "-C", &dev_tools_repo_dir])),
+    )?;
+
+    composer_install(
+        dev_tools_dir,
+        "phpactor",
+        "phpactor/phpactor",
+        bin_dir,
+        "phpactor",
+    )?;
+
+    composer_install(
+        dev_tools_dir,
+        "php-cs-fixer",
+        "friendsofphp/php-cs-fixer",
+        bin_dir,
+        "php-cs-fixer",
+    )?;
+
+    composer_install(dev_tools_dir, "psalm", "vimeo/psalm", bin_dir, "*")?;
+
+    npm_install(
+        dev_tools_dir,
+        "commitlint",
+        &["@commitlint/cli", "@commitlint/config-conventional"],
+        bin_dir,
+        "commitlint",
+    )?;
+
+    npm_install(
+        dev_tools_dir,
+        "elm-language-server",
+        &["@elm-tooling/elm-language-server"],
+        bin_dir,
+        "elm-language-server",
+    )?;
+
+    npm_install(
+        dev_tools_dir,
+        "bash-language-server",
+        &["bash-language-server"],
+        bin_dir,
+        "bash-language-server",
+    )?;
+
+    npm_install(
+        dev_tools_dir,
+        "dockerfile-language-server-nodejs",
+        &["dockerfile-language-server-nodejs"],
+        bin_dir,
+        "docker-langserver",
+    )?;
+
+    npm_install(
+        dev_tools_dir,
+        "eslint_d",
+        &["eslint_d"],
+        bin_dir,
+        "eslint_d",
+    )?;
+
+    npm_install(
+        dev_tools_dir,
+        "graphql-language-service-cli",
+        &["graphql-language-service-cli"],
+        bin_dir,
+        "graphql-lsp",
+    )?;
+
+    npm_install(
+        dev_tools_dir,
+        "prettierd",
+        &["@fsouza/prettierd"],
+        bin_dir,
+        "prettierd",
+    )?;
+
+    npm_install(
+        dev_tools_dir,
+        "sql-language-server",
+        &["sql-language-server"],
+        bin_dir,
+        "sql-language-server",
+    )?;
+
+    npm_install(
+        dev_tools_dir,
+        "vscode-langservers-extracted",
+        &["vscode-langservers-extracted"],
+        bin_dir,
+        "*",
+    )?;
+
+    npm_install(
+        dev_tools_dir,
+        "yaml-language-server",
+        &["yaml-language-server"],
+        bin_dir,
+        "yaml-language-server",
+    )?;
+
+    npm_install(
+        dev_tools_dir,
+        "typescript-language-server",
+        &["typescript-language-server", "typescript"],
+        bin_dir,
+        "typescript-language-server",
+    )?;
+
+    npm_install(
+        dev_tools_dir,
+        "quicktype",
+        &["quicktype"],
+        bin_dir,
+        "quicktype",
+    )?;
 
     pip_install(
         dev_tools_dir,
