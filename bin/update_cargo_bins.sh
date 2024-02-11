a4fd2b3471459ed59bc5d52cefbd57397772a998
Gianluca Randazzo <fusillicode@gmail.com>
2024-02-11 11:04:24 +0100

    🍤 for LSPs & "dev tools", cargo bin for "generic" cli tools

--- bin/update_cargo_bins.sh
+++ bin/update_cargo_bins.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-# cargo bins ❤️
-rustup component add rust-analyzer
+# cargo CLI tools ❤️
 cargo install --force \
   atuin cargo-make ccase drill fd-find gitui pv ripgrep mise tailspin typos-cli sd sqlx-cli
-cargo install --force taplo-cli --all-features
