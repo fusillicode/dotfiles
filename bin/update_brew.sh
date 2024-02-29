20d40c90bb6c0757ea295ef4ae3767efa7503407
Gianluca Randazzo <fusillicode@gmail.com>
2024-02-29 07:41:23 +0100

    I'll do that myself (.cit)

--- bin/update_brew.sh
+++ bin/update_brew.sh
@@ -7,8 +7,13 @@ brew upgrade
 
 # Hardcore stuff 🦾
 brew upgrade --cask homebrew/cask-versions/wezterm-nightly --no-quarantine --greedy-latest
-brew upgrade neovim --fetch-HEAD
 
 brew cleanup -s
 rm -rf ~/Library/Caches/Homebrew/*
 brew doctor
+
+cd ~/data/dev/neovim || git clone https://github.com/neovim/neovim && \
+  git pull origin master && \
+  make distclean && \
+  make CMAKE_BUILD_TYPE=Release && \
+  sudo make install
