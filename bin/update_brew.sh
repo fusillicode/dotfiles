74218b15bb44b18e6b659cb3226de81e0cf31732
Gianluca Randazzo <fusillicode@gmail.com>
2024-03-01 08:19:04 +0100

    Temporary nvim installation (will be moved in 🍤)

--- bin/update_brew.sh
+++ bin/update_brew.sh
@@ -12,8 +12,14 @@ brew cleanup -s
 rm -rf ~/Library/Caches/Homebrew/*
 brew doctor
 
-cd ~/data/dev/neovim || git clone https://github.com/neovim/neovim && \
+mkdir -p ~/data/dev/neovim && \
+  cd ~/data/dev/neovim && \
+  set +e && \
+  (git clone https://github.com/neovim/neovim . || true) &&
+  set -e && \
+  git checkout master && \
   git pull origin master && \
   make distclean && \
   make CMAKE_BUILD_TYPE=Release && \
-  sudo make install
+  sudo make install && \
+  cd -
