800419a7c48c9d00189a05ac4cf1b135456e1095
Gianluca Randazzo <fusillicode@gmail.com>
2024-03-05 10:42:26 +0100

    🤦‍♂️

--- nvim/lua/plugins/delimited.lua
+++ nvim/lua/plugins/delimited.lua
@@ -1,13 +1,11 @@
 return {
   'mizlan/delimited.nvim',
   event = 'DiagnosticChanged',
-  opts = {
-    post = function()
-      local delimited = require('delimited')
-      local keymap_set = require('utils').keymap_set
+  config = function()
+    local delimited = require('delimited')
+    local keymap_set = require('utils').keymap_set
 
-      keymap_set('n', 'dp', delimited.goto_prev)
-      keymap_set('n', 'dn', delimited.goto_next)
-    end,
-  },
+    keymap_set('n', 'dp', delimited.goto_prev)
+    keymap_set('n', 'dn', delimited.goto_next)
+  end,
 }
