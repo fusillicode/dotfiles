e5a61ad704cee8df5d97f2653cd7b7660d6b3df8
fusillicode <2248534+fusillicode@users.noreply.github.com>
2024-03-20 15:29:11 +0100

    Gonna do it myself (.cit)

--- nvim/lua/plugins/search-replace.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'roobert/search-replace.nvim',
-  keys = { '<leader>', mode = { 'n', 'v', }, },
-  config = function()
-    require('keymaps').search_replace()
-    require('search-replace').setup({})
-  end,
-}
