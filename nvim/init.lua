3860bd14be9d1141c5cb46e2ba1cd7bc05fff092
Gianluca Randazzo <fusillicode@gmail.com>
2024-03-21 15:42:41 +0100

    folke...for real?

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,12 +3,14 @@ require('core')
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    'git',
+    'gh',
+    'repo',
     'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvm.git',
-    '--branch=stable',
+    'folke/lazy.nvm.git',
     lazypath,
+    '--',
+    '--filter=blob:none',
+    '--branch=stable',
   })
 end
 vim.opt.rtp:prepend(lazypath)
