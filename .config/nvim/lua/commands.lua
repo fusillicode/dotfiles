4c8ccd59440793362911a17738b3c2b4abaa160b
Gianluca Randazzo <fusillicode@gmail.com>
2024-05-21 13:47:32 +0200

    mmm...commands are not so bad

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,1 +1,3 @@
 vim.api.nvim_create_user_command('Lazy update', ':Lazy update', {})
+vim.api.nvim_create_user_command('Lazy profile', ':Lazy profile', {})
+vim.api.nvim_create_user_command('Copy all', ':%y+', {})
