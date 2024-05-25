39c28e9c29afa13aa4c7502fbc74ef8375e81e5d
Gianluca Randazzo <fusillicode@gmail.com>
2024-05-25 15:19:19 +0200

    all hail the custom cmds

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -2,3 +2,31 @@ vim.api.nvim_create_user_command('CopyAll', ':%y+', {})
 vim.api.nvim_create_user_command('LazyProfile', ':Lazy profile', {})
 vim.api.nvim_create_user_command('LazyUpdate', ':Lazy update', {})
 vim.api.nvim_create_user_command('SelectAll', 'normal! ggVG', {})
+
+vim.api.nvim_create_user_command('SearchReplaceBuf', function()
+  local search = vim.fn.input('Search: ')
+  if not search or search == '' then
+    print('No search provided')
+    return
+  end
+  local replace = vim.fn.input('Replace: ')
+  if not replace or replace == '' then
+    print('No replace provided')
+    return
+  end
+  vim.api.nvim_command(':%s/' .. search .. '/' .. replace .. '/g')
+end, {})
+
+vim.api.nvim_create_user_command('ReplaceSearch', function()
+  local search = vim.fn.getreg('/')
+  if not search or search == '' then
+    print('No search provided')
+    return
+  end
+  local replace = vim.fn.input('Replace: ')
+  if not replace or replace == '' then
+    print('No replace provided')
+    return
+  end
+  vim.api.nvim_command(':%s/' .. search .. '/' .. replace .. '/g')
+end, {})
