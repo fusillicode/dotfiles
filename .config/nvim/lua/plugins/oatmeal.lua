7da3e0c3619c72143497949bee34ef0797d00112
Gianluca Randazzo <fusillicode@gmail.com>
2024-06-03 12:13:11 +0200

    Ok no 🥣

--- .config/nvim/lua/plugins/oatmeal.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'dustinblackman/oatmeal.nvim',
-  cmd = { 'Oatmeal', },
-  keys = { { '<leader>om', mode = 'n', }, },
-  opts = {
-    backend = 'ollama',
-    model = 'codellama:latest',
-  },
-}
