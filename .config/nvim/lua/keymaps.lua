a15b09f86698dc9b5c73ab75b7a83fd23639e7f5
Gianluca Randazzo <fusillicode@gmail.com>
2024-06-16 17:26:13 +0200

    I hate you nvim_buf_set_keymap

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -192,4 +192,12 @@ function M.model()
   keymap_set({ 'n', 'v', }, '<leader>[', ':Mchat ', { silent = false, })
 end
 
+function M.mchat()
+  for _, mode in pairs({ 'n', 'v', }) do
+    vim.api.nvim_buf_set_keymap(0, mode, '<leader><leader>', ':Mchat<cr>', {})
+    vim.api.nvim_buf_set_keymap(0, mode, '<leader>x', ':bd!<cr>', {})
+    vim.api.nvim_buf_set_keymap(0, mode, '<c-c>', ':Mcancel<cr>', {})
+  end
+end
+
 return M
