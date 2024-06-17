d85166e7ede766258195c0ae491f2c29b52a0c73
Gianluca Randazzo <fusillicode@gmail.com>
2024-06-17 12:09:02 +0200

    fix: refactor with lua table & self:

--- .config/nvim/lua/plugins/model.lua
+++ .config/nvim/lua/plugins/model.lua
@@ -1,36 +1,31 @@
-local function llama3_header(role)
-  return '<|start_header_id|>' .. role .. '<|end_header_id|>'
-end
+local Llama3 = {
+  header = function(role) return '<|start_header_id|>' .. role .. '<|end_header_id|>' end,
+  prompt_as = function(self, role, prompt) return self.header(role) .. '\n' .. prompt .. '<|eot_id|>' end,
+  chat = function(self, messages, config, system_prompt)
+    local prompt = self:prompt_as(
+      'system',
+      (system_prompt and system_prompt or '') .. (config.system and ('Additionally ' .. config.system) or '')
+    )
 
-local function llama3_prompt_as(role, prompt)
-  return llama3_header(role) .. '\n' .. prompt .. '<|eot_id|>'
-end
+    for _, msg in ipairs(messages) do
+      prompt = prompt .. self:prompt_as(msg.role, msg.content)
+    end
 
-local function llama3_chat(messages, config, system_prompt)
-  local prompt = llama3_prompt_as(
-    'system',
-    (system_prompt and system_prompt or '') .. (config.system and ('Additionally ' .. config.system) or '')
-  )
+    prompt = prompt .. self.header('assistant')
 
-  for _, msg in ipairs(messages) do
-    prompt = prompt .. llama3_prompt_as(msg.role, msg.content)
-  end
-
-  prompt = prompt .. llama3_header('assistant')
-
-  return { prompt = prompt, raw = true, }
-end
-
-local function llama3_chat_config(system_prompt)
-  return {
-    provider = require('model.providers.ollama'),
-    params = { model = 'llama3:latest', },
-    create = function(input, context) return context.selection and input or '' end,
-    run = function(messages, config)
-      return llama3_chat(messages, config, system_prompt)
-    end,
-  }
-end
+    return { prompt = prompt, raw = true, }
+  end,
+  chat_config = function(self, system_prompt)
+    return {
+      provider = require('model.providers.ollama'),
+      params = { model = 'llama3:latest', },
+      create = function(input, context) return context.selection and input or '' end,
+      run = function(messages, config)
+        return self:chat(messages, config, system_prompt)
+      end,
+    }
+  end,
+}
 
 return {
   'gsuuon/model.nvim',
@@ -49,9 +44,9 @@ return {
 
     require('model').setup({
       chats = {
-        concise = llama3_chat_config("Use less words as possible and don't hallucinate!"),
-        friendly = llama3_chat_config('Be kind and friendly'),
-        bool = llama3_chat_config('Answer ONLY with yes or no'),
+        concise = Llama3:chat_config("Use less words as possible and don't hallucinate!"),
+        friendly = Llama3:chat_config('Be kind and friendly'),
+        bool = Llama3:chat_config('Answer ONLY with yes or no'),
       },
       prompts = {
         commit = {
@@ -65,11 +60,11 @@ return {
               error('Empty git diff')
             end
 
-            local prompt = llama3_prompt_as(
+            local prompt = Llama3:prompt_as(
                   'system',
                   "You're a Software Engineer who write clear and succinct commits following the Convetional Commits convention."
                 )
-                .. llama3_prompt_as(
+                .. Llama3:prompt_as(
                   'user',
                   'Write JUST the commit message for the following git diff with conventional commit type in lowercase: '
                   .. '```\n'
@@ -77,7 +72,7 @@ return {
                   .. '\n```'
                   .. '<|eot_id|>'
                 )
-                .. llama3_header('assistant')
+                .. Llama3.header('assistant')
 
             return { prompt = prompt, raw = true, }
           end,
@@ -93,17 +88,17 @@ return {
               error('No language supplied')
             end
 
-            local prompt = llama3_prompt_as(
+            local prompt = Llama3:prompt_as(
                   'system',
                   "You're an " .. lang .. ' native speaker who work as a translator.'
                 )
-                .. llama3_prompt_as(
+                .. Llama3:prompt_as(
                   'user',
                   'Translate the following text into ' .. lang .. ' and print ONLY the translation: '
                   .. input
                   .. '<|eot_id|>'
                 )
-                .. llama3_header('assistant')
+                .. Llama3.header('assistant')
 
             return { prompt = prompt, raw = true, }
           end,
