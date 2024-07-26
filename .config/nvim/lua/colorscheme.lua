5f84ee0b38ecc039b04d2f8e2cf909c1b5b04b8d
fusillicode <2248534+fusillicode@users.noreply.github.com>
2024-07-26 17:01:38 +0200

    goodbye white I guess

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -5,7 +5,7 @@ local cyan = 'cyan'
 local green = 'limegreen'
 local orange = 'orange'
 local red = 'red'
-local white = 'white'
+local fuchsia = 'fuchsia'
 
 local match_highlight = { fg = vim.api.nvim_get_hl(0, { name = 'Special', }).fg, bold = true, }
 for hl, value in pairs({
@@ -15,27 +15,27 @@ for hl, value in pairs({
   DiagnosticError = { fg = red, },
   DiagnosticWarn = { fg = orange, },
   DiagnosticInfo = { fg = cyan, },
-  DiagnosticHint = { fg = white, },
+  DiagnosticHint = { fg = fuchsia, },
   DiagnosticOk = { fg = green, },
   DiagnosticFloatingError = { fg = red, },
   DiagnosticFloatingWarn = { fg = orange, },
   DiagnosticFloatingInfo = { fg = cyan, },
-  DiagnosticFloatingHint = { fg = white, },
+  DiagnosticFloatingHint = { fg = fuchsia, },
   DiagnosticFloatingOk = { fg = green, },
   DiagnosticSignError = { fg = red, },
   DiagnosticSignWarn = { fg = orange, },
   DiagnosticSignInfo = { fg = cyan, },
-  DiagnosticSignHint = { fg = white, },
+  DiagnosticSignHint = { fg = fuchsia, },
   DiagnosticSignOk = { fg = green, },
   DiagnosticUnderlineError = { undercurl = true, sp = red, },
   DiagnosticUnderlineWarn = { undercurl = true, sp = orange, },
   DiagnosticUnderlineInfo = { undercurl = true, sp = cyan, },
-  DiagnosticUnderlineHint = { undercurl = true, sp = white, },
+  DiagnosticUnderlineHint = { undercurl = true, sp = fuchsia, },
   DiagnosticUnderlineOk = { undercurl = true, sp = green, },
   DiagnosticVirtualTextError = { fg = red, },
   DiagnosticVirtualTextWarn = { fg = orange, },
   DiagnosticVirtualTextInfo = { fg = cyan, },
-  DiagnosticVirtualTextHint = { fg = white, },
+  DiagnosticVirtualTextHint = { fg = fuchsia, },
   DiagnosticVirtualTextOk = { fg = green, },
   DiagnosticUnnecessary = { fg = '', undercurl = true, sp = orange, },
   DiagnosticDeprecated = { fg = '', strikethrough = true, },
