7e9e5148b3efb9f2a81da83f1074577663e352e4
Gianluca Randazzo <fusillicode@gmail.com>
2024-09-20 10:31:11 +0200

    dont know about this

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -1,14 +1,14 @@
 local M = {}
 
+local set_hl = vim.api.nvim_set_hl
+local get_hl = vim.api.nvim_get_hl
+
 function M.setup(colorscheme)
   if colorscheme then vim.cmd.colorscheme(colorscheme) end
 
   vim.o.background = 'dark'
   vim.o.termguicolors = true
   vim.o.colorcolumn = '120'
-
-  local set_hl = vim.api.nvim_set_hl
-  local get_hl = vim.api.nvim_get_hl
 
   local status_line_hl = { bg = 'none', }
   for hl, value in pairs({
@@ -17,9 +17,13 @@ function M.setup(colorscheme)
   }) do set_hl(0, hl, value) end
 
   for _, lvl in ipairs({ 'Error', 'Warn', 'Info', 'Hint', 'Ok', }) do
-    set_hl(0, 'DiagnosticStatusLine' .. lvl,
-      { fg = get_hl(0, { name = 'Diagnostic' .. lvl, }).fg, bg = status_line_hl.bg, }
-    )
+    local diagn_hl = get_hl(0, { name = 'Diagnostic' .. lvl, })
+    diagn_hl.bg = status_line_hl.bg
+    set_hl(0, 'DiagnosticStatusLine' .. lvl, diagn_hl)
+
+    local diagn_underline_hl = get_hl(0, { name = 'DiagnosticUnderline' .. lvl, })
+    diagn_underline_hl.undercurl = true
+    set_hl(0, 'DiagnosticUnderline' .. lvl, diagn_underline_hl)
   end
 end
 
