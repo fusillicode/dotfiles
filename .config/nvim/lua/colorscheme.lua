0b54b2fcc6c93907b8ad58867d7a454aaca94fa0
fusillicode <2248534+fusillicode@users.noreply.github.com>
2024-09-20 09:15:35 +0200

    bit of refactoring in colorscheme

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -7,15 +7,18 @@ function M.setup(colorscheme)
   vim.o.termguicolors = true
   vim.o.colorcolumn = '120'
 
+  local set_hl = vim.api.nvim_set_hl
+  local get_hl = vim.api.nvim_get_hl
+
   local status_line_hl = { bg = 'none', }
   for hl, value in pairs({
     ColorColumn = { bg = 'NvimDarkGrey3', },
     StatusLine = status_line_hl,
-  }) do vim.api.nvim_set_hl(0, hl, value) end
+  }) do set_hl(0, hl, value) end
 
   for _, lvl in ipairs({ 'Error', 'Warn', 'Info', 'Hint', 'Ok', }) do
-    vim.api.nvim_set_hl(0, 'DiagnosticStatusLine' .. lvl,
-      { fg = vim.api.nvim_get_hl(0, { name = 'Diagnostic' .. lvl, }).fg, bg = status_line_hl.bg, }
+    set_hl(0, 'DiagnosticStatusLine' .. lvl,
+      { fg = get_hl(0, { name = 'Diagnostic' .. lvl, }).fg, bg = status_line_hl.bg, }
     )
   end
 end
