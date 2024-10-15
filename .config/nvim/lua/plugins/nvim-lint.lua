fa6266065c6cf3b076d4f98e52aae807da889ab8
fusillicode <2248534+fusillicode@users.noreply.github.com>
2024-10-15 08:37:26 +0200

    make all vale lints as hints

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -1,3 +1,33 @@
+local function vale_config()
+  return {
+    cmd = 'vale',
+    stdin = true,
+    args = { '--no-exit', '--output', 'JSON', },
+    stream = 'stdout',
+    ignore_exitcode = true,
+    parser = function(output, _)
+      local diagnostics = {}
+      local decoded = vim.fn.json_decode(output)
+
+      if not decoded then return diagnostics end
+
+      for _, diagnostic in ipairs(decoded['stdin.txt']) do
+        table.insert(diagnostics, {
+          lnum = diagnostic.Line - 1,
+          col = diagnostic.Span[1] - 1,
+          end_lnum = diagnostic.Line - 1,
+          end_col = diagnostic.Span[2],
+          severity = vim.diagnostic.severity.HINT,
+          message = diagnostic.Message,
+          source = 'vale',
+        })
+      end
+
+      return diagnostics
+    end,
+  }
+end
+
 return {
   'mfussenegger/nvim-lint',
   config = function()
@@ -12,6 +42,8 @@ return {
       typescript = { 'eslint_d', },
       typescriptreact = { 'eslint_d', },
     }
+
+    lint.linters.vale = vale_config()
 
     vim.api.nvim_create_autocmd('BufWritePost', {
       group = vim.api.nvim_create_augroup('FormatBufferWithNvimLint', { clear = true, }),
