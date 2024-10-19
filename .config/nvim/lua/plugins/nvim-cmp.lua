e0f8e315023519a4deea9484ac354a5115e2a066
Gianluca Randazzo <fusillicode@gmail.com>
2024-10-19 15:01:32 +0200

    fix cmp doc up & down keymap

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -31,8 +31,8 @@ return {
       performance = { max_view_entries = 12, },
       snippet = { expand = function(args) luasnip.lsp_expand(args.body) end, },
       mapping = cmp.mapping.preset.insert({
-        ['<c-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<c-u>'] = cmp.mapping.scroll_docs(4),
+        ['<c-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<c-d>'] = cmp.mapping.scroll_docs(4),
         ['<c-space>'] = cmp.mapping.complete(),
         ['<cr>'] = cmp.mapping.confirm({ select = true, }),
         ['<tab>'] = cmp.mapping(function(fallback)
