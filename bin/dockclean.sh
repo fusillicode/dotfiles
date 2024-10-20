a6f2ee1c632f87335e420133864eb2d5f7e30720
fusillicode <2248534+fusillicode@users.noreply.github.com>
2024-10-20 10:58:23 +0200

    clean some sh

--- bin/dockclean.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker rm "$(docker ps -aq)"
-docker volume rm "$(docker volume ls --format '{{.Name}}')"
--- bin/dockexec.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-container_name=${1:-"fast-data-dev"}
-command=${2:-bash}
-
-docker exec -it "$(docker ps | /opt/homebrew/bin/rg "$container_name" | awk '{print $1;}')" "$command"
