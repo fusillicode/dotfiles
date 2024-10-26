3a77be8157dc8733816de6b0e6dfeca8565e5e85
Gianluca Randazzo <fusillicode@gmail.com>
2024-10-26 09:56:46 +0200

    add mutable PgpassLine update (not sure...not much FP)

--- tempura/src/vpg/src/main.rs
+++ tempura/src/vpg/src/main.rs
@@ -8,7 +8,6 @@ use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
-use std::str::FromStr;
 
 use anyhow::anyhow;
 use anyhow::bail;
@@ -26,38 +25,23 @@ fn main() -> anyhow::Result<()> {
     let mut pgpass_path = PathBuf::from(std::env::var("HOME")?);
     pgpass_path.push(".pgpass");
 
-    let mut lines = get_pgpass_lines(&pgpass_path)?;
-    let (metadata_line_idx, metadata_line) = get_metadata_line(&lines, alias)?;
+    let mut lines = read_pgpass_lines(&pgpass_path)?;
 
-    let pgpass_line_idx = metadata_line_idx + 1;
-    let mut pgpass_line = lines
-        .get(metadata_line_idx + 1)
-        .map(|(_, line)| PgpassLine::from_str(line))
-        .ok_or_else(|| anyhow!("no PgPassLine found at idx {pgpass_line_idx} for metadata line {metadata_line_idx} {metadata_line}"))??;
-
-    let &[_, vault_path] = metadata_line
-        .split_whitespace()
-        .collect::<Vec<_>>()
-        .as_slice()
-    else {
-        bail!(
-            "malformed metadata line {metadata_line_idx} {metadata_line} in file {pgpass_path:?}"
-        );
-    };
+    // We do all of this before interacting with Vault to avoid unneeded calls.
+    let (metadata_line_idx, metadata_line) = find_metadata_line(&lines, alias)?;
+    let mut pgpass_line = get_pgpass_line(&lines, *metadata_line_idx)?;
+    let vault_path = extract_vault_path(&metadata_line)?;
 
     login_to_vault_if_required()?;
 
-    let vault_read_output: VaultPathOutput = serde_json::from_slice(
+    let vault_read_output: VaultReadOutput = serde_json::from_slice(
         &Command::new("vault")
             .args(["read", vault_path, "--format=json"])
             .output()?
             .stdout,
     )?;
 
-    pgpass_line.user = vault_read_output.data.username;
-    pgpass_line.pwd = vault_read_output.data.password;
-    let new_pgpass_line = pgpass_line.to_string();
-    lines[pgpass_line_idx] = (pgpass_line_idx, new_pgpass_line);
+    pgpass_line.update(vault_read_output, &mut lines);
 
     save_new_pgpass_file(lines, &pgpass_path)?;
 
@@ -66,7 +50,7 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn get_pgpass_lines(pgpass_path: &Path) -> anyhow::Result<Vec<(usize, String)>> {
+fn read_pgpass_lines(pgpass_path: &Path) -> anyhow::Result<Vec<(usize, String)>> {
     Ok(std::fs::read_to_string(pgpass_path)?
         .lines()
         .enumerate()
@@ -74,7 +58,7 @@ fn get_pgpass_lines(pgpass_path: &Path) -> anyhow::Result<Vec<(usize, String)>> 
         .collect())
 }
 
-fn get_metadata_line<'a>(
+fn find_metadata_line<'a>(
     lines: &'a [(usize, String)],
     alias: &'a str,
 ) -> anyhow::Result<&'a (usize, String)> {
@@ -90,8 +74,31 @@ fn get_metadata_line<'a>(
     }
 }
 
+fn get_pgpass_line(
+    lines: &[(usize, String)],
+    metadata_line_idx: usize,
+) -> anyhow::Result<PgpassLine> {
+    let pgpass_line_idx = metadata_line_idx + 1;
+    Ok(lines
+        .get(metadata_line_idx + 1)
+        .map(PgpassLine::try_from)
+        .ok_or_else(|| anyhow!("no PgPassLine found at idx {pgpass_line_idx} for metadata line {metadata_line_idx}"))??)
+}
+
+fn extract_vault_path(metadata_line: &str) -> anyhow::Result<&str> {
+    let &[_, vault_path] = metadata_line
+        .split_whitespace()
+        .collect::<Vec<_>>()
+        .as_slice()
+    else {
+        bail!("malformed metadata line {metadata_line}");
+    };
+    Ok(vault_path)
+}
+
 #[derive(Debug, PartialEq, Eq)]
 struct PgpassLine {
+    pub idx: usize,
     pub host: String,
     pub port: i32,
     pub db: String,
@@ -106,14 +113,21 @@ impl PgpassLine {
             self.user, self.host, self.port, self.db
         )
     }
+
+    pub fn update(&mut self, vault_read_output: VaultReadOutput, lines: &mut [(usize, String)]) {
+        self.user = vault_read_output.data.username;
+        self.pwd = vault_read_output.data.password;
+        lines[self.idx] = (self.idx, self.to_string());
+    }
 }
 
-impl FromStr for PgpassLine {
-    type Err = anyhow::Error;
+impl TryFrom<&(usize, String)> for PgpassLine {
+    type Error = anyhow::Error;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+    fn try_from((idx, s): &(usize, String)) -> Result<Self, Self::Error> {
         match s.split(':').collect::<Vec<_>>().as_slice() {
             [host, port, db, user, pwd] => Ok(Self {
+                idx: *idx,
                 host: host.to_string(),
                 port: port
                     .parse()
@@ -139,7 +153,7 @@ impl Display for PgpassLine {
 
 #[allow(dead_code)]
 #[derive(Deserialize, Debug)]
-struct VaultPathOutput {
+struct VaultReadOutput {
     pub request_id: String,
     pub lease_id: String,
     pub lease_duration: i32,
@@ -203,13 +217,14 @@ mod tests {
     fn test_pgpass_line_from_str_returns_the_expected_pgpass_line() {
         assert_eq!(
             PgpassLine {
+                idx: 42,
                 host: "host".into(),
                 port: 5432,
                 db: "db".into(),
                 user: "user".into(),
                 pwd: "pwd".into(),
             },
-            PgpassLine::from_str("host:5432:db:user:pwd").unwrap()
+            PgpassLine::try_from(&(42, "host:5432:db:user:pwd".into())).unwrap()
         )
     }
 
@@ -217,7 +232,7 @@ mod tests {
     fn test_pgpass_line_from_str_returns_an_error_if_port_is_not_a_number() {
         assert_eq!(
             "Err(unexpected port value, found foo, required i32\n\nCaused by:\n    invalid digit found in string)",
-            format!("{:?}", PgpassLine::from_str("host:foo:db:user:pwd"))
+            format!("{:?}", PgpassLine::try_from(&(42, "host:foo:db:user:pwd".into())))
         )
     }
 
@@ -225,7 +240,10 @@ mod tests {
     fn test_pgpass_line_from_str_returns_an_error_if_str_is_malformed() {
         assert_eq!(
             r#"Err(unexpected split parts ["host", "5432", "db", "user"] for str host:5432:db:user)"#,
-            format!("{:?}", PgpassLine::from_str("host:5432:db:user"))
+            format!(
+                "{:?}",
+                PgpassLine::try_from(&(42, "host:5432:db:user".into()))
+            )
         )
     }
 
@@ -234,6 +252,7 @@ mod tests {
         assert_eq!(
             "psql postgres://user@host:5432/db",
             PgpassLine {
+                idx: 42,
                 host: "host".into(),
                 port: 5432,
                 db: "db".into(),
