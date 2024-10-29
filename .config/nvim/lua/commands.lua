5fc810c1e047b7e9c0efac55b9a013680e2db91f
fusillicode <2248534+fusillicode@users.noreply.github.com>
2024-10-29 10:59:19 +0100

    autocmd in commands.lua & diagnostics in diagnostics.lua

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -4,3 +4,20 @@ vim.api.nvim_create_user_command('LazyProfile', ':Lazy profile', {})
 vim.api.nvim_create_user_command('LazyUpdate', ':Lazy update', {})
 vim.api.nvim_create_user_command('SelectAll', 'normal! ggVG', {})
 vim.api.nvim_create_user_command('Messages', ':Messages', {})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('YankHighlight', { clear = true, }),
+  pattern = '*',
+  callback = function() vim.highlight.on_yank() end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', }, {
+  group = vim.api.nvim_create_augroup('AutosaveBuffers', { clear = true, }),
+  command = ':silent! wa!',
+})
+
+vim.api.nvim_create_autocmd({ 'FileType', }, {
+  group = vim.api.nvim_create_augroup('QuickfixConfig', { clear = true, }),
+  pattern = 'qf',
+  callback = function() require('keymaps').quickfix() end,
+})
