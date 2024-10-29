80f6e5f15c15c4365a9227500614dc563b6f0d64
fusillicode <2248534+fusillicode@users.noreply.github.com>
2024-10-29 08:52:29 +0100

    nuke nvim-lsp config custom handlers and on-attach

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -83,52 +83,6 @@ local function get_lsps_configs()
           showUnlinkedFileNotification = false,
         },
       },
-      -- on_attach = function(client, _)
-      --   client.handlers['textDocument/publishDiagnostics'] = function(_, result, ctx, config)
-      --     local file = io.open(log_file, 'a')
-      --     if file then
-      --       local bufnr = vim.uri_to_bufnr(result.uri)
-      --       if not vim.api.nvim_buf_is_loaded(bufnr) then return end
-      --
-      --       vim.diagnostic.reset(vim.lsp.diagnostic.get_namespace(ctx.client_id), bufnr)
-      --
-      --       for idx, diagnostic in ipairs(result.diagnostics) do
-      --         if diagnostic.source == 'rustc' then
-      --           file:write(vim.inspect(diagnostic))
-      --           result.diagnostics[idx] = result.diagnostics[idx]
-      --         else
-      --           result.diagnostics[idx] = result.diagnostics[idx]
-      --         end
-      --       end
-      --       vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
-      --       file:close()
-      --     end
-      --   end
-      -- end,
-      -- handlers = {
-      --   ['textDocument/hover'] = function(_, result, _, config)
-      --     if not (result and result.contents) then return end
-      --
-      --     -- Get the current buffer number and cursor position
-      --     local bufnr = vim.api.nvim_get_current_buf()
-      --     local line = vim.api.nvim_win_get_cursor(0)[1] - 1 -- Adjust 1-indexed to 0-indexed
-      --
-      --     -- Retrieve all diagnostics for the current line
-      --     local diagnostics = vim.diagnostic.get(bufnr, { lnum = line, })
-      --
-      --     -- Concatenate the hover information with diagnostics if available
-      --     local message = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
-      --     if diagnostics and #diagnostics > 0 then
-      --       table.insert(message, '\n\n**Rust Diagnostics:**')
-      --       for _, diagnostic in ipairs(diagnostics) do
-      --         table.insert(message, diagnostic.message)
-      --       end
-      --     end
-      --
-      --     -- Display the concatenated hover and diagnostic message
-      --     vim.lsp.util.open_floating_preview(message, 'markdown', config)
-      --   end,
-      -- },
     },
     sqlls = {},
     taplo = {},
