6a5d82ac555f7f32e2a3d1e3d999d66c1522eaa0
Gianluca Randazzo <fusillicode@gmail.com>
2024-11-14 12:54:15 +0100

    back to Monaco?

--- .wezterm.lua
+++ .wezterm.lua
@@ -34,9 +34,9 @@ config.colors = {
     inactive_tab = { bg_color = background, fg_color = 'grey', },
   },
 }
-config.font = wezterm.font('Courier Prime')
-config.font_size = 16
-config.line_height = 1.2
+config.font = wezterm.font('Monaco')
+config.font_size = 14
+config.line_height = 1.1
 
 config.keys = {
   { key = 'LeftArrow',  mods = 'OPT',         action = act.SendKey { key = 'b', mods = 'ALT', }, },
