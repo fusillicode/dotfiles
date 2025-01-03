5c16fffb83eb90a499ff3c04b3447cdf52d31d91
Gianluca Randazzo <fusillicode@gmail.com>
2025-01-03 10:11:24 +0100

    yakshaving

--- yog/src/gcu/src/main.rs
+++ yog/src/gcu/src/main.rs
@@ -93,7 +93,7 @@ fn switch_branch(branch: &str) -> anyhow::Result<()> {
     if !output.status.success() {
         bail!("{}", std::str::from_utf8(&output.stderr)?.trim())
     }
-    println!("🔁 {branch}");
+    println!("🍃 {branch}");
     Ok(())
 }
 
