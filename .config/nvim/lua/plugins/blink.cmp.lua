1bc839bb2834695370e4bc9319e5f12edd61cef7
Gianluca Randazzo <fusillicode@gmail.com>
2025-01-16 13:55:32 +0100

    simplify I guess

--- .config/nvim/lua/plugins/blink.cmp.lua
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -2,9 +2,6 @@ return {
   'saghen/blink.cmp',
   event = 'InsertEnter',
   build = 'cargo build --release',
-  dependencies = {
-    'L3MON4D3/LuaSnip',
-  },
   opts = {
     appearance = {
       use_nvim_cmp_as_default = true,
@@ -35,16 +32,6 @@ return {
       ['<c-x>'] = { 'show', 'show_documentation', 'hide_documentation', },
       ['<c-u>'] = { 'scroll_documentation_up', 'fallback', },
       ['<c-d>'] = { 'scroll_documentation_down', 'fallback', },
-    },
-    snippets = {
-      expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require('luasnip').jumpable(filter.direction)
-        end
-        return require('luasnip').in_snippet()
-      end,
-      jump = function(direction) require('luasnip').jump(direction) end,
     },
     sources = {
       providers = {
