bf7d3de918a32280183ef2024b375238119a5862
Gianluca Randazzo <fusillicode@gmail.com>
2025-01-20 09:27:20 +0100

    little optimization

--- .config/nvim/lua/statuscolumn.lua
+++ .config/nvim/lua/statuscolumn.lua
@@ -1,11 +1,13 @@
 local M = {}
 
+M.draw_statuscolumn = require('rua').draw_statuscolumn
+
 function M.draw(cur_lnum)
   local line_signs = vim.api.nvim_buf_get_extmarks(
     vim.fn.bufnr(), -1, { cur_lnum - 1, 0, }, { cur_lnum - 1, -1, },
     { type = 'sign', details = true, overlap = false, }
   )
-  return require('rua').draw_statuscolumn(cur_lnum, line_signs)
+  return M.draw_statuscolumn(cur_lnum, line_signs)
 end
 
 return M
