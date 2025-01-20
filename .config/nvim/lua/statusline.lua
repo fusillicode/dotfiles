3f9d5a5b71df1bca7f245f7827967222a41c0dd7
Gianluca Randazzo <fusillicode@gmail.com>
2025-01-20 07:54:57 +0100

    little optimization to statusline.lua

--- .config/nvim/lua/statusline.lua
+++ .config/nvim/lua/statusline.lua
@@ -1,15 +1,14 @@
--- Thank you ChatGPT
-local function current_buffer_path()
-  return vim.fn.fnamemodify(
+local M = {}
+
+M.draw_statusline = require('rua').draw_statusline
+
+function M.draw()
+  -- Thank you ChatGPT
+  local current_buffer_path = vim.fn.fnamemodify(
     vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(vim.fn.win_getid(vim.fn.winnr('#')))),
     ':~:.'
   )
-end
-
-local M = {}
-
-function M.draw()
-  return require('rua').draw_statusline(vim.fn.bufnr(), current_buffer_path(), vim.diagnostic.get())
+  return M.draw_statusline(vim.fn.bufnr(), current_buffer_path, vim.diagnostic.get())
 end
 
 vim.api.nvim_create_autocmd({ 'DiagnosticChanged', 'BufEnter', }, {
