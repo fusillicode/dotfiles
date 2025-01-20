40c3b4fd48598b118e184250d278056e67d27f37
Gianluca Randazzo <fusillicode@gmail.com>
2025-01-20 08:55:13 +0100

    something is working, multiple diag signs + v:lnum still in lua

--- .config/nvim/lua/statuscolumn.lua
+++ .config/nvim/lua/statuscolumn.lua
@@ -1,39 +1,11 @@
 local M = {}
 
-local function format_extmark(extmark)
-  return (extmark and ('%#' .. extmark.sign_hl_group .. '#' .. vim.trim(extmark.sign_text) .. '%*') or ' ')
-end
-
 function M.draw(current_lnum)
   local line_signs = vim.api.nvim_buf_get_extmarks(
     vim.fn.bufnr(), -1, { current_lnum - 1, 0, }, { current_lnum - 1, -1, },
     { type = 'sign', details = true, overlap = false, }
   )
-  -- require('utils').dbg(line_signs)
-  -- require('rua').draw_statuscolumn(line_signs)
-
-  local git_sign, error_sign, warn_sign, hint_sign, info_sign, ok_sign
-  for _, sign in ipairs(line_signs) do
-    local sign_details = sign[4]
-
-    if sign_details.sign_hl_group:sub(1, 8) == 'GitSigns' then
-      git_sign = sign_details
-    elseif sign_details.sign_hl_group == 'DiagnosticSignError' then
-      error_sign = sign_details
-    elseif sign_details.sign_hl_group == 'DiagnosticSignWarn' then
-      warn_sign = sign_details
-    elseif sign_details.sign_hl_group == 'DiagnosticSignInfo' then
-      info_sign = sign_details
-    elseif sign_details.sign_hl_group == 'DiagnosticSignHint' then
-      hint_sign = sign_details
-    elseif sign_details.sign_hl_group == 'DiagnosticSignOk' then
-      ok_sign = sign_details
-    end
-  end
-
-  return format_extmark(error_sign or warn_sign or info_sign or hint_sign or ok_sign)
-      .. format_extmark(git_sign)
-      .. ' %=%{v:lnum} '
+  return require('rua').draw_statuscolumn(line_signs) .. ' %=%{v:lnum} '
 end
 
 return M
--- yog/src/rua/src/statuscolumn.rs
+++ yog/src/rua/src/statuscolumn.rs
@@ -1,29 +1,47 @@
 use mlua::prelude::*;
 
 pub fn draw(_lua: &Lua, signs: Signs) -> LuaResult<String> {
-    // local git_sign, error_sign, warn_sign, hint_sign, info_sign, ok_sign
-    // for _, sign in ipairs(line_signs) do
-    //   local sign_details = sign[4]
-    //
-    //   if sign_details.sign_hl_group:sub(1, 8) == 'GitSigns' then
-    //     git_sign = sign_details
-    //   elseif sign_details.sign_hl_group == 'DiagnosticSignError' then
-    //     error_sign = sign_details
-    //   elseif sign_details.sign_hl_group == 'DiagnosticSignWarn' then
-    //     warn_sign = sign_details
-    //   elseif sign_details.sign_hl_group == 'DiagnosticSignInfo' then
-    //     info_sign = sign_details
-    //   elseif sign_details.sign_hl_group == 'DiagnosticSignHint' then
-    //     hint_sign = sign_details
-    //   elseif sign_details.sign_hl_group == 'DiagnosticSignOk' then
-    //     ok_sign = sign_details
-    //   end
-    // end
-    //
-    // return format_extmark(error_sign or warn_sign or info_sign or hint_sign or ok_sign)
-    //     .. format_extmark(git_sign)
-    //     .. ' %=%{v:lnum} '
-    Ok("foo".into())
+    let mut statuscolumn = Statuscolumn::default();
+
+    for sign in signs.0 {
+        match sign.sign_hl_group.as_str() {
+            "DiagnosticSignError" => statuscolumn.error = Some(sign),
+            "DiagnosticSignWarn" => statuscolumn.warn = Some(sign),
+            "DiagnosticSignInfo" => statuscolumn.info = Some(sign),
+            "DiagnosticSignHint" => statuscolumn.hint = Some(sign),
+            "DiagnosticSignOk" => statuscolumn.ok = Some(sign),
+            git if git.contains("GitSigns") => statuscolumn.git = Some(sign),
+            _ => (),
+        };
+    }
+
+    Ok(statuscolumn.draw())
+}
+
+#[derive(Default)]
+struct Statuscolumn {
+    error: Option<Sign>,
+    warn: Option<Sign>,
+    info: Option<Sign>,
+    hint: Option<Sign>,
+    ok: Option<Sign>,
+    git: Option<Sign>,
+}
+
+impl Statuscolumn {
+    fn draw(&self) -> String {
+        [
+            &self.error,
+            &self.warn,
+            &self.info,
+            &self.hint,
+            &self.ok,
+            &self.git,
+        ]
+        .iter()
+        .filter_map(|s| s.as_ref().map(Sign::draw))
+        .collect()
+    }
 }
 
 pub struct Signs(Vec<Sign>);
@@ -46,9 +64,6 @@ impl FromLua for Signs {
 }
 
 pub struct Sign {
-    ns_id: i64,
-    priority: usize,
-    right_gravity: bool,
     sign_hl_group: String,
     sign_text: String,
 }
@@ -58,9 +73,6 @@ impl FromLua for Sign {
         if let LuaValue::Table(table) = value {
             let sign = table.get::<LuaTable>(4)?;
             return Ok(Sign {
-                ns_id: sign.get("ns_id")?,
-                priority: sign.get("priority")?,
-                right_gravity: sign.get("right_gravity")?,
                 sign_hl_group: sign.get("sign_hl_group")?,
                 sign_text: sign.get("sign_text")?,
             });
