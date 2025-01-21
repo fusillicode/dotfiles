57c908bb1414193e61853fc9825617a6ad2e3867
Gianluca Randazzo <fusillicode@gmail.com>
2025-01-21 16:00:11 +0100

    move types where they re used

--- yog/src/rua/src/diagnostics/formatting.rs
+++ yog/src/rua/src/diagnostics/formatting.rs
@@ -1,7 +1,5 @@
 use mlua::prelude::*;
 
-use crate::diagnostics::Diagnostic;
-
 /// Returns the formatted [`String`] representation of an LSP diagnostic.
 pub fn format_diagnostic(_lua: &Lua, diag: Diagnostic) -> LuaResult<String> {
     let msg = get_msg(&diag).map_or_else(
@@ -47,4 +45,100 @@ fn get_code(diag: &Diagnostic) -> Option<&str> {
         .as_ref()
         .and_then(|user_data| user_data.lsp.as_ref().and_then(|lsp| lsp.code.as_deref()))
         .or(diag.code.as_deref())
+}
+
+#[derive(Debug)]
+pub struct Diagnostic {
+    source: Option<String>,
+    code: Option<String>,
+    message: Option<String>,
+    user_data: Option<UserData>,
+}
+
+#[derive(Debug)]
+pub struct UserData {
+    lsp: Option<Lsp>,
+    message: Option<String>,
+}
+
+#[derive(Debug)]
+pub struct Lsp {
+    source: Option<String>,
+    code: Option<String>,
+    data: Option<LspData>,
+}
+
+#[derive(Debug)]
+pub struct LspData {
+    rendered: Option<String>,
+}
+
+impl FromLua for Diagnostic {
+    fn from_lua(value: mlua::Value, _lua: &mlua::Lua) -> mlua::Result<Self> {
+        if let LuaValue::Table(table) = value {
+            let out = Self {
+                source: table.get("source")?,
+                code: table.get("code")?,
+                message: table.get("message")?,
+                user_data: table.get("user_data")?,
+            };
+            return Ok(out);
+        }
+        Err(mlua::Error::FromLuaConversionError {
+            from: value.type_name(),
+            to: "Diagnostic".into(),
+            message: Some(format!("expected a table got {value:?}")),
+        })
+    }
+}
+
+impl FromLua for UserData {
+    fn from_lua(value: LuaValue, _lua: &Lua) -> LuaResult<Self> {
+        if let LuaValue::Table(table) = value {
+            let out = UserData {
+                message: table.get("message")?,
+                lsp: table.get("lsp")?,
+            };
+            return Ok(out);
+        }
+        Err(mlua::Error::FromLuaConversionError {
+            from: value.type_name(),
+            to: "UserData".into(),
+            message: Some(format!("expected a table got {value:?}")),
+        })
+    }
+}
+
+impl FromLua for Lsp {
+    fn from_lua(value: LuaValue, _lua: &Lua) -> LuaResult<Self> {
+        if let LuaValue::Table(table) = value {
+            let out = Lsp {
+                source: table.get("source")?,
+                code: table.get("code")?,
+                data: table.get("data")?,
+            };
+            return Ok(out);
+        }
+        Err(mlua::Error::FromLuaConversionError {
+            from: value.type_name(),
+            to: "Lsp".into(),
+            message: Some(format!("expected a table got {value:?}")),
+        })
+    }
+}
+
+impl FromLua for LspData {
+    fn from_lua(value: LuaValue, _lua: &Lua) -> LuaResult<Self> {
+        if let LuaValue::Table(table) = value {
+            let out = LspData {
+                rendered: table.get("rendered")?,
+            };
+            return Ok(out);
+        }
+        Err(mlua::Error::FromLuaConversionError {
+            from: value.type_name(),
+            to: "LspData".into(),
+            message: Some(format!("expected a table got {value:?}")),
+        })
+    }
 }
