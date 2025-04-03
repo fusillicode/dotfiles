bd04a6abe03cab88570efe6cec6a026cb2e4705c
Gianluca Randazzo <fusillicode@gmail.com>
2025-04-03 14:03:42 +0200

    sanity rename?

--- yog/src/fkr/src/main.rs
+++ yog/src/fkr/src/main.rs
@@ -3,16 +3,15 @@
 use fake::Fake;
 use inquire::error::InquireResult;
 use inquire::ui::RenderConfig;
+use inquire::InquireError;
 use inquire::Select;
 use strum::IntoEnumIterator;
 use strum_macros::Display;
 use strum_macros::EnumIter;
 
 fn main() -> anyhow::Result<()> {
-    let selection_res = minimal_select(Dummy::iter().collect()).cancellable_prompt()?;
-
-    if let Some(selected_dummy) = selection_res {
-        println!("{}", selected_dummy.gen())
+    if let Some(selected_opt) = minimal_select(FkrOption::iter().collect()).cancellable_prompt()? {
+        println!("{}", selected_opt.gen_string())
     }
 
     Ok(())
@@ -25,18 +24,18 @@ trait SelectExt<'a, T: std::fmt::Display> {
 impl<'a, T: std::fmt::Display> SelectExt<'a, T> for Select<'a, T> {
     fn cancellable_prompt(self) -> InquireResult<Option<T>> {
         self.prompt().map(Some).or_else(|e| match e {
-            inquire::InquireError::OperationCanceled
-            | inquire::InquireError::OperationInterrupted => Ok(None),
-            inquire::InquireError::NotTTY
-            | inquire::InquireError::InvalidConfiguration(_)
-            | inquire::InquireError::IO(_)
-            | inquire::InquireError::Custom(_) => Err(e),
+            InquireError::OperationCanceled
+            | InquireError::OperationInterrupted => Ok(None),
+            InquireError::NotTTY
+            | InquireError::InvalidConfiguration(_)
+            | InquireError::IO(_)
+            | InquireError::Custom(_) => Err(e),
         })
     }
 }
 
-fn minimal_select<'a, T: std::fmt::Display>(items: Vec<T>) -> Select<'a, T> {
-    Select::new("", items)
+fn minimal_select<'a, T: std::fmt::Display>(options: Vec<T>) -> Select<'a, T> {
+    Select::new("", options)
         .with_render_config(minimal_render_config())
         .without_help_message()
 }
@@ -49,7 +48,7 @@ fn minimal_render_config<'a>() -> RenderConfig<'a> {
 }
 
 #[derive(EnumIter, Display)]
-pub enum Dummy {
+pub enum FkrOption {
     Uuidv4,
     Uuidv7,
     Email,
@@ -59,16 +58,16 @@ pub enum Dummy {
     MACAddress,
 }
 
-impl Dummy {
-    pub fn gen(&self) -> String {
+impl FkrOption {
+    pub fn gen_string(&self) -> String {
         match self {
-            Dummy::Uuidv4 => fake::uuid::UUIDv4.fake::<String>(),
-            Dummy::Uuidv7 => fake::uuid::UUIDv7.fake::<String>(),
-            Dummy::Email => fake::faker::internet::en::SafeEmail().fake::<String>(),
-            Dummy::UserAgent => fake::faker::internet::en::UserAgent().fake::<String>(),
-            Dummy::MACAddress => fake::faker::internet::en::MACAddress().fake::<String>(),
-            Dummy::IPv4 => fake::faker::internet::en::IPv4().fake::<String>(),
-            Dummy::IPv6 => fake::faker::internet::en::IPv6().fake::<String>(),
+            FkrOption::Uuidv4 => fake::uuid::UUIDv4.fake::<String>(),
+            FkrOption::Uuidv7 => fake::uuid::UUIDv7.fake::<String>(),
+            FkrOption::Email => fake::faker::internet::en::SafeEmail().fake::<String>(),
+            FkrOption::UserAgent => fake::faker::internet::en::UserAgent().fake::<String>(),
+            FkrOption::MACAddress => fake::faker::internet::en::MACAddress().fake::<String>(),
+            FkrOption::IPv4 => fake::faker::internet::en::IPv4().fake::<String>(),
+            FkrOption::IPv6 => fake::faker::internet::en::IPv6().fake::<String>(),
         }
     }
 }
