7cb8b950fce8957149ece3faff1853fbacd3ab66
Gianluca Randazzo <fusillicode@gmail.com>
2025-04-06 11:18:59 +0200

    fkr cp

--- yog/src/fkr/src/main.rs
+++ yog/src/fkr/src/main.rs
@@ -7,11 +7,21 @@ use inquire::InquireError;
 use inquire::Select;
 
 /// Prints on terminal a fake value generated on the fly based on the selection.
+/// If "cp" is supplied as first argument also copies the generated value to the system clipboard.
 /// If the selection is cancelled (<ESC>) or interrupted (<CTRL-C>) exists with success without
 /// printing anything.
 fn main() -> anyhow::Result<()> {
-    if let Some(selected_opt) = minimal_select(FkrOption::to_vec()).cancellable_prompt()? {
-        println!("{}", selected_opt.gen_string())
+    if let Some(generated_value) = minimal_select(FkrOption::to_vec())
+        .cancellable_prompt()?
+        .map(|fkr_opt| fkr_opt.gen_string())
+    {
+        println!("{generated_value}");
+        if utils::system::get_args()
+            .first()
+            .is_some_and(|arg| arg == "cp")
+        {
+            utils::system::copy_to_system_clipboard(&mut generated_value.as_bytes())?;
+        }
     }
     Ok(())
 }
