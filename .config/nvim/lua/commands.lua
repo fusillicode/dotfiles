85bb2997a7c79d31643ffa4d89c543273294715a
Gian Lu <fusillicode@users.noreply.github.com>
2025-04-06 11:10:06 +0200

    [206]: Expose fkr lib to rua and implement dummy uuid gen for nvim (#208)
    * bumper
    
    * something
    
    * its working even if its ugly
    
    * strum in workspace
    
    * bumper
    
    * sanity rename
    
    * remove unneeded stuff
    
    * bashing
    
    * some comments
    
    * bumper

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -5,6 +5,15 @@ vim.api.nvim_create_user_command('LazyUpdate', ':Lazy update', {})
 vim.api.nvim_create_user_command('SelectAll', 'normal! ggVG', {})
 vim.api.nvim_create_user_command('Messages', ':Messages', {})
 
+local rua = require('rua')
+local utils = require('utils')
+for _, cmd in ipairs(rua.get_fkr_cmds()) do
+  vim.api.nvim_create_user_command(cmd.name, function()
+    local row, col = utils.unpack(vim.api.nvim_win_get_cursor(0))
+    vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { rua.gen_fkr_value(cmd.fkr_arg), })
+  end, {})
+end
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('YankHighlight', { clear = true, }),
   pattern = '*',
