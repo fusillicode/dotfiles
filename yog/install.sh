ae210c638123bbdd5e59b1672854f74943d74b40
Gianluca Randazzo <fusillicode@gmail.com>
2025-04-19 13:34:58 +0200

    bashing install.sh

--- yog/install.sh
+++ yog/install.sh
@@ -3,27 +3,26 @@
 set -euo pipefail
 
 bins_path="$HOME/.local/bin"
-yog_release_path="$PWD/target/release"
+target_path="$PWD/target"
+cargo_build_profile=
+
+is_debug=$([[ "${1:-}" == "--debug" ]] && echo true || echo false)
+if $is_debug; then
+  target_location="debug"
+else
+  target_location="release"
+  cargo_build_profile="--$target_location"
+fi
+
+echo "Installing in $target_location mode"
+target_path+="/$target_location"
 
 cargo fmt && \
 cargo clippy --all-targets --all-features -- -D warnings && \
-cargo build --release && \
-    rm -f "$bins_path/idt" && \
-    ln -s "$yog_release_path/idt" "$bins_path" && \
-    rm -f "$bins_path/yghfl" && \
-    ln -s "$yog_release_path/yghfl" "$bins_path" && \
-    rm -f "$bins_path/yhfp" && \
-    ln -s "$yog_release_path/yhfp" "$bins_path" && \
-    rm -f "$bins_path/oe" && \
-    ln -s "$yog_release_path/oe" "$bins_path" && \
-    rm -f "$bins_path/catl" && \
-    ln -s "$yog_release_path/catl" "$bins_path" && \
-    rm -f "$bins_path/gcu" && \
-    ln -s "$yog_release_path/gcu" "$bins_path"
-    rm -f "$bins_path/vpg" && \
-    ln -s "$yog_release_path/vpg" "$bins_path" && \
-    rm -f "$bins_path/try" && \
-    ln -s "$yog_release_path/try" "$bins_path" && \
-    rm -f "$bins_path/fkr" && \
-    ln -s "$yog_release_path/fkr" "$bins_path" && \
-    mv "$yog_release_path/librua.dylib" "$yog_release_path/rua.so"
+cargo build $cargo_build_profile
+
+for binary in idt yghfl yhfp oe catl gcu vpg try fkr; do
+  rm -f "$bins_path/$binary"
+  ln -s "$target_path/$binary" "$bins_path"
+done
+mv "$target_path/librua.dylib" "$target_path/rua.so"
