a3c0f13e4a6e948c36c112fdf0b9226d3ca143e9
fusillicode <2248534+fusillicode@users.noreply.github.com>
2025-07-02 19:33:20 +0200

    more space for code editing

--- .wezterm.lua
+++ .wezterm.lua
@@ -88,7 +88,7 @@ config.window_decorations = 'RESIZE'
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0, }
 config.window_frame = { active_titlebar_bg = background, inactive_titlebar_bg = background, }
 
-local split_perc = 0.66
+local split_perc = 0.69
 
 -- 🥲 https://github.com/wez/wezterm/issues/3173
 wezterm.on('window-config-reloaded', function(window, _)
