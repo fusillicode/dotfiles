8eb05be609d4f76f0b65c610c566b10ef49fe692
Gianluca Randazzo <fusillicode@gmail.com>
2025-08-01 18:56:17 +0200

    fix text-transform

--- .config/nvim/lua/plugins/text-transform.lua
+++ .config/nvim/lua/plugins/text-transform.lua
@@ -4,6 +4,12 @@ return {
   config = function()
     require('text-transform').setup({
       popup_type = 'select',
+      keymap = {
+        telescope_popup = {
+          ['n'] = '<leader>u',
+          ['v'] = '<leader>u',
+        },
+      },
     })
   end,
 }
