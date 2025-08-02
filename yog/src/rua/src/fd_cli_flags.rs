09701c186a142da1bcfdaec6888f41150700488a
Gianluca Randazzo <fusillicode@gmail.com>
2025-08-02 06:30:42 +0200

    reaching for abstractions

--- yog/src/rua/src/fd_cli_flags.rs
+++ yog/src/rua/src/fd_cli_flags.rs
@@ -2,17 +2,18 @@ use mlua::prelude::*;
 
 use crate::globs::EXCLUSIONS;
 
+pub const BASE_FLAGS: [&str; 5] = [
+    "--color never",
+    "--follow",
+    "--hidden",
+    "--no-ignore-vcs",
+    "--type f",
+];
+
 /// Returns fd CLI flags as a [`LuaTable`].
 pub fn get(lua: &Lua, _: Option<LuaString>) -> LuaResult<LuaTable> {
-    let base_opts = [
-        "--color never",
-        "--follow",
-        "--hidden",
-        "--no-ignore-vcs",
-        "--type f",
-    ];
-    let glob_opts = EXCLUSIONS
+    let glob_flags = EXCLUSIONS
         .into_iter()
         .map(|glob| format!("--exclude '{glob}'"));
-    lua.create_sequence_from(base_opts.into_iter().map(Into::into).chain(glob_opts))
+    lua.create_sequence_from(BASE_FLAGS.into_iter().map(Into::into).chain(glob_flags))
 }
