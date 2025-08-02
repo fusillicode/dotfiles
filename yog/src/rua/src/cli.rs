08e4bc8ae359a9c17d36ac57ddc3a0eb502e35e5
Gianluca Randazzo <fusillicode@gmail.com>
2025-08-02 08:26:00 +0200

    no self ref as object safety is not required now

--- yog/src/rua/src/cli.rs
+++ yog/src/rua/src/cli.rs
@@ -12,20 +12,17 @@ pub const GLOB_BLACKLIST: [&str; 6] = [
 ];
 
 pub trait Flags {
-    fn base_flags(&self) -> Vec<&str>;
+    fn base_flags() -> Vec<&'static str>;
 
-    fn format_glob(&self, glob: &str) -> String;
+    fn glob_flag(glob: &str) -> String;
 
     fn get(&self) -> LuaFunction<'_> {
         Box::new(|lua: &Lua, _: Option<LuaString>| {
-            let glob_flags = GLOB_BLACKLIST
-                .into_iter()
-                .map(|glob| self.format_glob(glob));
             lua.create_sequence_from(
-                self.base_flags()
+                Self::base_flags()
                     .into_iter()
                     .map(Into::into)
-                    .chain(glob_flags),
+                    .chain(GLOB_BLACKLIST.into_iter().map(Self::glob_flag)),
             )
         })
     }
