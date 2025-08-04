c84e25b0815b89a77e8a9803e13bfe82dfb5e23d
Gianluca Randazzo <fusillicode@gmail.com>
2025-08-04 08:26:05 +0200

    lets try this thing...

--- .config/nvim/lua/plugins/copilot-chat-nvim.lua
+++ .config/nvim/lua/plugins/copilot-chat-nvim.lua
@@ -1,12 +1,41 @@
 return {
-  {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    dependencies = {
-      { 'nvim-lua/plenary.nvim', branch = 'master', },
-    },
-    build = 'make tiktoken',
-    config = function()
-      require('CopilotChat').setup({})
-    end,
+  'CopilotC-Nvim/CopilotChat.nvim',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim',                     branch = 'master', },
+    { 'MeanderingProgrammer/render-markdown.nvim', dependencies = { 'nvim-treesitter/nvim-treesitter', }, },
   },
+  build = 'make tiktoken',
+  config = function()
+    local copilot_chat = require('CopilotChat')
+
+    copilot_chat.setup({
+      auto_follow_cursor = false,
+      highlight_headers = false,
+      insert_at_end = true,
+      separator = '---',
+      error_header = '> [!ERROR] Error',
+      selection = function(source)
+        return require('CopilotChat.select').visual(source) or
+            require('CopilotChat.select').buffer(source)
+      end,
+    })
+
+    require('keymaps').copilot_chat(copilot_chat)
+
+    vim.api.nvim_create_autocmd('BufEnter', {
+      pattern = 'copilot-*',
+      callback = function()
+        vim.opt_local.relativenumber = false
+        vim.opt_local.number = false
+        vim.opt_local.conceallevel = 0
+      end,
+    })
+
+    require('render-markdown').setup({
+      completions = {
+        blink = { enabled = true, },
+        lsp = { enabled = true, },
+      },
+    })
+  end,
 }
