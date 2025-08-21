71f023cfb8aa2216dbe2149698fe2dc23b876447
fusillicode <2248534+fusillicode@users.noreply.github.com>
2025-08-21 09:10:29 +0200

    dbeeeeeee

--- .config/nvim/lua/plugins/nvim-dbee.lua
+++ .config/nvim/lua/plugins/nvim-dbee.lua
@@ -3,6 +3,12 @@ return {
   dependencies = { 'MunifTanjim/nui.nvim', },
   build = function() require('dbee').install() end,
   config = function()
-    require('dbee').setup()
+    require('dbee').setup({
+      sources = {
+        require('dbee.sources').FileSource:new(
+          vim.env.HOME .. '/.local/state/nvim/dbee/conns.json'
+        ),
+      },
+    })
   end,
 }
