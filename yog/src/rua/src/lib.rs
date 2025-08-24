acac84edd7dd60c63bfa2c770a0b8326e6466bab
fusillicode <2248534+fusillicode@users.noreply.github.com>
2025-08-24 14:09:31 +0200

    log_error become log_result

--- yog/src/rua/src/lib.rs
+++ yog/src/rua/src/lib.rs
@@ -1,7 +1,3 @@
-use std::fs::OpenOptions;
-use std::io::Write;
-
-use anyhow::anyhow;
 use mlua::prelude::*;
 
 use crate::cli::Flags;
@@ -14,7 +10,7 @@ mod statusline;
 mod test_runner;
 mod utils;
 
-/// Entrypoint of Rust exported fns.
+/// Entrypoint of Rust exported functions.
 #[mlua::lua_module]
 fn rua(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table()?;
@@ -56,25 +52,40 @@ where
 {
     lua.create_function(move |lua, args: A| {
         let res = func(lua, args);
-        if cfg!(debug_assertions)
-            && let Err(ref error) = res
-        {
-            log_error(error)?;
-        }
+        #[cfg(debug_assertions)]
+        log_result(&res)?;
         res.map_err(mlua::Error::from)
     })
 }
 
 #[cfg(debug_assertions)]
-fn log_error(error: &anyhow::Error) -> anyhow::Result<()> {
+fn log_result<R: IntoLuaMulti + std::fmt::Debug>(res: &Result<R, anyhow::Error>) -> anyhow::Result<()> {
+    use std::fs::OpenOptions;
+    use std::io::Write;
+
+    use anyhow::anyhow;
+
     let log_path = ::utils::system::build_home_path(&[".local", "state", "nvim", "rua.log"]).map_err(|e| anyhow!(e))?;
     let mut log_file = OpenOptions::new().append(true).create(true).open(log_path)?;
-    let log_msg = serde_json::json!({
-        "timestamp": chrono::Utc::now(),
-        "error": format!("{error:#?}"),
-        "source": format!("{:#?}", error.source()),
-        "backtrace": format!("{:#?}", error.backtrace())
-    });
+
+    let now = chrono::Utc::now();
+    let log_msg = res.as_ref().map_or_else(
+        |error| {
+            serde_json::json!({
+                "timestamp": now,
+                "error": format!("{error:#?}"),
+                "source": format!("{:#?}", error.source()),
+                "backtrace": format!("{:#?}", error.backtrace())
+            })
+        },
+        |r| {
+            serde_json::json!({
+                "timestamp": now,
+                "result": format!("{r:#?}"),
+            })
+        },
+    );
     writeln!(log_file, "{log_msg}")?;
+
     Ok(())
 }
