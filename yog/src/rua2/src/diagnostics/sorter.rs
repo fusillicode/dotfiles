13e5276a6a252386b22f3d4131959fc4d823c29b
Gianluca Randazzo <fusillicode@gmail.com>
2025-08-25 15:37:59 +0200

    BROKEN

--- yog/src/rua2/src/diagnostics/sorter.rs
+++ yog/src/rua2/src/diagnostics/sorter.rs
@@ -1,27 +1,20 @@
 use nvim_oxi::Dictionary;
 use nvim_oxi::Function;
+use nvim_oxi::Integer;
 use nvim_oxi::Object;
 
 pub fn sort() -> Object {
-    Object::from(Function::<Dictionary, nvim_oxi::Result<_>>::from_fn(sort_core))
+    Object::from(Function::<Vec<Dictionary>, nvim_oxi::Result<_>>::from_fn(sort_core))
 }
 
-pub fn sort_core(lsp_diags: Dictionary) -> nvim_oxi::Result<Dictionary> {
-    // let mut lsp_diags_by_severity = lsp_diags
-    //     .as_slice()
-    //     .iter()
-    //     .enumerate()
-    //     .map(|(idx, kv)| {
-    //         let dict = Dictionary::try_from(kv.value().clone()).unwrap();
-    //         let severity = dict.get("severity").and_then(|so| usize::try_from(so)).unwrap_or(idx);
-    //
-    //         (severity, dict)
-    //     })
-    //     .collect::<Vec<_>>();
-    //
-    // lsp_diags_by_severity.sort_by(|(sev_a, _), (sev_b, _)| sev_a.cmp(sev_b));
-    //
-    // Ok(lsp_diags_by_severity.into_iter().map(|(_, dict)| dict).collect())
-
+pub fn sort_core(mut lsp_diags: Vec<Dictionary>) -> nvim_oxi::Result<Vec<Dictionary>> {
+    lsp_diags.sort_by_key(get_severity_or_default);
     Ok(lsp_diags)
+}
+
+fn get_severity_or_default(dict: &Dictionary) -> Integer {
+    dict.get("severity")
+        .map(|o| Integer::try_from(o.clone()))
+        .unwrap_or(Ok(Integer::MIN))
+        .unwrap_or(Integer::MIN)
 }
---
+++ yog/src/rua2/src/statusline.rs
@@ -0,0 +1,197 @@
+use std::borrow::Cow;
+use std::collections::HashMap;
+
+use nvim_oxi::Function;
+use nvim_oxi::Object;
+use nvim_oxi::conversion::FromObject;
+use nvim_oxi::lua::ffi::State;
+use nvim_oxi::serde::Deserializer;
+use serde::Deserialize;
+
+pub fn draw() -> Object {
+    Object::from(Function::<Diagnostics, nvim_oxi::Result<_>>::from_fn(draw_core))
+}
+
+fn draw_core(diagnostics: Diagnostics) -> nvim_oxi::Result<String> {
+    let cur_buf = nvim_oxi::api::get_current_buf();
+    let cur_buf_path = cur_buf.get_name()?;
+    let cur_buf_nr = cur_buf.handle();
+
+    let mut statusline = Statusline {
+        cur_buf_path: cur_buf_path.to_string_lossy(),
+        cur_buf_diags: HashMap::new(),
+        workspace_diags: HashMap::new(),
+    };
+
+    for diagnostic in diagnostics.0 {
+        if cur_buf_nr == diagnostic.bufnr {
+            *statusline.cur_buf_diags.entry(diagnostic.severity).or_insert(0) += 1;
+        }
+        *statusline.workspace_diags.entry(diagnostic.severity).or_insert(0) += 1;
+    }
+
+    Ok(statusline.draw())
+}
+
+#[derive(Deserialize)]
+pub struct Diagnostics(Vec<Diagnostic>);
+
+impl FromObject for Diagnostics {
+    fn from_object(obj: Object) -> Result<Self, nvim_oxi::conversion::Error> {
+        Self::deserialize(Deserializer::new(obj)).map_err(Into::into)
+    }
+}
+
+impl nvim_oxi::lua::Poppable for Diagnostics {
+    unsafe fn pop(lstate: *mut State) -> Result<Self, nvim_oxi::lua::Error> {
+        unsafe {
+            let obj = Object::pop(lstate)?;
+            Self::from_object(obj).map_err(nvim_oxi::lua::Error::pop_error_from_err::<Self, _>)
+        }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct Diagnostic {
+    bufnr: i32,
+    severity: Severity,
+}
+
+impl FromObject for Diagnostic {
+    fn from_object(obj: Object) -> Result<Self, nvim_oxi::conversion::Error> {
+        Self::deserialize(Deserializer::new(obj)).map_err(Into::into)
+    }
+}
+
+impl nvim_oxi::lua::Poppable for Diagnostic {
+    unsafe fn pop(lstate: *mut State) -> Result<Self, nvim_oxi::lua::Error> {
+        unsafe {
+            let obj = Object::pop(lstate)?;
+            Self::from_object(obj).map_err(nvim_oxi::lua::Error::pop_error_from_err::<Self, _>)
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Hash, PartialEq, Eq, Copy, Clone)]
+pub enum Severity {
+    Error,
+    Warn,
+    Info,
+    Hint,
+}
+
+impl Severity {
+    const ORDER: &'static [Self] = &[Self::Error, Self::Warn, Self::Info, Self::Hint];
+
+    fn draw_diagnostics(&self, diags_count: i32) -> String {
+        if diags_count == 0 {
+            return "".into();
+        }
+        let (hg_group, sym) = match self {
+            Severity::Error => ("DiagnosticStatusLineError", "E"),
+            Severity::Warn => ("DiagnosticStatusLineWarn", "W"),
+            Severity::Info => ("DiagnosticStatusLineInfo", "I"),
+            Severity::Hint => ("DiagnosticStatusLineHint", "H"),
+        };
+        format!("%#{hg_group}#{sym}:{diags_count}")
+    }
+}
+
+#[derive(Debug)]
+struct Statusline<'a> {
+    cur_buf_path: Cow<'a, str>,
+    cur_buf_diags: HashMap<Severity, i32>,
+    workspace_diags: HashMap<Severity, i32>,
+}
+
+impl<'a> Statusline<'a> {
+    fn draw(&self) -> String {
+        let mut cur_buf_diags = Severity::ORDER
+            .iter()
+            .filter_map(|s| self.cur_buf_diags.get(s).map(|c| s.draw_diagnostics(*c)))
+            .collect::<Vec<_>>()
+            .join(" ");
+
+        let workspace_diags = Severity::ORDER
+            .iter()
+            .filter_map(|s| self.workspace_diags.get(s).map(|c| s.draw_diagnostics(*c)))
+            .collect::<Vec<_>>()
+            .join(" ");
+
+        if !cur_buf_diags.is_empty() {
+            cur_buf_diags.push(' ');
+        }
+
+        format!(
+            "{cur_buf_diags}%#StatusLine#{} %m %r%={workspace_diags}",
+            self.cur_buf_path
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_status_line_draw_works_as_expected() {
+        for statusline in [
+            Statusline {
+                cur_buf_path: "foo".into(),
+                cur_buf_diags: HashMap::new(),
+                workspace_diags: HashMap::new(),
+            },
+            Statusline {
+                cur_buf_path: "foo".into(),
+                cur_buf_diags: [(Severity::Info, 0)].into_iter().collect(),
+                workspace_diags: HashMap::new(),
+            },
+            Statusline {
+                cur_buf_path: "foo".into(),
+                cur_buf_diags: HashMap::new(),
+                workspace_diags: [(Severity::Info, 0)].into_iter().collect(),
+            },
+            Statusline {
+                cur_buf_path: "foo".into(),
+                cur_buf_diags: [(Severity::Info, 0)].into_iter().collect(),
+                workspace_diags: [(Severity::Info, 0)].into_iter().collect(),
+            },
+        ] {
+            let res = statusline.draw();
+            assert_eq!(
+                "%#StatusLine#foo %m %r%=", &res,
+                "unexpected not empty diagnosticts drawn, res {res}, statusline {statusline:#?}"
+            );
+        }
+
+        let statusline = Statusline {
+            cur_buf_path: "foo".into(),
+            cur_buf_diags: [(Severity::Info, 1), (Severity::Error, 3)].into_iter().collect(),
+            workspace_diags: [(Severity::Info, 0)].into_iter().collect(),
+        };
+        assert_eq!(
+            "%#DiagnosticStatusLineError#E:3 %#DiagnosticStatusLineInfo#I:1 %#StatusLine#foo %m %r%=",
+            &statusline.draw()
+        );
+
+        let statusline = Statusline {
+            cur_buf_path: "foo".into(),
+            cur_buf_diags: [(Severity::Info, 0)].into_iter().collect(),
+            workspace_diags: [(Severity::Info, 1), (Severity::Error, 3)].into_iter().collect(),
+        };
+        assert_eq!(
+            "%#StatusLine#foo %m %r%=%#DiagnosticStatusLineError#E:3 %#DiagnosticStatusLineInfo#I:1",
+            &statusline.draw()
+        );
+
+        let statusline = Statusline {
+            cur_buf_path: "foo".into(),
+            cur_buf_diags: [(Severity::Hint, 3), (Severity::Warn, 2)].into_iter().collect(),
+            workspace_diags: [(Severity::Info, 1), (Severity::Error, 3)].into_iter().collect(),
+        };
+        assert_eq!(
+            "%#DiagnosticStatusLineWarn#W:2 %#DiagnosticStatusLineHint#H:3 %#StatusLine#foo %m %r%=%#DiagnosticStatusLineError#E:3 %#DiagnosticStatusLineInfo#I:1",
+            &statusline.draw()
+        );
+    }
+}
