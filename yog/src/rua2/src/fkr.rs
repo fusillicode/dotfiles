69a083a743d7173602f745f640dbc307db57daa4
Gianluca Randazzo <fusillicode@gmail.com>
2025-08-26 14:38:59 +0200

    oxi print in case of errors

--- yog/src/rua2/src/fkr.rs
+++ yog/src/rua2/src/fkr.rs
@@ -15,9 +15,26 @@ fn create_cmds_core(_: ()) -> nvim_oxi::Result<()> {
         nvim_oxi::api::create_user_command(
             cmd_name(&fkr_opt),
             move |_| -> nvim_oxi::Result<()> {
+                let cur_win = Window::current();
+                let Ok((row, col)) = cur_win
+                    .get_cursor()
+                    .inspect_err(|e| nvim_oxi::print!("error getting cursor from window {cur_win:?}, error {e:?}"))
+                else {
+                    return Ok(());
+                };
+
+                let row = row.saturating_sub(1);
+                let line_range = row..row;
+                let start_col = col;
+                let end_col = col;
+                let repl = fkr_opt.gen_string();
+
                 let mut cur_buf = Buffer::current();
-                let (row, col) = Window::current().get_cursor()?;
-                cur_buf.set_text(row..row + 1, col, col, vec![fkr_opt.gen_string()])?;
+                if let Err(e) = cur_buf.set_text(line_range.clone(), start_col, end_col, vec![repl.clone()]) {
+                    nvim_oxi::print!(
+                        "error setting text {repl:?} on buffer {cur_buf:?}, line_range {line_range:?}, start_col {start_col:?}, end_col {end_col:?}, error {e:?}"
+                    )
+                }
 
                 Ok(())
             },
