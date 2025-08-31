a38127651c700f90aa977fce73ae845772d65bb8
Gianluca Randazzo <fusillicode@gmail.com>
2025-08-31 08:51:07 +0200

    dict macro

--- yog/src/rua/src/lib.rs
+++ yog/src/rua/src/lib.rs
@@ -24,30 +24,15 @@ mod test_runner;
 /// The main plugin function that returns a [`Dictionary`] of Lua functions exposed to Neovim.
 #[nvim_oxi::plugin]
 fn rua() -> Dictionary {
-    Dictionary::from_iter([
-        (
-            "format_diagnostic",
-            Object::from(Function::from_fn(diagnostics::formatter::format)),
-        ),
-        (
-            "sort_diagnostics",
-            Object::from(Function::from_fn(diagnostics::sorter::sort)),
-        ),
-        (
-            "filter_diagnostics",
-            Object::from(Function::from_fn(diagnostics::filter::filter)),
-        ),
-        ("draw_statusline", Object::from(Function::from_fn(statusline::draw))),
-        ("draw_statuscolumn", Object::from(Function::from_fn(statuscolumn::draw))),
-        ("create_fkr_cmds", Object::from(Function::from_fn(fkr::create_cmds))),
-        (
-            "get_fd_cli_flags",
-            Object::from(Function::from_fn(cli_flags::fd::FdCliFlags.get())),
-        ),
-        (
-            "get_rg_cli_flags",
-            Object::from(Function::from_fn(cli_flags::rg::RgCliFlags.get())),
-        ),
-        ("run_test", Object::from(Function::from_fn(test_runner::run_test))),
-    ])
+    dict! {
+        "format_diagnostic": Object::from(Function::from_fn(diagnostics::formatter::format)),
+        "sort_diagnostics": Object::from(Function::from_fn(diagnostics::sorter::sort)),
+        "filter_diagnostics": Object::from(Function::from_fn(diagnostics::filter::filter)),
+        "draw_statusline": Object::from(Function::from_fn(statusline::draw)),
+        "draw_statuscolumn": Object::from(Function::from_fn(statuscolumn::draw)),
+        "create_fkr_cmds": Object::from(Function::from_fn(fkr::create_cmds)),
+        "get_fd_cli_flags": Object::from(Function::from_fn(cli_flags::fd::FdCliFlags.get())),
+        "get_rg_cli_flags": Object::from(Function::from_fn(cli_flags::rg::RgCliFlags.get())),
+        "run_test": Object::from(Function::from_fn(test_runner::run_test)),
+    }
 }
