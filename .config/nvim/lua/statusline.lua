6c84f21f88614a754baff224f2aa380a78b85d10
Gianluca Randazzo <fusillicode@gmail.com>
2025-09-04 21:24:41 +0200

    fix regression on statusline, error E730

--- .config/nvim/lua/statusline.lua
+++ .config/nvim/lua/statusline.lua
@@ -2,6 +2,9 @@ local M = {}
 
 local statusline = require('nvrim').statusline
 
+-- Called from vim.opt.statusline (Rust side). We'll see if I can get rid of this at some point...
+function M.draw() return statusline.draw(vim.diagnostic.get()) end
+
 vim.api.nvim_create_autocmd({ 'DiagnosticChanged', 'BufEnter', }, {
   group = vim.api.nvim_create_augroup('StatusLine', {}),
   callback = function() vim.o.statusline = statusline.draw(vim.diagnostic.get()) end,
