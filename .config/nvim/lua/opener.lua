3782677ae4043e6986af1e49c217ea83e42fc360
Gianluca Randazzo <fusillicode@gmail.com>
2025-09-05 18:05:27 +0200

    opener is now working (hopefully)

--- .config/nvim/lua/opener.lua
+++ .config/nvim/lua/opener.lua
@@ -3,9 +3,12 @@ local M = {}
 local buffer = require('nvrim').buffer
 
 function M.open_under_cursor()
-  local word = vim.fn.expand(buffer.get_word_under_cursor())
-  if word == nil or word == '' then return end
-  vim.fn.jobstart({ 'open', word, }, {
+  local word = buffer.get_word_under_cursor()
+  require('utils').dbg(word)
+  -- Do not open binary files or "words". Only opens URLs, directories, and text files.
+  if not word or word.kind == 'BinaryFile' or word.kind == 'Word' then return end
+
+  vim.fn.jobstart({ 'open', vim.fn.expand(word.value), }, {
     detach = true,
     on_exit = function(_, code, _)
       if code ~= 0 then
