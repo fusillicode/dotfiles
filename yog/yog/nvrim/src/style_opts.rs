3c04c3f5d3c20ebcc1fdbfe241151522c30bce45
Gianluca Randazzo <fusillicode@gmail.com>
2025-09-12 13:37:27 +0200

    fzf_lua_previewer is an fn now

--- yog/yog/nvrim/src/style_opts.rs
+++ yog/yog/nvrim/src/style_opts.rs
@@ -1,6 +1,7 @@
 use nvim_oxi::Dictionary;
 
 use crate::dict;
+use crate::fn_from;
 
 /// UI style options as a Nvim [`Dictionary`].
 ///
@@ -10,16 +11,20 @@ pub fn dict() -> Dictionary {
         "window": dict! {
             "border": "rounded",
         },
-        "fzf_lua_previewer": dict! {
-            "previewer": "builtin",
-            "winopts": dict! {
-                "title": "",
-                "height": 0.95,
-                "preview": dict! {
-                    "default":  "builtin",
-                    "layout":  "vertical",
-                    "vertical":  "down:60%",
-                }
+        "fzf_lua_previewer": fn_from!(fzf_lua_previewer)
+    }
+}
+
+fn fzf_lua_previewer(previewer_kind: Option<String>) -> Dictionary {
+    dict! {
+        "previewer": previewer_kind.unwrap_or_else(|| "builtin".to_string()),
+        "winopts": dict! {
+            "title": "",
+            "height": 0.95,
+            "preview": dict! {
+                "default":  "builtin",
+                "layout":  "vertical",
+                "vertical":  "down:60%",
             }
         }
     }
