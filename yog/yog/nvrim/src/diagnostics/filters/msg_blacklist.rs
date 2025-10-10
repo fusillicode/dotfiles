389b1d44e411fcad4ac860ab306cc7eff4c852b7
Gianluca Randazzo <fusillicode@gmail.com>
2025-10-10 16:00:21 +0200

    filter harper thinghies

--- yog/yog/nvrim/src/diagnostics/filters/msg_blacklist.rs
+++ yog/yog/nvrim/src/diagnostics/filters/msg_blacklist.rs
@@ -10,80 +10,7 @@ pub struct MsgBlacklistFilter {
     /// Blacklist of messages per source.
     pub blacklist: HashMap<String, Vec<String>>,
     /// The buffer path pattern to match.
-    pub buf_path: String,
-}
-
-impl MsgBlacklistFilter {
-    /// Returns all message blacklist filters.
-    pub fn all() -> Vec<Box<dyn DiagnosticsFilter>> {
-        Self::typos_lsp_msg_filters()
-    }
-
-    /// Creates filters for typos LSP messages.
-    fn typos_lsp_msg_filters() -> Vec<Box<dyn DiagnosticsFilter>> {
-        let typos_common_blacklist = vec![(
-            "typos".into(),
-            [
-                "accidentes",
-                "administrar",
-                "anual",
-                "aplicable",
-                "autor",
-                "calle",
-                "clase",
-                "clea",
-                "cliente",
-                "clientes",
-                "comercial",
-                "conceptos",
-                "confidencial",
-                "constituye",
-                "decisiones",
-                "emision",
-                "explosivas",
-                "foto",
-                "importante",
-                "individuales",
-                "informativo",
-                "informe",
-                "internacional",
-                "legislativo",
-                "limite",
-                "materiales",
-                "materias",
-                "minerales",
-                "momento",
-                "patrones",
-                "presentes",
-                "producto",
-                "profesional",
-                "regulatorias",
-                "responsable",
-                "ser",
-                "ue",
-                "utiliza",
-                "nd",
-                "ot",
-                "aci",
-            ]
-            .iter()
-            .map(|term| format!("`{term}` should be"))
-            .collect(),
-        )]
-        .into_iter()
-        .collect::<HashMap<_, _>>();
-
-        vec![
-            Box::new(Self {
-                buf_path: "/es-be/".into(),
-                blacklist: typos_common_blacklist.clone(),
-            }),
-            Box::new(Self {
-                buf_path: "/yog/".into(),
-                blacklist: typos_common_blacklist,
-            }),
-        ]
-    }
+    pub buf_path: Option<String>,
 }
 
 impl DiagnosticsFilter for MsgBlacklistFilter {
@@ -95,7 +22,9 @@ impl DiagnosticsFilter for MsgBlacklistFilter {
         let Some(lsp_diag) = lsp_diag else {
             return Ok(false);
         };
-        if !buf_path.contains(&self.buf_path) {
+        if let Some(ref bp) = self.buf_path
+            && !buf_path.contains(bp)
+        {
             return Ok(false);
         }
         let Some(blacklist) = self.blacklist.get(&lsp_diag.get_t::<nvim_oxi::String>("source")?) else {
@@ -107,4 +36,72 @@ impl DiagnosticsFilter for MsgBlacklistFilter {
         }
         Ok(false)
     }
+}
+
+pub fn harper_ls_filters() -> Vec<Box<dyn DiagnosticsFilter>> {
+    let mut blacklist = HashMap::new();
+    blacklist.insert("Harper".into(), vec!["stderr".into(), "stdout".into(), "stdin".into()]);
+    vec![Box::new(MsgBlacklistFilter {
+        buf_path: None,
+        blacklist,
+    })]
+}
+
+pub fn typos_lsp_filters() -> Vec<Box<dyn DiagnosticsFilter>> {
+    let typos_common_blacklist = vec![(
+        "typos".into(),
+        [
+            "accidentes",
+            "administrar",
+            "anual",
+            "aplicable",
+            "autor",
+            "calle",
+            "clase",
+            "clea",
+            "cliente",
+            "clientes",
+            "comercial",
+            "conceptos",
+            "confidencial",
+            "constituye",
+            "decisiones",
+            "emision",
+            "explosivas",
+            "foto",
+            "importante",
+            "individuales",
+            "informativo",
+            "informe",
+            "internacional",
+            "legislativo",
+            "limite",
+            "materiales",
+            "materias",
+            "minerales",
+            "momento",
+            "patrones",
+            "presentes",
+            "producto",
+            "profesional",
+            "regulatorias",
+            "responsable",
+            "ser",
+            "ue",
+            "utiliza",
+            "nd",
+            "ot",
+            "aci",
+        ]
+        .iter()
+        .map(|term| format!("`{term}` should be"))
+        .collect(),
+    )]
+    .into_iter()
+    .collect::<HashMap<_, _>>();
+
+    vec![Box::new(MsgBlacklistFilter {
+        buf_path: None,
+        blacklist: typos_common_blacklist.clone(),
+    })]
 }
