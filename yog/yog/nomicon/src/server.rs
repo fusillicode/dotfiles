3c8a537eb700f217a21c7e60e467f5bd592f3411
Gianluca Randazzo <fusillicode@gmail.com>
2025-10-13 10:32:07 +0200

    just trying something out

--- yog/yog/nomicon/src/server.rs
+++ yog/yog/nomicon/src/server.rs
@@ -1,13 +1,25 @@
+use std::convert::Infallible;
+
+use async_stream::stream;
 use axum::Router;
+use axum::response::IntoResponse;
+use axum::response::Sse;
 use axum::routing::get;
+use datastar::prelude::PatchSignals;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new().route("/", get(gen_events));
 
     Ok(router.into())
 }
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+async fn gen_events() -> impl IntoResponse {
+    Sse::new(stream! {
+        for idx in 0..42  {
+            let patch = PatchSignals::new(format!(r#"{{"generating": {idx}}}"#));
+            let sse_event = patch.write_as_axum_sse_event();
+            yield Ok::<_, Infallible>(sse_event);
+        }
+    })
 }
