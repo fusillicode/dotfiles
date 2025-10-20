1116f180cb9583dd6f81c4a64c9c86dd15e57890
fusillicode <2248534+fusillicode@users.noreply.github.com>
2025-10-20 11:45:21 +0200

    extract diagnostic config with text override

---
+++ yog/yog/nvrim/src/diagnostics/config.rs
@@ -0,0 +1,42 @@
+use nvim_oxi::Array;
+use nvim_oxi::Dictionary;
+use nvim_oxi::Object;
+use strum::IntoEnumIterator;
+
+use crate::diagnostics::DiagnosticSeverity;
+use crate::diagnostics::dict;
+use crate::diagnostics::formatter;
+use crate::fn_from;
+use crate::oxi_ext::dict::DictionaryExt;
+
+/// Nvim diagnostics configuration.
+pub fn get() -> Dictionary {
+    let texts_signs: Array = DiagnosticSeverity::iter()
+        .map(|s| Object::from(s.to_string()))
+        .collect();
+
+    dict! {
+        "severity_sort": true,
+        "signs": true,
+        "underline": true,
+        "update_in_insert": false,
+        "virtual_text": false,
+        "float": dict! {
+            "anchor_bias": "above",
+            "border": crate::style_opts::dict()
+                .get_dict(&["window"])
+                .unwrap_or_default()
+                .unwrap_or_default()
+                .get_t::<nvim_oxi::String>("border").unwrap_or_else(|_| "none".to_string()),
+            "focusable": true,
+            "format": fn_from!(formatter::format),
+            "header": "",
+            "prefix": "",
+            "source": false,
+            "suffix": "",
+        },
+        "signs": dict! {
+            "text": texts_signs
+        }
+    }
+}
