1c11757e2f41a97a77ab56fd51e2c45d706d886e
Gianluca Randazzo <fusillicode@gmail.com>
2025-11-05 08:23:28 +0100

    add get_diag_msg_or_skip unit tests

--- yog/yog/nvrim/src/diagnostics/filters/lsps.rs
+++ yog/yog/nvrim/src/diagnostics/filters/lsps.rs
@@ -4,6 +4,7 @@ use ytil_nvim_oxi::dict::DictionaryExt as _;
 pub mod harper_ls;
 pub mod typos_lsp;
 
+#[cfg_attr(test, derive(Debug, Eq, PartialEq))]
 pub enum GetDiagMsgOutput {
     Msg(String),
     Skip,
@@ -23,5 +24,94 @@ pub trait LspFilter {
             return Ok(GetDiagMsgOutput::Skip);
         }
         Ok(GetDiagMsgOutput::Msg(lsp_diag.get_t::<nvim_oxi::String>("message")?))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_diag_msg_or_skip_when_buf_path_not_matched_returns_skip() {
+        let filter = TestFilter {
+            source: "Test",
+            buf_path: Some("src/"),
+        };
+        let diag = dict! {
+            source: "Test",
+            message: "some message",
+        };
+        assert2::let_assert!(Ok(result) = filter.get_diag_msg_or_skip("tests/main.rs", &diag));
+        pretty_assertions::assert_eq!(result, GetDiagMsgOutput::Skip);
+    }
+
+    #[test]
+    fn get_diag_msg_or_skip_when_buf_path_matched_but_source_none_returns_skip() {
+        let filter = TestFilter {
+            source: "Test",
+            buf_path: Some("src/"),
+        };
+        let diag = dict! {
+            message: "some message",
+        };
+        assert2::let_assert!(Ok(result) = filter.get_diag_msg_or_skip("src/main.rs", &diag));
+        pretty_assertions::assert_eq!(result, GetDiagMsgOutput::Skip);
+    }
+
+    #[test]
+    fn get_diag_msg_or_skip_when_buf_path_matched_but_source_mismatch_returns_skip() {
+        let filter = TestFilter {
+            source: "Test",
+            buf_path: Some("src/"),
+        };
+        let diag = dict! {
+            source: "Other",
+            message: "some message",
+        };
+        assert2::let_assert!(Ok(result) = filter.get_diag_msg_or_skip("src/main.rs", &diag));
+        pretty_assertions::assert_eq!(result, GetDiagMsgOutput::Skip);
+    }
+
+    #[test]
+    fn get_diag_msg_or_skip_when_buf_path_and_source_matches_returns_msg() {
+        let filter = TestFilter {
+            source: "Test",
+            buf_path: Some("src/"),
+        };
+        let diag = dict! {
+            source: "Test",
+            message: "some message",
+        };
+        assert2::let_assert!(Ok(result) = filter.get_diag_msg_or_skip("src/main.rs", &diag));
+        pretty_assertions::assert_eq!(result, GetDiagMsgOutput::Msg("some message".to_string()));
+    }
+
+    #[test]
+    fn get_diag_msg_or_skip_when_no_buf_path_and_source_matches_returns_msg() {
+        let filter = TestFilter {
+            source: "Test",
+            buf_path: None,
+        };
+        let diag = dict! {
+            source: "Test",
+            message: "another message",
+        };
+        assert2::let_assert!(Ok(result) = filter.get_diag_msg_or_skip("any/path.rs", &diag));
+        pretty_assertions::assert_eq!(result, GetDiagMsgOutput::Msg("another message".to_string()));
+    }
+
+    struct TestFilter {
+        source: &'static str,
+        buf_path: Option<&'static str>,
+    }
+
+    impl LspFilter for TestFilter {
+        fn buf_path(&self) -> Option<&str> {
+            self.buf_path
+        }
+
+        fn source(&self) -> &str {
+            self.source
+        }
     }
 }
