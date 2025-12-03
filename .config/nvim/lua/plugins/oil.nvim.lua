b3d38c455b01c416fa8d33e431870ad54772c1cc
Gianluca Randazzo <fusillicode@gmail.com>
2025-12-03 07:50:34 +0100

    oil

--- .config/nvim/lua/plugins/oil.nvim.lua
+++ .config/nvim/lua/plugins/oil.nvim.lua
@@ -15,7 +15,7 @@ return {
       delete_to_trash = true,
       float = {
         border = style_opts['window']['border'],
-        max_width = 0.7,
+        max_width = 0.68,
         max_height = 0.99,
         override = function(conf)
           return vim.tbl_extend('force', conf, {
