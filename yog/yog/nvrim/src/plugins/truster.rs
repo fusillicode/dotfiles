a3ea12e871cc7ab2ae0ae1f059b62dcc745b7fa7
Gianluca Randazzo <fusillicode@gmail.com>
2025-12-09 20:24:13 +0100

    is this the issue?

--- yog/yog/nvrim/src/plugins/truster.rs
+++ yog/yog/nvrim/src/plugins/truster.rs
@@ -135,7 +135,7 @@ fn run_test_in_nvim_term(test_runner: &str, test_name: &str) -> Option<()> {
         return None;
     };
 
-    terminal_buffer.send_command(&format!("{test_runner} {test_name}\n"));
+    ytil_noxi::buffer::send_command(&terminal_buffer, &format!("{test_runner} {test_name}\n"));
 
     Some(())
 }
