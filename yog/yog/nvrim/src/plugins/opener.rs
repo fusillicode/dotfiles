e45dfbd09e7c9c2c82042b931427d09d57466980
Gianluca Randazzo <fusillicode@gmail.com>
2025-12-11 15:11:38 +0100

    MaybeTextFile

--- yog/yog/nvrim/src/plugins/opener.rs
+++ yog/yog/nvrim/src/plugins/opener.rs
@@ -13,18 +13,25 @@ fn open_token_under_cursor(_: ()) -> Option<()> {
     let token_under_cursor = token_under_cursor::get(())?;
 
     match token_under_cursor {
-        TokenUnderCursor::BinaryFile(_) | TokenUnderCursor::Word(_) => None,
-        TokenUnderCursor::TextFile { path, lnum, .. } => {
-            let open_path_at_line_cmd = format!("edit +{lnum} {path}");
+        TokenUnderCursor::BinaryFile(_) | TokenUnderCursor::MaybeTextFile { .. } => None,
+        TokenUnderCursor::TextFile { path, lnum, col } => {
+            let open_path_cmd = format!(
+                "edit {path} | call cursor({}, {})",
+                lnum.unwrap_or_default(),
+                col.unwrap_or_default()
+            );
+
             let vim_script = if let Some(win_num) =
                 ytil_noxi::window::find_with_buffer("").and_then(|(win, _)| ytil_noxi::window::get_number(&win))
             {
-                format!("{win_num} wincmd w | {open_path_at_line_cmd}")
+                format!("{win_num} wincmd w | {open_path_cmd}")
             } else {
                 let width = crate::layout::compute_width(70)?;
-                format!("vsplit | vertical resize {width} | {open_path_at_line_cmd}")
+                format!("vsplit | vertical resize {width} | {open_path_cmd}")
             };
+
             ytil_noxi::common::exec_vim_script(&vim_script, None);
+
             Some(())
         }
         TokenUnderCursor::Url(arg) | TokenUnderCursor::Directory(arg) => ytil_sys::open(&arg)
