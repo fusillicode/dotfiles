ee0b72254c9501b0e5c17021a384adb061ab03d3
user <user@nocciolotto.station>
2025-12-16 15:56:39 +0100

    less is more

--- bin/update_cargo_bins.sh
+++ bin/update_cargo_bins.sh
@@ -4,14 +4,11 @@ set -euo pipefail
 
 # cargo CLI tools ❤️
 cargo install --force \
-  \ # atuin \
   cargo-machete \
   cargo-make \
   cargo-sort \
   cargo-sort-derives \
   ccase \
-  \ # difftastic \
-  \ # drill \
   fd-find \
   mise \
   pv \
@@ -20,4 +17,3 @@ cargo install --force \
   sqlx-cli \
   starship \
   typos-cli \
-  \ # watchexec-cli
