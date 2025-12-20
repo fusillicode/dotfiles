5c5653bae132c2db0bb675ead47f1b5f6cee0263
fusillicode <2248534+fusillicode@users.noreply.github.com>
2025-12-20 17:46:03 +0100

    more macos stuff

--- macos.sh
+++ macos.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# Disable general animations
+defaults write -g QLPanelAnimationDuration -float 0;
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false;
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001;
+
+# Menubar
+defaults -currentHost write com.apple.controlcenter Bluetooth -int 18;
+defaults -currentHost write com.apple.controlcenter Sound -int 18;
+defaults -currentHost write com.apple.controlcenter Battery -int 18;
+defaults -currentHost write com.apple.controlcenter BatteryShowPercentage -bool true;
+defaults write com.apple.menuextra.clock ShowDate -int 1;
+killall ControlCenter; killall SystemUIServer;
+
 # Finder
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true;
 defaults write com.apple.finder AppleShowAllFiles -bool true;
@@ -8,7 +21,9 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false;
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv";
 defaults write com.apple.finder _FXSortFoldersFirst -bool true;
 defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true;
-sudo find / -name ".DS_Store" -delete;
+echo 'Remove all .DS_Store'
+sudo find / -name '.DS_Store' -delete;
+killall Finder;
 
 # Dock
 defaults write com.apple.dock autohide-delay -float 0;
@@ -18,11 +33,4 @@ defaults write com.apple.dock mineffect -string "scale";
 defaults write com.apple.dock minimize-to-application -bool true;
 defaults write com.apple.dock show-recents -bool false;
 defaults write com.apple.dock static-only -bool true;
-
-# Disable other animations
-defaults write -g QLPanelAnimationDuration -float 0;
-defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false;
-defaults write NSGlobalDomain NSWindowResizeTime -float 0.001;
-
-# Apply changes by restarting the Dock and Finder
-killall Dock; killall Finder
+killall Dock;
