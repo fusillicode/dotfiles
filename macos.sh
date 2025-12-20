f725d122a247e5118c7d5b96842a819997574809
fusillicode <2248534+fusillicode@users.noreply.github.com>
2025-12-20 14:52:03 +0100

    other macos stuff

--- macos.sh
+++ macos.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
 # Finder
-defaults write com.apple.finder DisableAllAnimations -bool true;
-defaults write com.apple.finder AppleShowAllFiles -bool true;
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv";
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true;
+defaults write com.apple.finder AppleShowAllFiles -bool true;
+defaults write com.apple.finder DisableAllAnimations -bool true;
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false;
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv";
 defaults write com.apple.finder _FXSortFoldersFirst -bool true;
 defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true;
 sudo find / -name ".DS_Store" -delete;
 
 # Dock
-defaults write com.apple.dock autohide-time-modifier -float 0;
 defaults write com.apple.dock autohide-delay -float 0;
-defaults write com.apple.dock static-only -bool true;
+defaults write com.apple.dock autohide-time-modifier -float 0;
+defaults write com.apple.dock expose-animation-duration -float 0;
+defaults write com.apple.dock mineffect -string "scale";
+defaults write com.apple.dock minimize-to-application -bool true;
 defaults write com.apple.dock show-recents -bool false;
+defaults write com.apple.dock static-only -bool true;
 
 # Disable other animations
+defaults write -g QLPanelAnimationDuration -float 0;
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false;
 defaults write NSGlobalDomain NSWindowResizeTime -float 0.001;
-defaults write -g QLPanelAnimationDuration -float 0;
-defaults write com.apple.dock expose-animation-duration -float 0;
 
 # Apply changes by restarting the Dock and Finder
 killall Dock; killall Finder
