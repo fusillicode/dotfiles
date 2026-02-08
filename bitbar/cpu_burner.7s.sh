c0ba8e00cbaa620f8b0f16a0e7abe1eb1d76ad51
fusillicode <2248534+fusillicode@users.noreply.github.com>
2026-02-08 16:25:04 +0100

    goodbye bitbar

--- bitbar/cpu_burner.7s.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck disable=SC2207
-cpu_burner_data=($(ps -Ar -o pid= -o %cpu= -o comm= | head -n 1))
-
-cpu_burner_name=${cpu_burner_data[2]##*/}
-
-echo "${cpu_burner_data[0]}  $cpu_burner_name  ${cpu_burner_data[1]}% | size=12"
