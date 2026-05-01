f8ab44885489f8b52ab80d6779e6da992f7e118b
fusillicode <2248534+fusillicode@users.noreply.github.com>
2026-05-01 07:22:07 +0200

    format inner

--- .config/nvim/lua/plugins/conform.nvim.lua
+++ .config/nvim/lua/plugins/conform.nvim.lua
@@ -4,11 +4,13 @@ return {
   cmd = { 'ConformInfo', 'ConformAt', },
   config = function()
     local conform = require('conform')
+    local nvrim = require('nvrim')
     -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#automatically-run-slow-formatters-async
     local slow_format_filetypes = {}
 
     conform.setup({
       formatters = {
+        qj = { command = 'qj', args = { '.', }, stdin = true, },
         sqruff = {
           prepend_args = { '--config', os.getenv('HOME') .. '/data/dev/dotfiles/dotfiles/.sqruff', },
           require_cwd = false,
@@ -49,17 +51,39 @@ return {
       end,
     })
 
-    -- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#format-command
-    vim.api.nvim_create_user_command('Format', function(args)
-      local range = nil
-      if args.count ~= -1 then
-        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-        range = {
-          start = { args.line1, 0, },
-          ['end'] = { args.line2, end_line:len(), },
-        }
+    local function formatter_names(args, lines)
+      local explicit = args.fargs[1]
+      if explicit and explicit ~= '' then
+        return conform.formatters_by_ft[explicit] or { explicit, }
       end
-      conform.format({ async = true, lsp_format = 'fallback', range = range, })
-    end, { range = true, })
+
+      if conform.formatters_by_ft[vim.bo.filetype] then return conform.formatters_by_ft[vim.bo.filetype] end
+      if pcall(vim.json.decode, table.concat(lines, '\n')) then return conform.formatters_by_ft.json end
+      return conform.formatters_by_ft['*']
+    end
+
+    local function conform_at(args)
+      local selection = nvrim.buffer.get_selection_for_ex_range(args.line1, args.line2)
+      if not selection then return end
+      conform.format_lines(formatter_names(args, selection.lines), selection.lines, { async = true, }, function(err, formatted_lines)
+        if err then
+          vim.notify(err.message or err, vim.log.levels.ERROR)
+          return
+        end
+        vim.api.nvim_buf_set_text(0, selection.start[1], selection.start[2], selection['end'][1], selection['end'][2], formatted_lines)
+      end)
+    end
+
+    local function format_range(args)
+      if args.count ~= -1 then
+        conform_at(args)
+        return
+      end
+
+      conform.format({ async = true, lsp_format = 'fallback', })
+    end
+
+    vim.api.nvim_create_user_command('ConformAt', format_range, { nargs = '?', range = true, })
+    vim.api.nvim_create_user_command('Format', format_range, { nargs = '?', range = true, })
   end,
 }
