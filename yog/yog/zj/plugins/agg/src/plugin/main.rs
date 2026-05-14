61c819a9323cf48e09499d1e3cbe8d48f0dbca45
fusillicode <2248534+fusillicode@users.noreply.github.com>
2026-05-14 19:21:52 +0200

    agg cleanup

--- yog/yog/zj/plugins/agg/src/plugin/main.rs
+++ yog/yog/zj/plugins/agg/src/plugin/main.rs
@@ -3,14 +3,11 @@ use std::path::PathBuf;
 
 use zellij_tile::prelude::Event;
 use zellij_tile::prelude::EventType;
-use zellij_tile::prelude::KeyWithModifier;
 use zellij_tile::prelude::Mouse;
 use zellij_tile::prelude::PaneId;
-use zellij_tile::prelude::PaneManifest;
 use zellij_tile::prelude::PermissionStatus;
 use zellij_tile::prelude::PermissionType;
 use zellij_tile::prelude::PipeMessage;
-use zellij_tile::prelude::TabInfo;
 use zellij_tile::prelude::ZellijPlugin;
 
 use crate::plugin::ppick::state::PpickState;
@@ -61,81 +58,6 @@ pub struct State {
     pub render_buf: String,
 }
 
-impl State {
-    fn update_permission_granted(&mut self) -> bool {
-        match &mut self.component {
-            ComponentState::Tbar(tbar) => crate::plugin::tbar::update_permission_granted(tbar),
-            ComponentState::Ppick(ppick) => crate::plugin::ppick::update_permission_granted(ppick),
-        }
-    }
-
-    fn update_tabs(&mut self, tabs: Vec<TabInfo>) -> bool {
-        match &mut self.component {
-            ComponentState::Tbar(tbar) => crate::plugin::tbar::update_tabs(tbar, tabs),
-            ComponentState::Ppick(ppick) => crate::plugin::ppick::update_tabs(ppick, tabs),
-        }
-    }
-
-    fn update_panes(&mut self, manifest: &PaneManifest) -> bool {
-        match &mut self.component {
-            ComponentState::Tbar(tbar) => crate::plugin::tbar::update_panes(tbar, manifest),
-            ComponentState::Ppick(ppick) => crate::plugin::ppick::update_panes(ppick, manifest),
-        }
-    }
-
-    fn update_pane_closed(&mut self, pane_id: u32) -> bool {
-        match &mut self.component {
-            ComponentState::Tbar(tbar) => crate::plugin::tbar::update_pane_closed(tbar, pane_id),
-            ComponentState::Ppick(ppick) => crate::plugin::ppick::update_pane_closed(ppick, pane_id),
-        }
-    }
-
-    fn update_cwd(&mut self, pane_id: u32, cwd: PathBuf) -> bool {
-        match &mut self.component {
-            ComponentState::Tbar(tbar) => crate::plugin::tbar::update_cwd(tbar, pane_id, cwd),
-            ComponentState::Ppick(ppick) => crate::plugin::ppick::update_cwd(ppick, pane_id, &cwd),
-        }
-    }
-
-    fn update_command(&mut self, pane_id: PaneId, command: &[String], is_foreground: bool) -> bool {
-        let ComponentState::Ppick(ppick) = &mut self.component else {
-            return false;
-        };
-        crate::plugin::ppick::update_command(ppick, pane_id, command, is_foreground)
-    }
-
-    fn update_key(&mut self, key: &KeyWithModifier) -> bool {
-        let ComponentState::Ppick(ppick) = &mut self.component else {
-            return false;
-        };
-        crate::plugin::ppick::update_key(ppick, key)
-    }
-
-    fn update_run_command_result(
-        &mut self,
-        exit_code: Option<i32>,
-        stdout: &[u8],
-        stderr: &[u8],
-        context: &BTreeMap<String, String>,
-    ) -> bool {
-        match &mut self.component {
-            ComponentState::Tbar(tbar) => {
-                crate::plugin::tbar::update_run_command_result(tbar, exit_code, stdout, context)
-            }
-            ComponentState::Ppick(ppick) => {
-                crate::plugin::ppick::update_run_command_result(ppick, exit_code, stdout, stderr, context)
-            }
-        }
-    }
-
-    fn update_mouse_left_click(&self, row: isize) -> bool {
-        let ComponentState::Tbar(tbar) = &self.component else {
-            return false;
-        };
-        crate::plugin::tbar::update_mouse_left_click(tbar, row, self.last_cols)
-    }
-}
-
 impl ZellijPlugin for State {
     fn load(&mut self, config: BTreeMap<String, String>) {
         self.component = ComponentState::from(Component::from(&config));
@@ -154,62 +76,55 @@ impl ZellijPlugin for State {
         zellij_tile::prelude::subscribe(&[EventType::PermissionRequestResult]);
     }
 
+    #[expect(
+        clippy::wildcard_enum_match_arm,
+        reason = "agg only subscribes to a small Event subset"
+    )]
     fn update(&mut self, event: Event) -> bool {
         match event {
-            Event::PermissionRequestResult(PermissionStatus::Granted) => self.update_permission_granted(),
-            Event::TabUpdate(tabs) => self.update_tabs(tabs),
-            Event::PaneUpdate(manifest) => self.update_panes(&manifest),
-            Event::PaneClosed(PaneId::Terminal(pane_id)) => self.update_pane_closed(pane_id),
-            Event::CwdChanged(PaneId::Terminal(pane_id), cwd, _clients) => self.update_cwd(pane_id, cwd),
-            Event::CommandChanged(pane_id, command, is_foreground, _clients) => {
-                self.update_command(pane_id, &command, is_foreground)
-            }
-            Event::Key(key) => self.update_key(&key),
-            Event::RunCommandResult(exit_code, stdout, stderr, context) => {
-                self.update_run_command_result(exit_code, &stdout, &stderr, &context)
-            }
-            Event::Mouse(Mouse::LeftClick(row, _col)) => self.update_mouse_left_click(row),
-            Event::ModeUpdate(_)
-            | Event::Mouse(_)
-            | Event::Timer(_)
-            | Event::CopyToClipboard(_)
-            | Event::SystemClipboardFailure
-            | Event::InputReceived
-            | Event::Visible(_)
-            | Event::CustomMessage(..)
-            | Event::FileSystemCreate(_)
-            | Event::FileSystemRead(_)
-            | Event::FileSystemUpdate(_)
-            | Event::FileSystemDelete(_)
-            | Event::PermissionRequestResult(_)
-            | Event::SessionUpdate(..)
-            | Event::WebRequestResult(..)
-            | Event::CommandPaneOpened(..)
-            | Event::CommandPaneExited(..)
-            | Event::PaneClosed(_)
-            | Event::EditPaneOpened(..)
-            | Event::EditPaneExited(..)
-            | Event::CommandPaneReRun(..)
-            | Event::FailedToWriteConfigToDisk(_)
-            | Event::ListClients(_)
-            | Event::HostFolderChanged(_)
-            | Event::FailedToChangeHostFolder(_)
-            | Event::PastedText(_)
-            | Event::ConfigWasWrittenToDisk
-            | Event::WebServerStatus(_)
-            | Event::FailedToStartWebServer(_)
-            | Event::BeforeClose
-            | Event::InterceptedKeyPress(_)
-            | Event::UserAction(..)
-            | Event::PaneRenderReport(_)
-            | Event::PaneRenderReportWithAnsi(_)
-            | Event::ActionComplete(..)
-            | Event::CwdChanged(..)
-            | Event::AvailableLayoutInfo(..)
-            | Event::PluginConfigurationChanged(_)
-            | Event::HighlightClicked { .. }
-            | Event::InitialKeybinds(_)
-            | _ => false,
+            Event::PermissionRequestResult(PermissionStatus::Granted) => match &mut self.component {
+                ComponentState::Tbar(tbar) => crate::plugin::tbar::update_permission_granted(tbar),
+                ComponentState::Ppick(ppick) => crate::plugin::ppick::update_permission_granted(ppick),
+            },
+            Event::TabUpdate(tabs) => match &mut self.component {
+                ComponentState::Tbar(tbar) => crate::plugin::tbar::update_tabs(tbar, tabs),
+                ComponentState::Ppick(ppick) => crate::plugin::ppick::update_tabs(ppick, tabs),
+            },
+            Event::PaneUpdate(manifest) => match &mut self.component {
+                ComponentState::Tbar(tbar) => crate::plugin::tbar::update_panes(tbar, &manifest),
+                ComponentState::Ppick(ppick) => crate::plugin::ppick::update_panes(ppick, &manifest),
+            },
+            Event::PaneClosed(PaneId::Terminal(pane_id)) => match &mut self.component {
+                ComponentState::Tbar(tbar) => crate::plugin::tbar::update_pane_closed(tbar, pane_id),
+                ComponentState::Ppick(ppick) => crate::plugin::ppick::update_pane_closed(ppick, pane_id),
+            },
+            Event::CwdChanged(PaneId::Terminal(pane_id), cwd, _clients) => match &mut self.component {
+                ComponentState::Tbar(tbar) => crate::plugin::tbar::update_cwd(tbar, pane_id, cwd),
+                ComponentState::Ppick(ppick) => crate::plugin::ppick::update_cwd(ppick, pane_id, &cwd),
+            },
+            Event::CommandChanged(pane_id, command, is_foreground, _clients) => match &mut self.component {
+                ComponentState::Tbar(_) => false,
+                ComponentState::Ppick(ppick) => {
+                    crate::plugin::ppick::update_command(ppick, pane_id, &command, is_foreground)
+                }
+            },
+            Event::Key(key) => match &mut self.component {
+                ComponentState::Tbar(_) => false,
+                ComponentState::Ppick(ppick) => crate::plugin::ppick::update_key(ppick, &key),
+            },
+            Event::RunCommandResult(exit_code, stdout, stderr, context) => match &mut self.component {
+                ComponentState::Tbar(tbar) => {
+                    crate::plugin::tbar::update_run_command_result(tbar, exit_code, &stdout, &context)
+                }
+                ComponentState::Ppick(ppick) => {
+                    crate::plugin::ppick::update_run_command_result(ppick, exit_code, &stdout, &stderr, &context)
+                }
+            },
+            Event::Mouse(Mouse::LeftClick(row, _col)) => match &self.component {
+                ComponentState::Tbar(tbar) => crate::plugin::tbar::update_mouse_left_click(tbar, row, self.last_cols),
+                ComponentState::Ppick(_) => false,
+            },
+            _ => false,
         }
     }
 
